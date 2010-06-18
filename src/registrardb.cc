58ff8fb8554c867df1b6a0efbc9b5d71b64741d5
Simon Morlat <simon.morlat@linphone.org>
2010-06-18 11:01:56 +0200

    add registrar object

---
+++ src/registrardb.cc
@@ -0,0 +1,74 @@
+/*
+	Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010  Belledonne Communications SARL.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <registrardb.hh>
+
+#include <time.h>
+#include <cstdio>
+
+
+#include <sofia-sip/sip_protos.h>
+
+using namespace::std;
+
+Record::Record(const sip_from_t *from, const sip_contact_t *contact, time_t expireTime){
+	su_home_init(&mHome);
+	mExpireTime=expireTime;
+	mFrom=sip_from_copy (&mHome,from);
+	mContact=sip_contact_copy(&mHome,contact);
+}
+
+Record::~Record(){
+	su_home_deinit(&mHome);
+}
+
+RegistrarDb::RegistrarDb(){
+}
+
+void RegistrarDb::addRecord(const sip_from_t *from, const sip_contact_t *contact, time_t expireTime){
+	char tmp[128]={0};
+	
+	snprintf(tmp,sizeof(tmp)-1,"%s@%s",from->a_url->url_user,from->a_url->url_host);
+	Record * &ref_record=mRecords[tmp];
+	if (ref_record!=NULL){
+		delete ref_record;
+	}
+	ref_record=new Record(from,contact,expireTime);
+	
+}
+
+const sip_contact_t* RegistrarDb::retrieve(const sip_from_t *from){
+	map<string,Record*>::iterator it;
+
+	char tmp[128]={0};
+	snprintf(tmp,sizeof(tmp)-1,"%s@%s",from->a_url->url_user,from->a_url->url_host);
+	it=mRecords.find(tmp);
+	if (it!=mRecords.end()){
+		Record *r=(*it).second;
+		/*check expiration*/
+		if (time(NULL) <= r->getExpireTime()){
+			return r->getContact();
+		}else{
+			/*can be removed*/
+			mRecords.erase(it);
+			delete r;
+		}
+	}
+	return NULL;
+}
+
---
+++ src/registrardb.hh
@@ -0,0 +1,53 @@
+/*
+	Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010  Belledonne Communications SARL.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef registrardb_hh
+#define registrardb_hh
+
+#include <map>
+#include <string>
+
+#include <sofia-sip/sip.h>
+
+class Record{
+	public:
+		Record(const sip_from_t *from, const sip_contact_t *contact, time_t expireTime);
+		const sip_contact_t * getContact()const{
+			return mContact;
+		}
+		time_t getExpireTime()const{
+			return mExpireTime;
+		}
+		~Record();
+	private:
+		su_home_t mHome;
+		sip_from_t *mFrom;
+		sip_contact_t *mContact;
+		int mExpireTime;
+};
+
+class RegistrarDb{
+	public:
+		RegistrarDb();
+		void addRecord(const sip_from_t *from, const sip_contact_t *contact, time_t expireTime);
+		const sip_contact_t* retrieve(const sip_from_t *from);
+	private:
+		std::map<std::string,Record*> mRecords;
+};
+
+#endif
