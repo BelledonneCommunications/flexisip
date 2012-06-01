77f0779392b3dfc291c373170b1f385433e23936
Guillaume Beraudo <guillaume.beraudo@belledonne-communications.com>
2012-06-01 12:02:43 +0200

    Fix latex compilation (shell escape).
    Restore the make fail on latex error.
    Configure without docs to workaround any bug.

--- doc/latex_include_generator.sh
+++ doc/latex_include_generator.sh
@@ -2,9 +2,9 @@
 
 #../src/flexisip --set tex --dump-default-config > flexisip.tmp.tex
 
-echo "\\begin{verbatim}" > flexisip-help.tmp.tex
+echo '\\begin{verbatim}' > flexisip-help.tmp.tex
 ../src/flexisip --help | sed 'sY../src/YY'>> flexisip-help.tmp.tex
-echo "\\end{verbatim}" >> flexisip-help.tmp.tex
+echo '\\end{verbatim}' >> flexisip-help.tmp.tex
 
 modules=`../src/flexisip --dump-default-config | grep module:: | sed 's/\[//g' | sed 's/\]//g'`
 for m in $modules
