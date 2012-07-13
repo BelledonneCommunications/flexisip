918779c598b924f426d3cbefceb77b6d60bbaa03
Jehan Monnier <jehan.monnier@linphone.org>
2012-07-13 15:53:28 +0200

    add SILK and amr-wb to the list of supported codec

--- src/module-gatewayadapter.cc
+++ src/module-gatewayadapter.cc
@@ -192,6 +192,11 @@ void GatewayRegister::onMessage(const sip_t *sip) {
 		} else if (sip->sip_status->st_status == 408) {
 			LOGD("REGISTER timeout");
 			end();
+		} else if (sip->sip_status->st_status == 401){
+			LOGD("REGISTER challenged ");
+			ostringstream auth;
+			auth << "Digest:\"" << getFrom()->a_url->url_host << "\":" << getFrom()->a_url->url_user << ":" << getPassword() << endl;
+			nua_authenticate(nh, NUTAG_AUTH(auth.str().c_str()),TAG_END());
 		} else {
 			LOGD("REGISTER not handled response: %i", sip->sip_status->st_status);
 			end();
