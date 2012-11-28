a43f337cb355dac4037a82cbc4453c9bfef5cd98
Simon Morlat <simon.morlat@linphone.org>
2012-11-28 14:54:25 +0100

    add missing files

---
+++ src/telephone-event-filter.cc
@@ -0,0 +1,34 @@
+/*
+ Flexisip, a flexible SIP proxy server with media capabilities.
+ Copyright (C) 2012  Belledonne Communications SARL.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#include "telephone-event-filter.hh"
+#include <ortp/rtp.h>
+
+
+TelephoneEventFilter::TelephoneEventFilter(int telephone_event_pt) : mTelephoneEventPt(telephone_event_pt){
+}
+
+bool TelephoneEventFilter::onTransfer(uint8_t *data, size_t size){
+	rtp_header_t *h=(rtp_header_t*)data;
+	if (size<sizeof(rtp_header_t)) return true;
+	if (h->paytype==mTelephoneEventPt){
+		LOGD("Detected telephone event in stream, dropping.");
+		return false;
+	}
+	return true;
+}
