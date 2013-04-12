986c38d8af528d9307112cd4e6b75293ec3a2c7d
Simon Morlat <simon.morlat@linphone.org>
2013-04-12 12:51:12 +0200

    allow make distcheck to pass

--- scripts/debian/update_debian_changelog.sh
+++ scripts/debian/update_debian_changelog.sh
@@ -4,6 +4,12 @@
 PACKAGE=$1
 VERSION=$EPOCH$2
 
+if ! which dch ; then 
+	echo "dch command not found, please install 'devscripts' debian package."
+	exit 127
+fi
+
+
 grep "`echo $VERSION | sed 's/\./\\\./g'`" debian/changelog
 if [ $? -gt 0 ]
 then
