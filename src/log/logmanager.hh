fc37fb6cb51a4cff9f28daf5c5f486b6c50b65ec
Guillaume Beraudo <guillaume.beraudo@belledonne-communications.com>
2013-07-12 10:03:10 +0200

    Don't check if logging is enabled
    Not present in boost v1 and probably useless anyway.

--- src/log/logmanager.hh
+++ src/log/logmanager.hh
@@ -256,7 +256,6 @@ namespace log {
 	static inline void formated_log_with_severity(flexisip::log::level lvl, const char *fmt, va_list l) {
 		namespace keywords = boost::log::keywords;
 		namespace logging = boost::log;
-		if (!logging::core::get()->get_logging_enabled()) return;
 		auto lg=flexisip_logger::get();
 		logging::record rec = lg.open_record(keywords::severity = lvl);
 		if (rec)
