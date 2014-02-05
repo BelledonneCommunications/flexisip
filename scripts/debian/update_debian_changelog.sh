e7487fbfadaa915b9515a431cfcd19d130289760
Simon Morlat <simon.morlat@belledonne-communications.com>
2014-02-05 15:27:00 +0100

    again

--- scripts/debian/update_debian_changelog.sh
+++ scripts/debian/update_debian_changelog.sh
@@ -13,7 +13,7 @@ fi
 grep "`echo $VERSION | sed 's/\./\\\./g'`" debian/changelog
 if [ $? -gt 0 ]
 then
-        dch --newversion $VERSION -m "New version"
+        dch -b --newversion $VERSION -m "New version"
 else
 	echo "Changelog already up to date"
 fi
