0dd9c3d23403724ef378a1e31cb697f054f2e836
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2015-04-14 13:50:18 +0200

    Added dos protection rules for tcp protocol

--- src/module-dos.cc
+++ src/module-dos.cc
@@ -27,19 +27,31 @@ class ModuleDoS: public Module, ModuleToolbox {
 
 private:
 	static ModuleInfo<ModuleDoS> sInfo;
+	int mTimePeriod;
 	int mPacketRateLimit;
+	int mBanTime;
 
 	void onDeclare(GenericStruct *module_config) {
 		ConfigItemDescriptor configs[] = {
-			{ Integer , "packet-rate-limit", "Maximum packet rate received in 1 second to consider to consider it a DoS attack.","10"},
+			{ Integer , "time-period", "Number of milliseconds to calculate the packet rate", "1000"},
+			{ Integer , "packet-rate-limit", "Maximum packet rate received in [time-period] millisecond(s) to consider to consider it a DoS attack.", "10"},
+			{ Integer , "ban-time", "Number of minutes to ban the ip/port using iptables", "1"},
 			config_item_end
 		};
-		module_config->get<ConfigBoolean>("enabled")->setDefault("false");
+		module_config->get<ConfigBoolean>("enabled")->setDefault("true");
 		module_config->addChildrenValues(configs);
 	}
 
 	void onLoad(const GenericStruct *mc) {
+		mTimePeriod = mc->get<ConfigInt>("time-period")->read();
 		mPacketRateLimit = mc->get<ConfigInt>("packet-rate-limit")->read();
+		mBanTime = mc->get<ConfigInt>("ban-time")->read();
+		
+		tport_t *primaries=tport_primaries(nta_agent_tports(mAgent->getSofiaAgent()));
+		if (primaries == NULL) LOGF("No sip transport defined.");
+		for(tport_t *tport = primaries; tport != NULL; tport = tport_next(tport)) {
+			tport_set_params(tport, TPTAG_DOS(mTimePeriod), TAG_END());
+		}
 	}
 
 	void onUnload() {
@@ -50,9 +62,25 @@ private:
 		shared_ptr<tport_t> inTport = ev->getIncomingTport();
 		tport_t *tport = inTport.get();
 		float packet_count_rate = tport_get_packet_count_rate(tport);
-		LOGD("[DoS] Packet count rate for current tport is: %f", packet_count_rate);
-		if (packet_count_rate >= mPacketRateLimit) {
-			LOGW("[DoS] Packet count rate is > to the limit %i", mPacketRateLimit);
+		LOGD("Packet count rate (%f)", packet_count_rate);
+		
+		if (packet_count_rate >= mPacketRateLimit && !tport_is_udp(tport)) {
+			char iptables_cmd[512];
+			sockaddr *addr = tport_get_address(tport)->ai_addr;
+			socklen_t len = tport_get_address(tport)->ai_addrlen;
+			char ip[NI_MAXHOST], port[NI_MAXSERV];
+			if (getnameinfo(addr, len, ip, sizeof(ip), port, sizeof(port), NI_NUMERICHOST | NI_NUMERICSERV) == 0) {
+				if (getuid() != 0) {
+					LOGE("Flexisip not started with root privileges! Can't add iptables rule");
+					return;
+				}
+				LOGW("Packet count rate (%f) >= limit (%i), blocking ip/port %s/%s on protocol tcp for %i minutes", packet_count_rate, mPacketRateLimit, ip, port, mBanTime);
+				snprintf(iptables_cmd, sizeof(iptables_cmd), "iptables -A INPUT -p tcp -s %s -m multiport --sports %s -j DROP && echo \"iptables -D INPUT -p tcp -s %s -m multiport --sports %s -j DROP\" | at now +%i minutes", 
+					ip, port, ip, port, mBanTime);
+				system(iptables_cmd);
+			}
+			
+			tport_reset_packet_count_rate(tport);
 		}
 	}
 	
