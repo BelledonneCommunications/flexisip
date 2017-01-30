679a8fb1dcc6780ba57dbdd32928cc5cc7350a89
Sandrine Avakian <sandrine.avakian@belledonne-communications.com>
2017-01-30 15:02:29 +0100

    Adding global flexisip module in doc generation for wiki. Fixing syntax problems in wiki page content.

--- doc/update_xwiki.sh
+++ doc/update_xwiki.sh
@@ -28,5 +28,5 @@ modules=`$FLEXISIP --list-modules`
 
 # upload for DokuWiki
 python "xw.py" "global" "global.xwiki.txt" 
-
+upload_to_wiki "global" "xwiki" "xw.py" 
 upload_to_wiki "${modules[@]}" "xwiki" "xw.py" 
