5f6078b0fbfd60c7c33a296736738076d59f7c6c
Benjamin Reis <benjamin.reis@belledonne-communications.com>
2017-04-05 10:19:38 +0200

    Put log file back

--- src/log/logmanager.cc
+++ src/log/logmanager.cc
@@ -127,10 +127,10 @@ namespace flexisip {
 					printf("%s", str);
 				}
 				
-				//std::ostringstream nameStream;
-				//nameStream << "FlexisipLogs_" << fName;
-				//bctbx_log_handler_t *handler = bctbx_create_file_log_handler(max_size, DEFAULT_LOG_DIR, nameStream.str().c_str(), f);
-				//bctbx_add_log_handler(handler);
+				std::ostringstream nameStream;
+				nameStream << "FlexisipLogs_" << fName;
+				bctbx_log_handler_t *handler = bctbx_create_file_log_handler(max_size, DEFAULT_LOG_DIR, nameStream.str().c_str(), f);
+				bctbx_add_log_handler(handler);
 			} else {
 				std::ostringstream stringStream;
 				stringStream << "Error while writing logs in : " << copyOfStr << "\n";
