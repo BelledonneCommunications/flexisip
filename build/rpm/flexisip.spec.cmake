c53f15c6ddb7d1a48f4e001dcfc2407ab7781584
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-01-29 10:48:12 +0100

    Improve RPM packaging.

--- build/rpm/flexisip.spec.cmake
+++ build/rpm/flexisip.spec.cmake
@@ -1,48 +1,7 @@
 # -*- rpm-spec -*-
-#
-# ack
-#
-# Default is optimized for Pentium IV but will execute on Pentium II &
-# later (i686).
 
-# These 2 lines are here because we can build the RPM for flexisip, in which 
-# case we prefix the entire installation so that we don't break compatibility
-# with the user's libs.
-# To compile with bc prefix, use rpmbuild -ba --with bc [SPEC]
-# -*- rpm-spec -*-
-
-## rpmbuild options
-
-# default is to build with redis & protobuf support
-%define     redis      %{?_without_redis:0}%{!?_without_redis:1}
-%define     protobuf   %{?_without_protobuf:0}%{!?_without_protobuf:1}
-%define     push       %{?_with_push:1}%{!?_with_push:0}
-%define     transcoder %{?_without_transcoder:0}%{!?_without_transcoder:1}
-%define     snmp       %{?_without_snmp:0}%{!?_without_snmp:1}
-%define     presence   %{?_with_presence:1}%{!?_with_presence:0}
-%define     pkg_prefix %{?_with_bc:bc-}%{!?_with_bc:}
-%define     soci       %{?_without_soci:0}%{!?_without_soci:1}
-%{?_with_bc: %define    _prefix         /opt/belledonne-communications}
-
-# This is for debian builds where debug_package has to be manually specified,
-# whereas in centos it does not
-%define     flex_debug      %{!?_enable_debug_packages:%debug_package}%{?_enable_debug_package:%{nil}}
-# will be 1 if we need to generate a /opt/belledonne-communications RPM
-%define     bcpkg      %{?_with_bc:1}%{!?_with_bc:0}
-
-## end rpmbuild options
-
-%define _unpackaged_files_terminate_build 0
-
-# These lines are here because we can build the RPM for flexisip, in which
-# case we prefix the entire installation so that we don't break compatibility
-# with the user's libs.
-# To compile with bc prefix, use rpmbuild -ba --with bc [SPEC]
-%if  %{presence}
-%define                 pkg_name        %{?_with_bc:bc-flexisip-presence}%{!?_with_bc:flexisip-presence}
-%else
-%define                 pkg_name        %{?_with_bc:bc-flexisip}%{!?_with_bc:flexisip}
-%endif
+%define _prefix    @CMAKE_INSTALL_PREFIX@
+%define pkg_prefix @BC_PACKAGE_NAME_PREFIX@
 
 # re-define some directories for older RPMBuild versions which don't. This messes up the doc/ dir
 # taken from https://fedoraproject.org/wiki/Packaging:RPMMacros?rd=Packaging/RPMMacros
@@ -52,7 +11,7 @@
 
 %define build_number @PROJECT_VERSION_BUILD@
 Summary:	SIP proxy with media capabilities
-Name:		%pkg_name
+Name:		@CPACK_PACKAGE_NAME@
 Version:	@PROJECT_VERSION@
 Release:	%build_number%{?dist}
 #to be alined with redhat which changed epoc to 1 for an unknown reason
@@ -66,28 +25,29 @@ BuildRoot:	%{_tmppath}/%{name}-%{version}-%{release}-buildroot
 Requires: bash >= 2.0
 Requires: at >= 3.1.10
 Requires: %{pkg_prefix}sofia-sip >= 1.13
-%if %{protobuf}
+
+%if @ENABLE_PROTOBUF@
 Requires: protobuf >= 2.3.0
 #Requires: protobuf-c >= 0.15
 #BuildRequires: protobuf-c-devel >= 0.15
 BuildRequires: protobuf-compiler >= 2.3.0
 %endif
 
-%if %{redis}
+%if @ENABLE_REDIS@
 Requires: %{pkg_prefix}hiredis-devel >= 0.13
 %endif
 
-%if %{snmp}
+%if @ENABLE_SNMP@
 Requires: net-snmp-libs
 Requires: net-snmp-devel
 %endif
 
-%if %{soci}
-Requires: soci
-Requires: soci-mysql-devel
+%if @ENABLE_SOCI@
+Requires: %{pkg_prefix}soci
+Requires: %{pkg_prefix}soci-mysql-devel
 %endif
 
-%if %{transcoder}
+%if @ENABLE_TRANSCODER@
 Requires: %{pkg_prefix}mediastreamer
 %endif
 
@@ -107,21 +67,15 @@ Requires(postun): /sbin/service
 %description
 Extensible SIP proxy with media capabilities. Designed for robustness and easy of use.
 
+# This is for debian builds where debug_package has to be manually specified, whereas in centos it does not
+%define custom_debug_package %{!?_enable_debug_packages:%debug_package}%{?_enable_debug_package:%{nil}}
+%custom_debug_package
+
 %prep
 %setup -n %{name}-%{version}-%build_number
 
-%flex_debug
-
 %build
-%{expand:%%%cmake_name} . -DCMAKE_INSTALL_LIBDIR:PATH=%{_libdir} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} \
-    -DENABLE_REDIS=%{redis} \
-    -DENABLE_PROTOBUF=%{protobuf} \
-	-DENABLE_SNMP=%{snmp} \
-	-DENABLE_SOCI=%{soci} \
-	-DENABLE_TRANSCODER=%{transcoder} \
-	-DENABLE_PRESENCE=%{presence} \
-	-DENABLE_PUSHNOTIFICATIN=%{push} \
-        -DSYSCONF_INSTALL_DIR:PATH=%{_sysconfdir}
+%{expand:%%%cmake_name} . -DCMAKE_INSTALL_LIBDIR:PATH=%{_libdir} -DCMAKE_PREFIX_PATH:PATH=%{_prefix} -DSYSCONF_INSTALL_DIR:PATH=%{_sysconfdir} @RPM_ALL_CMAKE_OPTIONS@
 
 
 make %{?_smp_mflags}
@@ -140,12 +94,12 @@ mkdir -p  $RPM_BUILD_ROOT/%{_docdir}
 mkdir -p  $RPM_BUILD_ROOT/%{_localstatedir}/log/flexisip
 %if "0%{?dist}" == "0.deb"
 	install -p -m 0744 scripts/debian/flexisip $RPM_BUILD_ROOT%{_sysconfdir}/init.d/flexisip
-	%if  %{presence}
+	%if @ENABLE_PRESENCE@
 		install -p -m 0744 scripts/debian/flexisip-presence $RPM_BUILD_ROOT%{_sysconfdir}/init.d/flexisip-presence
 	%endif
 %else
 	install -p -m 0744 scripts/redhat/flexisip $RPM_BUILD_ROOT%{_sysconfdir}/init.d/flexisip
-	%if  %{presence}
+	%if @ENABLE_PRESENCE@
 		install -p -m 0744 scripts/redhat/flexisip-presence $RPM_BUILD_ROOT%{_sysconfdir}/init.d/flexisip-presence
 	%endif
 %endif
@@ -153,7 +107,7 @@ mkdir -p  $RPM_BUILD_ROOT/%{_localstatedir}/log/flexisip
 mkdir -p $RPM_BUILD_ROOT/lib/systemd/system
 install -p -m 0644 scripts/flexisip.service $RPM_BUILD_ROOT/lib/systemd/system
 install -p -m 0644 scripts/flexisip\@.service $RPM_BUILD_ROOT/lib/systemd/system
-%if  %{presence}
+%if @ENABLE_PRESENCE@
 	install -p -m 0644 scripts/flexisip-presence.service $RPM_BUILD_ROOT/lib/systemd/system
 	install -p -m 0644 scripts/flexisip-presence\@.service $RPM_BUILD_ROOT/lib/systemd/system
 %endif
@@ -162,10 +116,6 @@ install -p -m 0644 scripts/flexisip-logrotate $RPM_BUILD_ROOT%{_sysconfdir}/logr
 
 install -p -m 0744 scripts/flexisip_stats.py $RPM_BUILD_ROOT%{_bindir}
 install -p -m 0744 scripts/flexisip_monitor.py $RPM_BUILD_ROOT%{_bindir}
-
-%if %{bcpkg}
-export QA_RPATHS=0x0003
-%endif
 
 %check
 %{ctest_name} -V %{?_smp_mflags}
@@ -179,7 +129,7 @@ if [ $1 = 1 ]; then
         /sbin/chkconfig flexisip on
         service flexisip start
 
-%if %{presence}
+%if @ENABLE_PRESENCE@
         /sbin/chkconfig --add flexisip-presence
         /sbin/chkconfig flexisip-presence on
         service flexisip-presence start
@@ -190,7 +140,7 @@ fi
 if [ $1 = 0 ]; then
         service flexisip stop >/dev/null 2>&1 ||:
         /sbin/chkconfig --del flexisip
-%if %{presence}
+%if @ENABLE_PRESENCE@
         service flexisip-presence stop >/dev/null 2>&1 ||:
         /sbin/chkconfig --del flexisip-presence
 %endif
@@ -199,7 +149,7 @@ fi
 %postun
 if [ "$1" -ge "1" ]; then
         service flexisip condrestart > /dev/null 2>&1 ||:
-%if %{presence}
+%if @ENABLE_PRESENCE@
         service flexisip-presence condrestart > /dev/null 2>&1 ||:
 %endif
 fi
@@ -211,7 +161,7 @@ fi
 %{_bindir}/*
 %{_libdir}/*
 
-%if %{presence}
+%if @ENABLE_PRESENCE@
 	%{_sysconfdir}/init.d/flexisip-presence
 	/lib/systemd/system/flexisip-presence.service
 	/lib/systemd/system/flexisip-presence@.service
