aafd5fc1928a2a8532d1ed08e56833c1dcf6ef34
Ghislain MARY <ghislain.mary@belledonne-communications.com>
2018-01-29 17:20:46 +0100

    Blacklist soci dependency for now.

--- prepare.py
+++ prepare.py
@@ -45,7 +45,12 @@ class FlexisipRpmTarget(prepare.Target):
         self.required_build_platforms = ['Linux', 'Darwin']
         self.config_file = 'configs/config-flexisip-rpm.cmake'
         self.external_source_path = os.path.join(current_path, 'submodules')
-        self.additional_args = [ '-DLINPHONE_BUILDER_TARGET=flexisip', '-DLINPHONE_BUILDER_TOP_DIR=' + current_path ]
+        external_builders_path = os.path.join(current_path, 'cmake_builder')
+        self.additional_args = [
+            "-DLINPHONE_BUILDER_TARGET=flexisip",
+            "-DLINPHONE_BUILDER_TOP_DIR=" + current_path,
+            "-DLINPHONE_BUILDER_EXTERNAL_BUILDERS_PATH=" + external_builders_path
+        ]
 
 class FlexisipTarget(prepare.Target):
 
@@ -55,7 +60,12 @@ class FlexisipTarget(prepare.Target):
         self.required_build_platforms = ['Linux', 'Darwin']
         self.config_file = 'configs/config-flexisip.cmake'
         self.external_source_path = os.path.join(current_path, 'submodules')
-        self.additional_args = [ '-DLINPHONE_BUILDER_TARGET=flexisip', '-DLINPHONE_BUILDER_TOP_DIR=' + current_path ]
+        external_builders_path = os.path.join(current_path, 'cmake_builder')
+        self.additional_args = [
+            "-DLINPHONE_BUILDER_TARGET=flexisip",
+            "-DLINPHONE_BUILDER_TOP_DIR=" + current_path,
+            "-DLINPHONE_BUILDER_EXTERNAL_BUILDERS_PATH=" + external_builders_path
+        ]
 
 
 flexisip_targets = {
