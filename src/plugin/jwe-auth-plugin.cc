0530d903217fd8012cb58fafc8915306ba9e4a7d
Ronan Abhamon <ronan.abhamon@belledonne-communications.com>
2018-05-11 14:32:22 +0200

    feat(JweAuth): check expiration time

--- src/plugin/jwe-auth-plugin.cc
+++ src/plugin/jwe-auth-plugin.cc
@@ -39,7 +39,7 @@ namespace {
 	constexpr char JwkFileExtension[] = "jwk";
 
 	// TODO: Remove me.
-	const string JwksPath = "/home/rabhamon/jwks/";
+	const string JwksPath("/home/rabhamon/jwks/");
 }
 
 // =============================================================================
@@ -195,11 +195,92 @@ static json_t *decryptJwe(const char *text, const json_t *jwk) {
 	return jwt;
 }
 
+template<typename T>
+struct TypeToJsonFormat {};
+
+template<>
+struct TypeToJsonFormat<int> {
+	static constexpr char value = 'i';
+};
+
+template<>
+struct TypeToJsonFormat<json_int_t> {
+	static constexpr char value = 'I';
+};
+
+template<typename T>
+static T extractJsonValue (json_t *jwt, const char *attrName, bool *error = nullptr) {
+	constexpr char format[] = { '{', 's', ':', TypeToJsonFormat<T>::value, '}', '\0' };
+
+	T value{};
+	bool soFarSoGood = true;
+
+	if (json_unpack(jwt, format, attrName, &value) < 0) {
+		cerr << "Unable to unpack value: `" << attrName << "`." << endl;
+		soFarSoGood = false;
+	}
+
+	if (error) *error = soFarSoGood;
+	return value;
+}
+
+template<typename T>
+static T extractJsonOptionalValue (json_t *jwt, const char *attrName, const T &defaultValue, bool *error = nullptr) {
+	constexpr char format[] = { TypeToJsonFormat<T>::value, '\0' };
+
+	T value(defaultValue);
+	bool soFarSoGood = true;
+
+	json_t *valueObject;
+	if (json_unpack(jwt, "{s?o}", attrName, &valueObject) < 0) {
+		cerr << "Unable to unpack optional object: `" << attrName << "`." << endl;
+		soFarSoGood = false;
+	} else if (valueObject && json_unpack(valueObject, format, &value) < 0) {
+		cerr << "Unable to unpack existing value: `" << attrName << "`." << endl;
+		soFarSoGood = false;
+	}
+
+	if (error) *error = soFarSoGood;
+	return value;
+}
+
+static bool checkJwtTime(json_t *jwt) {
+	const time_t currentTime = time(nullptr);
+	bool error;
+
+	// Check optional "exp" attr.
+	const json_int_t expValue = extractJsonOptionalValue<json_int_t>(jwt, "exp", -1, &error);
+	if (error)
+		return false;
+	if (expValue != -1 && time_t(expValue) < currentTime) {
+		cerr << "JWT (exp) has expired." << endl;
+		return false;
+	}
+
+	// Not in the JSON Web Token RFC. Check Specific optional "exp_in" attr.
+	const int expInValue = extractJsonOptionalValue<int>(jwt, "exp_in", -1, &error);
+	if (error)
+		return false;
+	if (expInValue != -1) {
+		json_int_t iatValue = extractJsonValue<int>(jwt, "iat", &error);
+		if (error) {
+			cerr << "`exp_in` can be used only if `iat` exists." << endl;
+			return false;
+		}
+
+		if (time_t(iatValue) + time_t(expInValue * 1000) < currentTime) {
+			cerr << "JWT (exp_in) has expired." << endl;
+			return false;
+		}
+	}
+
+	return true;
+}
+
 // =============================================================================
 // Plugin.
 // =============================================================================
 
-#include "module.hh"
 #include "plugin.hh"
 
 class JweAuth : public Module {
@@ -221,7 +302,7 @@ FLEXISIP_DECLARE_PLUGIN(JweAuth, JweAuthPluginName, JweAuthPluginVersion);
 
 // -----------------------------------------------------------------------------
 
-static bool checkJwtAttr(
+static bool checkJwtAttrFromSipHeader(
 	json_t *jwt,
 	const shared_ptr<const SipEvent> &ev,
 	const char *attrName,
@@ -271,21 +352,25 @@ json_t *JweAuth::decryptJwe(const string &text) const {
 };
 
 bool JweAuth::checkJwt(json_t *jwt, const shared_ptr<const RequestSipEvent> &ev) const {
-	// 1. Find incoming subject.
-	sip_unknown_t *header = ModuleToolbox::getCustomHeaderByName(ev->getSip(), "X-ticked-oid");
+	// 1. Check expiration time.
+	if (!checkJwtTime(jwt))
+		return false;
+
+	// 2. Find incoming subject.
+	const sip_unknown_t *header = ModuleToolbox::getCustomHeaderByName(ev->getSip(), "X-ticked-oid");
 	if (!header || !header->un_value || !ev->findIncomingSubject(header->un_value)) {
 		cerr << "Unable to find oid incoming subject in message." << endl;
 		return false;
 	}
 
-	// 2. Check attributes.
+	// 3. Check attributes.
 	static constexpr auto toCheck = {
 		pair<const char *, const char *>{ "oid", "X-ticked-oid" },
 		{ "aud", "X-ticked-aud" },
 		{ "req_act", "X-ticked-req_act" }
 	};
 	for (const auto &data : toCheck) {
-		if (!checkJwtAttr(jwt, ev, data.first, data.second))
+		if (!checkJwtAttrFromSipHeader(jwt, ev, data.first, data.second))
 			return false;
 	}
 
