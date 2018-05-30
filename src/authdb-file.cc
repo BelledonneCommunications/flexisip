aaae90d8c9bdae4076d22f8ed48c69068ae7ac44
Danmei Chen <danmei.chen@belledonne-communications.com>
2018-05-30 13:19:50 +0200

    fix unintialized username

--- src/authdb-file.cc
+++ src/authdb-file.cc
@@ -196,7 +196,7 @@ void FileAuthDb::sync() {
 					}
 					
 					// if user with space, replace %20 by space
-					string user_ref;
+					string user_ref=user;
 					user_ref.reserve(user.size());
 					url_unescape(&user_ref[0], user.c_str());
 					if (!ss.eof()) {
