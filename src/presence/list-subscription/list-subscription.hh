5ef108dd274dc766b928e38c5ff935fcee93e271
Benjamin Reis <benjamin.reis@belledonne-communications.com>
2018-11-07 11:00:18 +0100

    fix silly mistake

--- src/presence/list-subscription/list-subscription.hh
+++ src/presence/list-subscription/list-subscription.hh
@@ -92,7 +92,7 @@ protected:
 	 * that corresponds to the list.  Typically, this is the URI to which
 	 * the SUBSCRIBE request was sent.
 	 **/
-	const belle_sip_uri_t * = nullptr;;
+	const belle_sip_uri_t *mName = nullptr;;
 
 private:
 	ListSubscription(const ListSubscription &);
