d83418fce0657ca4cbe9741f47f68d0d175f6788
Erwan Croze <erwan.croze@belledonne-communications.com>
2018-11-16 14:51:27 +0100

    Fix binding of conference server

--- src/conference/conference-server.cc
+++ src/conference/conference-server.cc
@@ -205,11 +205,10 @@ void flexisip::ConferenceServer::bindConference() {
 	shared_ptr<FakeListener> listener = make_shared<FakeListener>();
 	auto config = GenericManager::get()->getRoot()->get<GenericStruct>("conference-server");
 	if (config && config->get<ConfigBoolean>("enabled")->read()) {
-		SofiaAutoHome home;
 		BindingParameters parameter;
 
-		sip_contact_t* sipContact = sip_contact_create(home.home(), reinterpret_cast<url_string_t*>(url_make(home.home(), mTransport.c_str())), nullptr);
-		url_t *from = url_make(home.home(), config->get<ConfigString>("conference-factory-uri")->read().c_str());
+		sip_contact_t* sipContact = sip_contact_make(mHome.home(), mTransport.c_str());
+		url_t *from = url_make(mHome.home(), config->get<ConfigString>("conference-factory-uri")->read().c_str());
 
 		parameter.callId = "CONFERENCE";
 		parameter.path = mPath;
@@ -232,12 +231,12 @@ void ConferenceServer::bindChatRoom (
 	const string &gruu,
 	const shared_ptr<ContactUpdateListener> &listener
 ) {
-	SofiaAutoHome home;
 	BindingParameters parameter;
 
-	sip_contact_t* sipContact = sip_contact_create(home.home(), reinterpret_cast<url_string_t*>(url_make(home.home(), contact.c_str())), ("+sip.instance=\"<" + gruu + ">\"").c_str());
-	url_t *from = url_make(home.home(), bindingUrl.c_str());
-	url_param_add(home.home(), from, ("gr=" + gruu).c_str());
+	sip_contact_t* sipContact = sip_contact_make(mHome.home(), contact.c_str());
+	sip_contact_add_param(mHome.home(), sipContact, su_strdup(mHome.home(), ("+sip.instance=\"<" + gruu + ">\"").c_str()));
+	url_t *from = url_make(mHome.home(), bindingUrl.c_str());
+	url_param_add(mHome.home(), from, ("gr=" + gruu).c_str());
 
 	parameter.callId = gruu;
 	parameter.path = mPath;
