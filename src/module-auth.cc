bc8a3b6bbce9aacd2d9a2a937605dab920294ca6
François Grisez <francois.grisez@belledonne-communications.com>
2018-11-28 16:41:32 +0100

    Fix several invalid reads

--- src/module-auth.cc
+++ src/module-auth.cc
@@ -1067,7 +1067,8 @@ int Authentication::AuthenticationListener::checkPasswordMd5(const char *passwd)
 	if (passwd) {
 		mPasswordFound = true;
 		++*getModule()->mCountPassFound;
-		strncpy(a1buf, passwd, sizeof(a1buf) - 1); // remove trailing NULL character
+		strncpy(a1buf, passwd, sizeof(a1buf)-1);
+		a1buf[sizeof(a1buf)-1] = '\0'; // ensure that the string is null terminated
 		a1 = a1buf;
 	} else {
 		++*getModule()->mCountPassNotFound;
@@ -1094,7 +1095,8 @@ int Authentication::AuthenticationListener::checkPasswordForAlgorithm(const char
 		if (passwd) {
 			mPasswordFound = true;
 			++*getModule()->mCountPassFound;
-			strncpy(a1, passwd, sizeof(a1) - 1); // remove trailing NULL character
+			strncpy(a1, passwd, sizeof(a1)-1);
+			a1[sizeof(a1)-1] = '\0'; // ensure that the string is null terminated
 		} else {
 			++*getModule()->mCountPassNotFound;
 			auth_digest_a1_for_algorithm(&mAr, "xyzzy", a1);
