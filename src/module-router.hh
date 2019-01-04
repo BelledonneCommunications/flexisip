6cf588a52178ba89645af5c66d137701d53c35ce
Sylvain Berfini <sylvain.berfini@belledonne-communications.com>
2019-01-04 11:15:15 +0100

    Added a header for module::Router

---
+++ src/module-router.hh
@@ -0,0 +1,111 @@
+/*
+	Flexisip, a flexible SIP proxy server with media capabilities.
+	Copyright (C) 2019  Belledonne Communications SARL.
+
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#ifndef MODULE_ROUTER_HH_
+#define MODULE_ROUTER_HH_
+
+#include "module.hh"
+#include "agent.hh"
+#include "registrardb.hh"
+#include "forkcallcontext.hh"
+#include "forkmessagecontext.hh"
+#include "forkbasiccontext.hh"
+
+struct RouterStats {
+	std::unique_ptr<StatPair> mCountForks;
+	std::unique_ptr<StatPair> mCountForkTransactions;
+	StatCounter64 *mCountNonForks;
+	StatCounter64 *mCountLocalActives;
+};
+
+class ModuleRouter : public Module, public ModuleToolbox, public ForkContextListener {
+	RouterStats mStats;
+	bool rewriteContactUrl(const std::shared_ptr<MsgSip> &ms, const url_t *ct_url, const char *route);
+
+  public:
+	ModuleRouter(Agent *ag) : Module(ag) {
+	}
+
+	~ModuleRouter() {
+	}
+
+	virtual void onDeclare(GenericStruct *mc);
+
+	virtual void onLoad(const GenericStruct *mc);
+
+	virtual void onUnload() {}
+
+	virtual void onRequest(std::shared_ptr<RequestSipEvent> &ev);
+
+	virtual void onResponse(std::shared_ptr<ResponseSipEvent> &ev);
+
+	virtual void onForkContextFinished(std::shared_ptr<ForkContext> ctx) override;
+
+	void extractContactByUniqueId(std::string uid);
+	void sendReply(std::shared_ptr<RequestSipEvent> &ev, int code, const char *reason, int warn_code = 0, const char *warning = nullptr);
+	void routeRequest(std::shared_ptr<RequestSipEvent> &ev, Record *aor, const url_t *sipUri);
+	void onContactRegistered(const std::string &uid, Record *aor, const url_t *sipUri);
+
+	const std::string &getFallbackRoute() const {
+		return mFallbackRoute;
+	}
+
+	bool isFallbackToParentDomainEnabled() const {
+		return mFallbackParentDomain;
+	}
+
+	bool isDomainRegistrationAllowed() const {
+		return mAllowDomainRegistrations;
+	}
+
+	bool isManagedDomain(const url_t *url) {
+		return ModuleToolbox::isManagedDomain(getAgent(), mDomains, url);
+	}
+
+  protected:
+	bool makeGeneratedContactRoute(std::shared_ptr<RequestSipEvent> &ev, Record *aor,
+								   std::list<std::shared_ptr<ExtendedContact>> &ec_list);
+	virtual bool dispatch(const std::shared_ptr<RequestSipEvent> &ev, const std::shared_ptr<ExtendedContact> &contact,
+				  std::shared_ptr<ForkContext> context, const std::string &targetUris);
+	std::string routingKey(const url_t *sipUri);
+
+	std::list<std::string> mDomains;
+	bool mFork;
+	std::shared_ptr<ForkContextConfig> mForkCfg;
+	std::shared_ptr<ForkContextConfig> mMessageForkCfg;
+	std::shared_ptr<ForkContextConfig> mOtherForkCfg;
+	typedef std::multimap<std::string, std::shared_ptr<ForkContext>> ForkMap;
+	ForkMap mForks;
+	std::string mGeneratedContactRoute;
+	std::string mExpectedRealm;
+	bool mUseGlobalDomain;
+	bool mStateful;
+
+	bool mGenerateContactEvenOnFilledAor;
+	bool mAllowDomainRegistrations;
+	bool mAllowTargetFactorization;
+	std::string mPreroute;
+	bool mResolveRoutes;
+	std::string mFallbackRoute;
+	bool mFallbackParentDomain;
+
+  private:
+	static ModuleInfo<ModuleRouter> sInfo;
+};
+
+#endif /* MODULE_ROUTER_HH_ */
