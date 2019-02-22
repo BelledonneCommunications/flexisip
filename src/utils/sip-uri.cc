8603e7aed4495f759e5852bfe6eefd8f9e77ee1b
François Grisez <francois.grisez@belledonne-communications.com>
2019-02-22 14:28:09 +0100

    Add extra check while creating a SipUri object
    * check that scheme matches 'sip' or 'sips'.
    * check that the URI has a host part.

--- src/utils/sip-uri.cc
+++ src/utils/sip-uri.cc
@@ -1,3 +1,4 @@
+#include <cstring>
 #include <sstream>
 #include <stdexcept>
 
@@ -8,13 +9,22 @@ using namespace std;
 namespace flexisip {
 
 SipUri::SipUri(const std::string &str) {
-	su_home_init(&_home);
-	_url = url_make(&_home, str.c_str());
-	if (_url == nullptr) {
+	try {
+		su_home_init(&_home);
+		_url = url_make(&_home, str.c_str());
+		if (_url == nullptr) throw invalid_argument("parsing failed");
+		if (_url->url_scheme == nullptr) throw invalid_argument("no scheme found");
+		if (strcmp(_url->url_scheme, "sip") != 0 && strcmp(_url->url_scheme, "sips") != 0) {
+			ostringstream os;
+			os << "invalid scheme (" << _url->url_scheme << ")";
+			throw invalid_argument(os.str());
+		}
+		if (_url->url_host == nullptr || _url->url_host[0] == '\0') {
+			throw invalid_argument("no host found");
+		}
+	} catch (...) {
 		su_home_deinit(&_home);
-		ostringstream os;
-		os << _urlAsStr << " is not a valid SIP URI";
-		throw invalid_argument(os.str());
+		throw;
 	}
 }
 
