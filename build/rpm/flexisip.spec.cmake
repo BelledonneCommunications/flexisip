c7f5308df4ac3bcb40745ac5f558cac3033d3752
François Grisez <francois.grisez@belledonne-communications.com>
2019-05-24 10:51:30 +0200

    Make RPM package do not replace logrotate script while update

--- build/rpm/flexisip.spec.cmake
+++ build/rpm/flexisip.spec.cmake
@@ -190,7 +190,7 @@ rm -rf $RPM_BUILD_ROOT
 %endif
 
 %{_sysconfdir}/flexisip
-%config %{_sysconfdir}/logrotate.d/flexisip-logrotate
+%config(noreplace) %{_sysconfdir}/logrotate.d/flexisip-logrotate
 
 /lib/systemd/system/flexisip.service
 /lib/systemd/system/flexisip@.service
