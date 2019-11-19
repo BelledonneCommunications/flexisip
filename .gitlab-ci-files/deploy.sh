a7d81878458bde0372f6219206b6427d4f3c0579
François Grisez <francois.grisez@belledonne-communications.com>
2019-11-19 17:16:26 +0100

    Make gitlab-ci to call depoly.sh

--- .gitlab-ci-files/deploy.sh
+++ .gitlab-ci-files/deploy.sh
@@ -7,22 +7,21 @@ if [ $# -ne 1 ]; then
 fi
 
 id=$(cat /dev/urandom | tr -dc '[:alnum:]' | fold -w 10 | head -n 1)
-tmpdir="$CENTOS7_MAKE_REPO_TMP/tmp-$id"
+tmpdir="$MAKE_REPO_TMP/tmp-$id"
 rsync_dest="$DEPLOY_SERVER:$tmpdir/"
 
 dist=$1
 case "$dist" in
 	'centos')
-		make_repo_args="rpm $tmpdir $CENTOS7_REPOSITORY"
+		make_repo_args="rpm $tmpdir $CENTOS_REPOSITORY"
 		rsync_src='WORK/flexisip-rpm/rpmbuild/RPMS/x86_64/'
 		legacy_repo_cmd="cp -v --link --no-clobber --preserve=timestamp $tmpdir/* $CENTOS7_DEPLOY_DIRECTORY && createrepo_c --update $CENTOS7_DEPLOY_DIRECTORY"
 		;;
 	'debian')
-		make_repo_args="deb $tmpdir $DEBIAN_FREIGHT_CONF_PATH $RELEASE"
+		make_repo_args="deb $tmpdir $FREIGHT_PATH $RELEASE"
 		echo "make_repo_args=$make_repo_args"
 		rsync_src='WORK/flexisip-rpm/rpmbuild/DEBS/'
-		freight_opts="--conf=$DEBIAN_FREIGHT_CONF_PATH"
-		legacy_repo_cmd="freight add $freight_opts $tmpdir/*.deb apt/$RELEASE && freight cache $freight_opts apt/$RELEASE"
+		legacy_repo_cmd="freight add --conf=$FREIGHT_PATH $tmpdir/*.deb apt/$RELEASE && freight cache $freight_opts apt/$RELEASE"
 		;;
 	       *)
 		echo "invalid distribution type: '$dist'. Only 'centos' and 'debian' are valid" 1>&2
