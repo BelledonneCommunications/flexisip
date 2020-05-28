cd783d73507392eb38e3de85b638a994903cc6ed
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2020-05-28 18:28:44 +0200

    First changes for registration-events

--- tester/tester.hh
+++ tester/tester.hh
@@ -10,7 +10,7 @@
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
  * GNU General Public License for more details.
- * 
+ *
  * You should have received a copy of the GNU General Public License
  * along with this program; if not, write to the Free Software
  * Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
@@ -39,6 +39,7 @@ extern "C" {
 #endif
 
 extern test_suite_t boolean_expressions_suite;
+extern test_suite_t registration_event_suite;
 
 
 void flexisip_tester_init(void(*ftester_printf)(int level, const char *fmt, va_list args));
