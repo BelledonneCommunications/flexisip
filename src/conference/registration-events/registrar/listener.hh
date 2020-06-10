c7d62b9889f3bcc130afb37f8be0fad7f56f459d
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2020-06-10 18:00:10 +0200

    Refactor and reorganize the classes Link RegistrationEvent with the Conference

---
+++ src/conference/registration-events/registrar/listener.hh
@@ -0,0 +1,24 @@
+#pragma once
+
+#include <linphone++/linphone.hh>
+#include <flexisip/registrardb.hh>
+
+using namespace std;
+using namespace linphone;
+using namespace flexisip;
+
+namespace RegistrationEvent::Registrar {
+    class Listener : public ContactRegisteredListener, public ContactUpdateListener {
+        public:
+            Listener(const shared_ptr<linphone::Event> &lev);
+            void onRecordFound(const shared_ptr<Record> &r) override;
+            void onError() override {}
+            void onInvalid() override {}
+            void onContactRegistered(const shared_ptr<Record> &r, const string &uid) override;
+            void onContactUpdated(const shared_ptr<ExtendedContact> &ec) override {}
+        private:
+            const shared_ptr<linphone::Event> event;
+            void processRecord(const shared_ptr<Record> &r);
+            string getDeviceName(const shared_ptr<ExtendedContact> &ec);
+    };
+}
