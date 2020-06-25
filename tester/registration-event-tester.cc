6b3c085e4a9b2a1cc901bd0cc18e7f5fad2926de
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2020-06-25 16:55:25 +0200

    Add a registration for the client

--- tester/registration-event-tester.cc
+++ tester/registration-event-tester.cc
@@ -33,6 +33,15 @@ using namespace linphone;
 using namespace flexisip;
 
 static void basic() {
+	// Agent initialisation
+
+	su_root_t *root = su_root_create(NULL);
+	shared_ptr<Agent> a = make_shared<Agent>(root);
+	Agent *agent = a->getAgent();
+
+	GenericManager *cfg = GenericManager::get();
+	cfg->load("/flexisip.conf");
+	agent->loadConfig(cfg);
 
 	// Client initialisation
 
@@ -41,19 +50,18 @@ static void basic() {
 	shared_ptr<Transports> transport = Factory::get()->createTransports();
 	transport->setTcpPort(rand() %0x0FFF + 1014);
 	clientCore->setTransports(transport);
+	clientCore->start();
 
-	auto me = Factory::get()->createAddress(clientCore->getIdentity());
-	me->setUriParam("gr", "abcd");
-	me->setPort(6064);
+	auto me = Factory::get()->createAddress("sip:test@sip.example.org");
 
 	shared_ptr<ProxyConfig> proxy = clientCore->createProxyConfig();
 	proxy->setIdentityAddress(me);
 	proxy->enableRegister(true);
-	proxy->setConferenceFactoryUri("sip:focus@127.0.0.1:6064;transport=tcp");
-	proxy->setServerAddr("sip:127.0.0.1:6064;transport=tcp");
+	proxy->setConferenceFactoryUri("sip:focus@sip.example.org");
+	proxy->setServerAddr("sip:127.0.0.1:5060;transport=tcp");
+	proxy->setRoute("sip:127.0.0.1:5060;transport=tcp");
 	clientCore->addProxyConfig(proxy);
 	clientCore->setDefaultProxyConfig(proxy);
-	clientCore->setPrimaryContact("sip:edhelas@127.0.0.1:48888;transport=tcp;gr=1234");
 
 	// RegEvent Server
 
@@ -65,30 +73,43 @@ static void basic() {
 	int regEventPort = rand() %0x0FFF + 1014;
 	regEventTransport->setTcpPort(regEventPort);
 	regEventCore->setTransports(regEventTransport);
-
-	// Agent initialisation
-
-	su_root_t *root = su_root_create(NULL);
-	shared_ptr<Agent> a = make_shared<Agent>(root);
-	Agent *agent = a->getAgent();
-
-	GenericManager *cfg = GenericManager::get();
-	cfg->load("/flexisip.conf");
-	agent->loadConfig(cfg);
+	regEventCore->addListener(make_shared<RegistrationEvent::Server>());
+	regEventCore->start();
 
 	// Conference Server
 
 	GenericStruct *gs = GenericManager::get()->getRoot()->get<GenericStruct>("conference-server");
 	gs->get<ConfigString>("database-backend")->set("sqlite");
 	gs->get<ConfigString>("database-connection-string")->set(":memory:");
-	gs->get<ConfigString>("outbound-proxy")->set("");
+	gs->get<ConfigString>("outbound-proxy")->set("sip:127.0.0.1:5060;transport=tcp");
 	gs->get<ConfigString>("transport")->set("sip:127.0.0.1:6064;transport=tcp");
-	gs->get<ConfigString>("conference-factory-uri")->set("sip:focus@127.0.0.1:6064;transport=tcp");
+	gs->get<ConfigString>("conference-factory-uri")->set("sip:focus@sip.example.org");
+
 	// Registrars / Local confs
-	gs->get<ConfigString>("local-domains")->set("127.0.0.1 [2a01:e0a:1ce:c860:f03d:d06:649f:6cfc]");
+	gs->get<ConfigString>("local-domains")->set("sip.example.org 127.0.0.1 [2a01:e0a:1ce:c860:f03d:d06:649f:6cfc]");
 
 	auto conferenceServer = make_shared<ConferenceServer>(a->getPreferredRoute(), root);
 	conferenceServer->init();
+
+	// Proxy configuration
+
+	GenericStruct *global = GenericManager::get()->getRoot()->get<GenericStruct>("global");
+	global->get<ConfigStringList>("transports")->set("sip:127.0.0.1:5060;transport=tcp");
+
+	// Configure module regevent
+
+	GenericStruct *registrarConf = GenericManager::get()->getRoot()->get<GenericStruct>("module::Registrar");
+	registrarConf->get<ConfigStringList>("reg-domains")->set("sip.example.org");
+
+	GenericStruct *regEventConf = GenericManager::get()->getRoot()->get<GenericStruct>("module::RegEvent");
+	regEventConf->get<ConfigString>("regevent-server")->set(string("sip:127.0.0.1:").append(to_string(regEventPort)).append(";transport=tcp"));
+
+	agent->start("", "");
+
+	while (proxy->getState() != RegistrationState::Ok) {
+		clientCore->iterate();
+		su_root_step(a->getRoot(), 100);
+	}
 
 	// Fill the RegistrarDB
 
@@ -111,12 +132,14 @@ static void basic() {
 	parameter.globalExpire = 1000;
 	parameter.callId = "123456789";
 	parameter.userAgent = "Linphone2 (Debian) LinphoneCore";
+	parameter.withGruu = true;
 
 	RegistrarDb::get()->bind(
 		url_make(home.home(), participantFrom.c_str()),
 		sip_contact_create(
 			home.home(),
-			(url_string_t *)participantFrom.append(";gr=abcde").c_str(),
+			(url_string_t *)participantFrom.c_str(),
+			string("+sip.instance=\"<1234>\"").c_str(),
 			nullptr
 		),
 		parameter,
@@ -127,15 +150,35 @@ static void basic() {
 	parameter2.globalExpire = 1000;
 	parameter2.callId = "1234567890";
 	parameter2.userAgent = "Linphone3 (Debian) LinphoneCore";
+	parameter2.withGruu = true;
 
 	RegistrarDb::get()->bind(
-		url_make(home.home(), participantFrom.c_str()),
+		url_make(home.home(), otherParticipantFrom.c_str()),
 		sip_contact_create(
 			home.home(),
-			(url_string_t *)participantFrom.append(";gr=fghijk").c_str(),
+			(url_string_t *)otherParticipantFrom.c_str(),
+			string("+sip.instance=\"<1234>\"").c_str(),
 			nullptr
 		),
 		parameter2,
+		make_shared<BindListener>()
+	);
+
+	BindingParameters parameter3;
+	parameter3.globalExpire = 1000;
+	parameter3.callId = "1234567890";
+	parameter3.userAgent = "Linphone4 (Debian) LinphoneCore";
+	parameter3.withGruu = true;
+
+	RegistrarDb::get()->bind(
+		url_make(home.home(), otherParticipantFrom.c_str()),
+		sip_contact_create(
+			home.home(),
+			(url_string_t *)otherParticipantFrom.c_str(),
+			string("+sip.instance=\"<3456>\"").c_str(),
+			nullptr
+		),
+		parameter3,
 		make_shared<BindListener>()
 	);
 
@@ -143,24 +186,11 @@ static void basic() {
 	participants.push_back(Factory::get()->createAddress(participantFrom));
 	participants.push_back(Factory::get()->createAddress(otherParticipantFrom));
 
-	regEventCore->addListener(make_shared<RegistrationEvent::Server>());
-
-	regEventCore->start();
-	clientCore->start();
-
 	auto chatRoomParams = clientCore->createDefaultChatRoomParams();
 	chatRoomParams->enableGroup(true);
-	auto chatRoom = clientCore->createChatRoom(chatRoomParams, me, "Chatroom with remote", participants);
+	auto chatRoom = clientCore->createChatRoom(chatRoomParams, proxy->getContact(), "Chatroom with remote", participants);
 
-	cout << "================= GNAP =================" << endl;
-
-	/*shared_ptr<RegistrationEvent::Client> client = make_shared<RegistrationEvent::Client>(
-		chatRoom,
-		const_pointer_cast<const Address>(Factory::get()->createAddress(from))
-	);
-	client->subscribe();*/
-
-	chatRoom->addParticipant(Factory::get()->createAddress(participantFrom));
+	//chatRoom->addParticipant(Factory::get()->createAddress(participantFrom));
 
 	for (shared_ptr<Participant> participant : chatRoom->getParticipants()){
 		cout << "HOY HOY PARTICIPANT " << participant->getAddress()->asString() << endl;
@@ -170,7 +200,8 @@ static void basic() {
 		}
 	}
 
-	while (1/*!client->notifyReceived*/) {
+	while (/*!client->notifyReceived*/1) {
+		//cout << "HOY HOY RECEIVED " << client->notifyReceived << endl;
 		clientCore->iterate();
 		regEventCore->iterate();
 		su_root_step(a->getRoot(), 100);
