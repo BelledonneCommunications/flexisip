87ceb9340718a6a94ab7d5c058f332c85f213625
François Grisez <francois.grisez@belledonne-communications.com>
2020-06-29 16:13:21 +0200

    Make the presence server to not abort when 'long-term-enabled' isn't set but no request has been set for fetching users by their phone number.
    New logic:
    	* if 'long-term-enabled' hasn't been set but the requests for the users database
          are set, then abort.
    	* if 'long-term-enabled' is explicitly set to 'false', then never abort.
    	* if 'long-term-enabled' is explicitly set to 'true', abort when the requests
          for the users database aren't properly set.
    
    That has the advantage to warn by a fatal the old users that use
    long-term presence but have never set 'long-term-enabled' explicitly,
    as it was 'true' by default before Flexisip 2.0.0 ; but avoiding
    new user to set exlicitly 'long-term-enabled' as they have to before
    this fix.

--- src/presence/presence-server.cc
+++ src/presence/presence-server.cc
@@ -204,17 +204,30 @@ PresenceServer::~PresenceServer(){
 void PresenceServer::_init() {
 	if (!mEnabled) return;
 	const auto *cr = GenericManager::get()->getRoot();
-	const auto &get_users_with_phones_request = cr->get<GenericStruct>("module::Authentication")->get<ConfigString>("soci-users-with-phones-request")->read();
-	const auto &db_implementation = cr->get<GenericStruct>("module::Authentication")->get<ConfigString>("db-implementation")->read();
 
 	const auto *longTermEnabledConfig = cr->get<GenericStruct>("presence-server")->get<ConfigBoolean>("long-term-enabled");
-	if (longTermEnabledConfig->isDefault()) {
-		LOGF("The default value of '%s' parameter has changed since Flexisip 2.0.0. "
-			"Please set it explicitly to remove this error.", longTermEnabledConfig->getCompleteName().c_str());
-	}
 	auto longTermEnabled = longTermEnabledConfig->read();
-	if (longTermEnabled && get_users_with_phones_request.empty() && db_implementation != "file") {
-		LOGF("Unable to start presence server: soci-users-with-phones-request is not precised in flexisip.conf, please fix it.");
+
+	const auto &dbImplementation = cr->get<GenericStruct>("module::Authentication")->get<ConfigString>("db-implementation")->read();
+	const auto *getUsersWithPhonesRequestParam = cr->get<GenericStruct>("module::Authentication")->get<ConfigString>("soci-users-with-phones-request");
+	const auto *getUserWithPhoneRequestParam = cr->get<GenericStruct>("module::Authentication")->get<ConfigString>("soci-user-with-phone-request");
+	const auto &getUsersWithPhonesRequest = getUsersWithPhonesRequestParam->read();
+	const auto &getUserWithPhoneRequest = getUserWithPhoneRequestParam->read();
+	auto userDbIsProperlySet = dbImplementation == "file" || !getUsersWithPhonesRequest.empty() || !getUserWithPhoneRequest.empty();
+
+	if (longTermEnabledConfig->isDefault() && !longTermEnabled && userDbIsProperlySet) {
+		LOGF("The default value of '%s' parameter has changed since Flexisip 2.0.0. "
+			"Please set this parameter explicitly or unset '%s' and '%s' to remove this error.",
+			longTermEnabledConfig->getName().c_str(),
+			getUserWithPhoneRequestParam->getCompleteName().c_str(),
+			getUsersWithPhonesRequestParam->getCompleteName().c_str()
+		);
+	}
+	if (longTermEnabled && !userDbIsProperlySet) {
+		LOGF("Unable to start presence server: neither '%s' or '%s' is set whereas 'Long-term Presence' is required.",
+			 getUserWithPhoneRequestParam->getCompleteName().c_str(),
+			 getUsersWithPhonesRequestParam->getCompleteName().c_str()
+		);
 	}
 
 	auto transports = cr->get<GenericStruct>("presence-server")->get<ConfigStringList>("transports")->read();
