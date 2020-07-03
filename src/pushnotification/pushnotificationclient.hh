e9023915d3a22f4635d924564ac1def0f0ac3b87
François Grisez <francois.grisez@belledonne-communications.com>
2020-07-03 18:48:33 +0200

    [ci skip] Rework PushNotification client to have several transport implementations

--- src/pushnotification/pushnotificationclient.hh
+++ src/pushnotification/pushnotificationclient.hh
@@ -33,11 +33,53 @@ namespace flexisip {
 
 class PushNotificationService;
 
+class PushNotificationTransport {
+public:
+	using OnSuccessCb = std::function<void(PushNotificationRequest &)>;
+	using OnErrorCb = std::function<void(PushNotificationRequest &, const std::string &)>;
+
+	virtual ~PushNotificationTransport() = default;
+
+	/**
+	* @return
+	* 	 0: success
+	*	-1: failure
+	*	-2: failure due to stale socket. You may try to send the push again.
+	*/
+	virtual int sendPush(PushNotificationRequest &req, bool hurryUp, const OnSuccessCb &onSuccess, const OnErrorCb &onError) = 0;
+};
+
+class PushNotificationTransportTls : public PushNotificationTransport {
+public:
+	struct SSLCtxDeleter {
+		void operator()(SSL_CTX *ssl) {SSL_CTX_free(ssl);}
+	};
+	using SSLCtxUniquePtr = std::unique_ptr<SSL_CTX, SSLCtxDeleter>;
+
+	PushNotificationTransportTls(SSLCtxUniquePtr &&ctx, const std::string &host, const std::string &port, bool isSecure);
+	~PushNotificationTransportTls() override = default;
+
+	int sendPush(PushNotificationRequest &req, bool hurryUp, const OnSuccessCb &onSuccess, const OnErrorCb &onError) override;
+
+private:
+	struct BIODeleter {
+		void operator()(BIO *bio) {BIO_free_all(bio);}
+	};
+	using BIOUniquePtr = std::unique_ptr<BIO, BIODeleter>;
+
+	void recreateConnection();
+
+	BIOUniquePtr mBio{nullptr};
+	SSLCtxUniquePtr mCtx{nullptr};
+	std::string mHost{}, mPort{};
+	time_t mLastUse{0};
+	bool mIsSecure{false};
+};
+
 class PushNotificationClient {
 	public:
-		PushNotificationClient(const std::string &name, const PushNotificationService &service,
-			 				   SSL_CTX *ctx, const std::string &host, const std::string &port,
-							   unsigned maxQueueSize, bool isSecure);
+		PushNotificationClient(std::unique_ptr<PushNotificationTransport> &&transport,
+							   const std::string &name, const PushNotificationService &service, unsigned maxQueueSize);
 		virtual ~PushNotificationClient();
 
 		virtual bool sendPush(const std::shared_ptr<PushNotificationRequest> &req);
@@ -46,26 +88,14 @@ class PushNotificationClient {
 
 	protected:
 		void run();
-		/**
-		 * @return
-		 * 	+  0: success
-		 *	+ -1: failure
-		 *	+ -2: failure due to stale socket. You may try to send the push again.
-		 */
-		int sendPushToServer(const std::shared_ptr<PushNotificationRequest> &req, bool hurryUp);
-		void recreateConnection();
 		void onError(PushNotificationRequest &req, const std::string &msg);
 		void onSuccess(PushNotificationRequest &req);
 
-		const PushNotificationService &mService;
-		BIO * mBio{nullptr};
-		SSL_CTX * mCtx{nullptr};
-		std::queue<std::shared_ptr<PushNotificationRequest>> mRequestQueue{};
 		std::string mName{};
-		std::string mHost{}, mPort{};
+		const PushNotificationService &mService;
+		std::unique_ptr<PushNotificationTransport> mTransport{};
+		std::queue<std::shared_ptr<PushNotificationRequest>> mRequestQueue{};
 		unsigned mMaxQueueSize{0};
-		time_t mLastUse{0};
-		bool mIsSecure{false};
 
 	private:
 		std::thread mThread{};
