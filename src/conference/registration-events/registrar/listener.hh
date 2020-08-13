1450243b144219508ce3f11057d521a2ce32f0f6
Timothée Jaussoin <timothee.jaussoin@belledonne-communications.com>
2020-08-13 15:42:17 +0200

    Complete the test to handle the conference local-domains configuration Fix a segfault

--- src/conference/registration-events/registrar/listener.hh
+++ src/conference/registration-events/registrar/listener.hh
@@ -7,20 +7,22 @@ using namespace std;
 using namespace linphone;
 using namespace flexisip;
 
-namespace RegistrationEvent::Registrar {
-    class Listener : public ContactRegisteredListener, public ContactUpdateListener {
-        public:
-            Listener(const shared_ptr<linphone::Event> &lev);
-            void onRecordFound(const shared_ptr<Record> &r) override;
-            void onError() override {}
-            void onInvalid() override {}
-            void onContactRegistered(const shared_ptr<Record> &r, const string &uid) override;
-            void onContactUpdated(const shared_ptr<ExtendedContact> &ec) override {}
-        private:
-            const shared_ptr<linphone::Event> event;
-            void processRecord(const shared_ptr<Record> &r);
-            string getDeviceName(const shared_ptr<ExtendedContact> &ec);
+namespace RegistrationEvent {
+    namespace Registrar {
+        class Listener : public ContactRegisteredListener, public ContactUpdateListener {
+            public:
+                Listener(const shared_ptr<linphone::Event> &lev);
+                void onRecordFound(const shared_ptr<Record> &r) override;
+                void onError() override {}
+                void onInvalid() override {}
+                void onContactRegistered(const shared_ptr<Record> &r, const string &uid) override;
+                void onContactUpdated(const shared_ptr<ExtendedContact> &ec) override {}
+            private:
+                const shared_ptr<linphone::Event> event;
+                void processRecord(const shared_ptr<Record> &r);
+                string getDeviceName(const shared_ptr<ExtendedContact> &ec);
 
-            // version, previouscontacts
-    };
+                // version, previouscontacts
+        };
+    }
 }
