f004e7b0accf8724f1320aee6ff03a9a3091a643
François Grisez <francois.grisez@belledonne-communications.com>
2020-10-02 16:52:34 +0200

    Embed libxsd headers inside the source code of Flexisip

--- src/registration-events/client.cpp
+++ src/registration-events/client.cpp
@@ -1,12 +1,31 @@
-#include "client.hh"
-#include "reginfo.hh"
-#include "utils.hh"
-#include "../conference/conference-server.hh"
+/*
+	Flexisip, a flexible SIP proxy server with media capabilities.
+	Copyright (C) 2010-2020  Belledonne Communications SARL, All rights reserved.
 
-#include <linphone++/linphone.hh>
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
+
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
+
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #include <iostream>
 #include <sstream>
+
+#include <linphone++/linphone.hh>
+
+#include "conference/conference-server.hh"
+#include "utils.hh"
+#include "xml/reginfo.hh"
+
+#include "client.hh"
 
 using namespace std;
 using namespace linphone;
