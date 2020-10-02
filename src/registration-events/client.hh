6a425623c5a353e6eabcb2b5deca7108ac604ef7
François Grisez <francois.grisez@belledonne-communications.com>
2020-10-02 16:52:34 +0200

    Embed libxsd headers inside the source code of Flexisip

--- src/registration-events/client.hh
+++ src/registration-events/client.hh
@@ -1,25 +1,28 @@
 /*
- Flexisip, a flexible SIP proxy server with media capabilities.
- Copyright (C) 2018 Belledonne Communications SARL.
+	Flexisip, a flexible SIP proxy server with media capabilities.
+	Copyright (C) 2010-2020  Belledonne Communications SARL, All rights reserved.
 
- This program is free software: you can redistribute it and/or modify
- it under the terms of the GNU Affero General Public License as
- published by the Free Software Foundation, either version 3 of the
- License, or (at your option) any later version.
+	This program is free software: you can redistribute it and/or modify
+	it under the terms of the GNU Affero General Public License as
+	published by the Free Software Foundation, either version 3 of the
+	License, or (at your option) any later version.
 
- This program is distributed in the hope that it will be useful,
- but WITHOUT ANY WARRANTY; without even the implied warranty of
- MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- GNU Affero General Public License for more details.
+	This program is distributed in the hope that it will be useful,
+	but WITHOUT ANY WARRANTY; without even the implied warranty of
+	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+	GNU Affero General Public License for more details.
 
- You should have received a copy of the GNU Affero General Public License
- along with this program.  If not, see <http://www.gnu.org/licenses/>.
- */
+	You should have received a copy of the GNU Affero General Public License
+	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
 
 #pragma once
 
-#include <linphone++/linphone.hh>
 #include <iostream>
+
+#include <linphone++/linphone.hh>
+
+#include "conference/conference-server.hh"
 
 using namespace std;
 using namespace linphone;
@@ -57,6 +60,7 @@ class Client : public enable_shared_from_this<Client>, public CoreListener {
 		ClientListener *mListener = nullptr;
 };
 
-} //end of namespace RegistrationEvent
+} // end of namespace RegistrationEvent
 
-}// end of namespace Flexisip
+} // end of namespace Flexisip
+
