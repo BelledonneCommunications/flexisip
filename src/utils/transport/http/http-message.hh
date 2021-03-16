60de1f7d0192c42fa5d0b219bc01cb2d3a496d13
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2021-03-16 17:30:00 +0100

    HTTP2 client client for Firebase
     - Factorize a HTTP2 client out of Apple PNR client
     - Modification of Apple and Firebase client to use this new Http2Client
     - Http2Client now contains a by request timeout handling

---
+++ src/utils/transport/http/http-message.hh
@@ -0,0 +1,64 @@
+/*
+ Flexisip, a flexible SIP proxy server with media capabilities.
+ Copyright (C) 2010-2021  Belledonne Communications SARL, All rights reserved.
+
+ This program is free software: you can redistribute it and/or modify
+ it under the terms of the GNU Affero General Public License as
+ published by the Free Software Foundation, either version 3 of the
+ License, or (at your option) any later version.
+
+ This program is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU Affero General Public License for more details.
+
+ You should have received a copy of the GNU Affero General Public License
+ along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
+
+#pragma once
+
+#include <string>
+
+#include "http-headers.hh"
+
+namespace flexisip {
+
+class HttpMessage {
+  public:
+	HttpMessage() = default;
+	HttpMessage(const HttpHeaders& headers,const std::vector<char> body) : mHeaders(headers), mBody(body){};
+	virtual ~HttpMessage() = default;
+
+	const std::vector<char>& getBody() const{
+		return mBody;
+	}
+
+	void setBody(std::vector<char>& body) {
+		this->mBody = body;
+	}
+
+	void appendBody(std::string& body) {
+		this->mBody.insert(mBody.end(), body.begin(), body.end());
+	}
+
+	const HttpHeaders& getHeaders() const {
+		return mHeaders;
+	}
+
+	HttpHeaders& getHeaders() {
+		return mHeaders;
+	}
+
+	void setHeaders(const HttpHeaders& headers) {
+		this->mHeaders = headers;
+	}
+
+	std::string toString() const noexcept;
+
+  protected:
+	HttpHeaders mHeaders{};
+	std::vector<char> mBody{};
+};
+
+} // namespace flexisip
