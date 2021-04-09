d5d562b1b7901aacd7908754609cd7d22c3e1c6e
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2021-04-09 16:51:00 +0200

    HTTP2 client client for Firebase
     - Adding test for Firebase and Apple push
     - Minor correction

--- src/pushnotification/firebase/firebase-client.hh
+++ src/pushnotification/firebase/firebase-client.hh
@@ -1,19 +1,19 @@
 /*
-	Flexisip, a flexible SIP proxy server with media capabilities.
-	Copyright (C) 2010-2021  Belledonne Communications SARL, All rights reserved.
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2021  Belledonne Communications SARL, All rights reserved.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #pragma once
@@ -31,7 +31,7 @@ namespace flexisip {
 namespace pushnotification {
 
 class FirebaseClient : public Client {
-  public:
+public:
 	FirebaseClient(su_root_t& root);
 
 	void sendPush(const std::shared_ptr<Request>& req) override;
@@ -39,15 +39,19 @@ class FirebaseClient : public Client {
 		return mHttp2Client->isIdle();
 	}
 
-	static constexpr const char* FIREBASE_ADDRESS = "fcm.googleapis.com";
-	static constexpr const char* FIREBASE_PORT = "443";
+	void enableInsecureTestMode() {
+		mHttp2Client->enableInsecureTestMode();
+	}
 
-  private:
+	static inline std::string FIREBASE_ADDRESS = "fcm.googleapis.com";
+	static inline std::string FIREBASE_PORT = "443";
+
+private:
 	void onResponse(const std::shared_ptr<HttpMessage>& request, const std::shared_ptr<HttpResponse>& response);
 	void onError(const std::shared_ptr<HttpMessage>& request);
 
-	std::string mLogPrefix{};
 	std::unique_ptr<Http2Client> mHttp2Client;
+	std::string mLogPrefix{};
 };
 
 } // namespace pushnotification
