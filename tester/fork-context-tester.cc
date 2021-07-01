8241c94854dd802c5ae08660da07bb5411f1da2c
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2021-07-01 15:21:43 +0200

    Reduce Fork[Call/Basic]Context leak on internal server errors
     - Better handling of "Too many hops" error (max-forward=0)
     - Better handling of "RTP port pool exhausted" (module-mediarelay)

--- tester/fork-context-tester.cc
+++ tester/fork-context-tester.cc
@@ -29,6 +29,9 @@ using namespace std::chrono_literals;
 using namespace std::chrono;
 using namespace flexisip;
 
+
+static su_root_t* root = nullptr;
+static shared_ptr<Agent> agent = nullptr;
 static bool responseReceived = false;
 
 /**
@@ -46,11 +49,21 @@ public:
 	}
 };
 
+static void beforeEach() {
+	responseReceived = false;
+	root = su_root_create(nullptr);
+	agent = make_shared<Agent>(root);
+}
+
+static void afterEach() {
+	agent->unloadConfig();
+	RegistrarDb::resetDB();
+	agent.reset();
+	su_root_destroy(root);
+}
+
 static void nullMaxFrowardAndForkBasicContext() {
 	// Agent initialization
-	auto root = su_root_create(nullptr);
-	auto agent = make_shared<Agent>(root);
-
 	auto cfg = GenericManager::get();
 	cfg->load(string(TESTER_DATA_DIR).append("/config/flexisip_fork_context.conf").c_str());
 	agent->loadConfig(cfg);
@@ -90,15 +103,13 @@ static void nullMaxFrowardAndForkBasicContext() {
 	                             "User-Agent: Linphone/3.3.99.10 (eXosip2/3.3.0)\r\n"
 	                             "Content-Length: 0\r\n\r\n");
 
-	// Flexisip and belle-sip loop, until 25 sec passed, because ForkBasicContext::onDecisionTimer is triggered after
-	// 20 sec.
-	auto beforePlus25 = system_clock::now() + 25s;
-	while (true) {
+	// Flexisip and belle-sip loop, until response is received by the belle-sip stack.
+	// If after 5s (MUST be inferior to ForkBasicContext timeout) nothing is received we break the loop and the test
+	// should fail.
+	auto beforePlus5 = system_clock::now() + 5s;
+	while (!responseReceived && beforePlus5 >= system_clock::now()) {
 		su_root_step(agent->getRoot(), 100);
 		bellesipUtils.stackSleep(100);
-		if (beforePlus25 < system_clock::now()) {
-			break;
-		}
 	}
 
 	auto moduleRouter = static_cast<ModuleRouter*>(agent->findModule("Router"));
@@ -107,12 +118,103 @@ static void nullMaxFrowardAndForkBasicContext() {
 	if (moduleRouter) {
 		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountForks->start->read(), 1, int, "%i");
 		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountForks->finish->read(), 1, int, "%i");
+		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountBasicForks->start->read(), 1, int, "%i");
+		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountBasicForks->finish->read(), 1, int, "%i");
+	}
+}
+
+static void notRtpPortAndForkCallContext() {
+	// Agent initialization
+	auto cfg = GenericManager::get();
+	cfg->load(string(TESTER_DATA_DIR).append("/config/flexisip_fork_context_media_relay.conf").c_str());
+	agent->loadConfig(cfg);
+
+	auto registrarConf = GenericManager::get()->getRoot()->get<GenericStruct>("module::Registrar");
+	registrarConf->get<ConfigStringList>("reg-domains")->set("127.0.0.1");
+
+	// Inserting a contact into the registrarDB.
+	sofiasip::Home home{};
+	SipUri user{"sip:participant1@127.0.0.1"};
+	BindingParameters parameter{};
+	parameter.globalExpire = 1000;
+	parameter.callId = "random_id_necessary_to_bind_1";
+	parameter.userAgent = "Linphone1 (Ubuntu) LinphoneCore";
+	parameter.withGruu = true;
+	auto participantContact = sip_contact_create(home.home(), (url_string_t*)user.str().c_str(), nullptr);
+	RegistrarDb::get()->bind(user, participantContact, parameter, make_shared<BindListener>());
+
+	// Starting Flexisip
+	agent->start("", "");
+
+	// Sending a request with Max-Forwards = 0
+	BellesipUtils bellesipUtils{"0.0.0.0", -1, "UDP", [](int status) {
+		                            if (status != 100) {
+			                            BC_ASSERT_EQUAL(status, 500, int, "%i");
+			                            responseReceived = true;
+		                            }
+	                            }};
+	bellesipUtils.sendRawRequest(
+	    // Sip message
+	    "INVITE sip:participant1@127.0.0.1 SIP/2.0\r\n"
+	    "Via: SIP/2.0/UDP "
+	    "10.23.17.117:22600;branch=z9hG4bK-d8754z-4d7620d2feccbfac-1---d8754z-;rport=4820;received=202.165.193.129\r\n"
+	    "Max-Forwards: 70\r\n"
+	    "Contact: <sip:bcheong@202.165.193.129:4820>\r\n"
+	    "To: <sip:participant1@127.0.0.1>\r\n"
+	    "From: <sip:anthony@127.0.0.1>;tag=465687829\r\n"
+	    "Call-ID: Y2NlNzg0ODc0ZGIxODU1MWI5MzhkNDVkNDZhOTQ4YWU.\r\n"
+	    "CSeq: 1 INVITE\r\n"
+	    "Allow: INVITE, ACK, CANCEL, OPTIONS, BYE, REFER, NOTIFY, MESSAGE, SUBSCRIBE, INFO, PRACK\r\n"
+	    "c: application/sdp\r\n"
+	    "Supported: replaces\r\n"
+	    "Supported: 100rel\r\n"
+	    "Authorization: Digest username=\"003332176\", realm=\"sip.ovh.net\", "
+	    "nonce=\"24212965507cde726e8bc37e04686459\", uri=\"sip:sip.ovh.net\", "
+	    "response=\"896e786e9c0525ca3085322c7f1bce7b\", algorithm=MD5, opaque=\"241b9fb347752f2\"\r\n"
+	    "User-Agent: X-Lite 4 release 4.0 stamp 58832\r\n",
+	    // Request body
+	    "v=0\r\n"
+	    "o=anthony.gauchy 3102 279 IN IP4 127.0.0.1\r\n"
+	    "s=Talk\r\n"
+	    "c=IN IP4 127.0.0.1\r\n"
+	    "t=0 0\r\n"
+	    "m=audio 7078 RTP/AVP 111 110 3 0 8 101\r\n"
+	    "a=rtpmap:111 speex/16000\r\n"
+	    "a=fmtp:111 vbr=on\r\n"
+	    "a=rtpmap:110 speex/8000\r\n"
+	    "a=fmtp:110 vbr=on\r\n"
+	    "a=rtpmap:101 telephone-event/8000\r\n"
+	    "a=fmtp:101 0-11\r\n"
+	    "m=video 8078 RTP/AVP 99 97 98\r\n"
+	    "c=IN IP4 192.168.0.18\r\n"
+	    "b=AS:380\r\n"
+	    "a=rtpmap:99 MP4V-ES/90000\r\n"
+	    "a=fmtp:99 profile-level-id=3\r\n");
+
+	// Flexisip and belle-sip loop, until response is received by the belle-sip stack.
+	// If after 5s (MUST be inferior to ForkBasicContext timeout) nothing is received we break the loop and the test
+	// should fail.
+	auto beforePlus5 = system_clock::now() + 5s;
+	while (!responseReceived && beforePlus5 >= system_clock::now()) {
+		su_root_step(agent->getRoot(), 100);
+		bellesipUtils.stackSleep(100);
+	}
+
+	auto moduleRouter = static_cast<ModuleRouter*>(agent->findModule("Router"));
+	BC_ASSERT_PTR_NOT_NULL(moduleRouter);
+	BC_ASSERT_TRUE(responseReceived);
+	if (moduleRouter) {
+		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountForks->start->read(), 1, int, "%i");
+		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountForks->finish->read(), 1, int, "%i");
+		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountCallForks->start->read(), 1, int, "%i");
+		BC_ASSERT_EQUAL(moduleRouter->mStats.mCountCallForks->finish->read(), 1, int, "%i");
 	}
 }
 
 static test_t tests[] = {
     TEST_NO_TAG("Max forward 0 and ForkBasicContext leak", nullMaxFrowardAndForkBasicContext),
+    TEST_NO_TAG("No RTP port available and ForkCallContext leak", notRtpPortAndForkCallContext),
 };
 
-test_suite_t fork_context_suite = {"Fork context", nullptr, nullptr, nullptr, nullptr, sizeof(tests) / sizeof(tests[0]),
+test_suite_t fork_context_suite = {"Fork context", nullptr, nullptr, beforeEach, afterEach, sizeof(tests) / sizeof(tests[0]),
                                    tests};
