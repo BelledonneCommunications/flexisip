2f3d8a51ee76fd77edd6c34888acac2285e353ca
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2021-09-14 17:46:34 +0200

    Persist message, first step
    Proxy for ForkMessageContext
    shared_ptr instead of basic ptr for listener and module.

--- include/flexisip/fork-context/fork-basic-context.hh
+++ include/flexisip/fork-context/fork-basic-context.hh
@@ -23,12 +23,12 @@
 
 #include <flexisip/agent.hh>
 #include <flexisip/event.hh>
-#include <flexisip/forkcontext.hh>
+#include <flexisip/fork-context/fork-context-base.hh>
 #include <flexisip/transaction.hh>
 
 namespace flexisip {
 
-class ForkBasicContext : public ForkContext {
+class ForkBasicContext : public ForkContextBase {
 private:
 	/**
 	 * Timeout after which an answer must be sent through the incoming transaction even if no success response was
@@ -38,7 +38,7 @@ private:
 
 public:
 	ForkBasicContext(Agent* agent, const std::shared_ptr<RequestSipEvent>& event,
-	                 std::shared_ptr<ForkContextConfig> cfg, ForkContextListener* listener,
+	                 std::shared_ptr<ForkContextConfig> cfg, const std::weak_ptr<ForkContextListener>& listener,
 	                 std::weak_ptr<StatPair> counter);
 	virtual ~ForkBasicContext();
 
