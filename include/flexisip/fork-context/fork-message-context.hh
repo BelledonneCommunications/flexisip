b8fa0d24beee140ea949d6be8ecf6e6d0d60d37e
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2021-10-07 17:41:22 +0200

    [skip ci] Persist message in database

--- include/flexisip/fork-context/fork-message-context.hh
+++ include/flexisip/fork-context/fork-message-context.hh
@@ -1,20 +1,20 @@
 /*
-	Flexisip, a flexible SIP proxy server with media capabilities.
-	Copyright (C) 2010-2015  Belledonne Communications SARL, All rights reserved.
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2021  Belledonne Communications SARL, All rights reserved.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
-*/
+    You should have received a copy of the GNU Affero General Public License
+    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+ */
 
 #pragma once
 
@@ -24,42 +24,72 @@
 #include "flexisip/agent.hh"
 #include "flexisip/event.hh"
 #include "flexisip/fork-context/fork-context-base.hh"
+#include "flexisip/fork-context/fork-message-context-db.hh"
 #include "flexisip/transaction.hh"
 
 namespace flexisip {
 
 class ForkMessageContext : public ForkContextBase {
 public:
-	static std::shared_ptr<ForkMessageContext> make(Agent* agent, const std::shared_ptr<RequestSipEvent>& event,
+	static std::shared_ptr<ForkMessageContext> make(Agent* agent,
+	                                                const std::shared_ptr<RequestSipEvent>& event,
 	                                                const std::shared_ptr<ForkContextConfig>& cfg,
 	                                                const std::weak_ptr<ForkContextListener>& listener,
 	                                                const std::weak_ptr<StatPair>& counter);
+
+	static std::shared_ptr<ForkMessageContext> make(Agent* agent,
+	                                                const std::shared_ptr<RequestSipEvent>& event,
+	                                                const std::shared_ptr<ForkContextConfig>& cfg,
+	                                                const std::weak_ptr<ForkContextListener>& listener,
+	                                                const std::weak_ptr<StatPair>& counter,
+	                                                ForkMessageContextDb& forkFromDb);
+
 	virtual ~ForkMessageContext();
 
 	virtual bool onNewRegister(const url_t* url, const std::string& uid);
-	virtual void onResponse(const std::shared_ptr<BranchInfo> &br, const std::shared_ptr<ResponseSipEvent> &ev);
+	virtual void onResponse(const std::shared_ptr<BranchInfo>& br, const std::shared_ptr<ResponseSipEvent>& ev);
+
+	ForkMessageContextDb getDbObject();
+	void restoreBranch(const BranchInfoDb& dbBranch);
+
+#ifdef ENABLE_UNIT_TESTS
+	void assertEqual(const std::shared_ptr<ForkMessageContext>& expected);
+#endif
 
 protected:
-	virtual void onNewBranch(const std::shared_ptr<BranchInfo> &br);
+	virtual void onNewBranch(const std::shared_ptr<BranchInfo>& br);
 	virtual bool shouldFinish();
 
 private:
-	ForkMessageContext(Agent* agent, const std::shared_ptr<RequestSipEvent>& event,
+	ForkMessageContext(Agent* agent,
+	                   const std::shared_ptr<RequestSipEvent>& event,
 	                   const std::shared_ptr<ForkContextConfig>& cfg,
-	                   const std::weak_ptr<ForkContextListener>& listener, const std::weak_ptr<StatPair>& counter);
-	static void sOnAcceptanceTimer(su_root_magic_t *magic, su_timer_t *t, su_timer_arg_t *arg);
+	                   const std::weak_ptr<ForkContextListener>& listener,
+	                   const std::weak_ptr<StatPair>& counter,
+	                   bool isRestored = false);
+
 	void acceptMessage();
 	void onAcceptanceTimer();
-	void logReceivedFromUserEvent(const std::shared_ptr<RequestSipEvent> &reqEv, const std::shared_ptr<ResponseSipEvent> &respEv);
+	void logReceivedFromUserEvent(const std::shared_ptr<RequestSipEvent>& reqEv,
+	                              const std::shared_ptr<ResponseSipEvent>& respEv);
 	void checkFinished();
-	void logDeliveredToUserEvent(const std::shared_ptr<RequestSipEvent> &reqEv, const std::shared_ptr<ResponseSipEvent> &respEv);
+	void logDeliveredToUserEvent(const std::shared_ptr<RequestSipEvent>& reqEv,
+	                             const std::shared_ptr<ResponseSipEvent>& respEv);
 
-	su_timer_t
-	    *mAcceptanceTimer; /*timeout after which an answer must be sent through the incoming transaction even if no
-	                                             success response was received on the outgoing transactions*/
-	static const int sAcceptanceTimeout = 20; /* this must be less than the transaction time (32 seconds)*/
+	/**
+	 * Timeout after which an answer must be sent through the incoming transaction even if no success response was
+	 * received on the outgoing transactions.
+	 */
+	std::unique_ptr<sofiasip::Timer> mAcceptanceTimer{nullptr};
 	int mDeliveredCount;
-	bool mIsMessage; /* tells if the ForkMessageContext is a message, if false it's a refer */
+	/**
+	 * Tells if the ForkMessageContext is a message, if false it's a refer.
+	 */
+	bool mIsMessage;
+	/**
+	 * Is used in fork late mode with message saved in DB to remember message expiration date.
+	 */
+	time_t mExpirationDate;
 };
 
-}
+} // namespace flexisip
