2cb265e10ed5214654d52bdb48c3d99d62a9661f
Danmei Chen <danmei.chen@belledonne-communications.com>
2021-10-11 18:21:45 +0200

    Revert "Put call in conference after ICE negotiations terminates"
    This reverts commit af8bb0d76e7cf482b6a8d638adfb92206ede6503.

--- src/conference/conference-server.cc
+++ src/conference/conference-server.cc
@@ -346,8 +346,6 @@ void ConferenceServer::onCallStateChanged(const std::shared_ptr<linphone::Core> 
 				  linphone::Call::State cstate, const std::string & message){
 
 	auto to = call->getToAddress();
-	auto stats = call->getStats();
-	auto iceState = stats->getIceState();
 	auto it = mConferences.find(to->getUsername());
 	switch(cstate){
 		case linphone::Call::State::IncomingReceived:
@@ -361,7 +359,7 @@ void ConferenceServer::onCallStateChanged(const std::shared_ptr<linphone::Core> 
 			}
 		break;
 		case linphone::Call::State::StreamsRunning:
-			if ((iceState != InProgress) && (it != mConferences.end())){
+			if (it != mConferences.end()){
 				(*it).second->addCall(call);
 			}else{
 				LOGD("Unable to add participant [%s] to any conference", call->getRemoteAddress()->asString().c_str());
