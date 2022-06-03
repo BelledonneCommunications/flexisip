4cc0f9137dc6a6b149421635a6fbb6fd9ae1d1b4
François Grisez <francois.grisez@belledonne-communications.com>
2022-06-03 11:00:55 +0200

    Add missing header inclusion

--- src/utils/transport/http/http2client.cc
+++ src/utils/transport/http/http2client.cc
@@ -1,6 +1,6 @@
 /*
  Flexisip, a flexible SIP proxy server with media capabilities.
- Copyright (C) 2010-2021  Belledonne Communications SARL, All rights reserved.
+ Copyright (C) 2010-2022  Belledonne Communications SARL, All rights reserved.
 
  This program is free software: you can redistribute it and/or modify
  it under the terms of the GNU Affero General Public License as
@@ -17,6 +17,7 @@
  */
 
 #include <algorithm>
+#include <array>
 #include <sstream>
 
 #include <nghttp2/nghttp2ver.h>
