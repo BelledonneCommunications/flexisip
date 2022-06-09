3a85f9b758e956c925e3a5f8342b479bd52ad232
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2022-06-09 17:47:07 +0200

    Add RockyLinux 8 to CI and improve jobs factorization

--- .gitlab-ci-files/deploy.sh
+++ .gitlab-ci-files/deploy.sh
@@ -2,16 +2,9 @@
 
 function print_usage {
 	prog=$(basename $0)
-	echo "syntax: $prog [--make-lagacy-repo] <dist>" 1>&2
+	echo "syntax: $prog <dist>" 1>&2
 	exit 2
 }
-
-if [ "$1" = '--make-legacy-repo' ]; then
-	make_legacy=1
-	shift
-else
-	make_legacy=0
-fi
 
 if [ -z "$1" ] || [ "${1:0:1}" = '-' ]; then
 	print_usage $0
@@ -32,16 +25,18 @@ case "$dist" in
 	'centos')
 		make_repo_args="rpm $tmpdir $CENTOS_REPOSITORY"
 		rsync_src='build/*.rpm'
-		legacy_repo_cmd="cp -v --link --no-clobber --preserve=timestamp $tmpdir/* $CENTOS7_DEPLOY_DIRECTORY && createrepo_c --update $CENTOS7_DEPLOY_DIRECTORY"
+		;;
+  'rockylinux')
+		make_repo_args="rpm $tmpdir $ROCKYLINUX_REPOSITORY"
+		rsync_src='build/*.rpm'
 		;;
 	'debian')
 		make_repo_args="deb $tmpdir $FREIGHT_PATH $RELEASE"
 		echo "make_repo_args=$make_repo_args"
 		rsync_src='build/*.deb build/*.ddeb'
-		legacy_repo_cmd="freight add --conf=$FREIGHT_PATH $tmpdir/*.deb apt/$RELEASE && freight cache --conf=$FREIGHT_PATH apt/$RELEASE"
 		;;
 	       *)
-		echo "invalid distribution type: '$dist'. Only 'centos' and 'debian' are valid" 1>&2
+		echo "invalid distribution type: '$dist'. Only 'centos', 'rockylinux' and 'debian' are valid" 1>&2
 		exit 2
 		;;
 esac
@@ -53,11 +48,6 @@ echo ">>> Connecting on '$DEPLOY_SERVER'"
 ssh $DEPLOY_SERVER "
 	echo '>>>> Making repository'
 	make_repo $make_repo_args || exit 1
-
-	if [ $make_legacy -eq 1 ]; then
-		echo '>>>> Making legacy repository'
-		$legacy_repo_cmd || exit 1
-	fi
 
 	echo \">>>> Removing '$tmpdir'\"
 	rm -r $tmpdir
--- docker/mariadb-entrypoint.sh
+++ docker/mariadb-entrypoint.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 # MYSQL DB for unit tests
-sudo service ${1:-mysql} start
+# First parameter MUST be the command to start mysql/mariadb service.
+sudo  ${1}
 sudo mysql -e "CREATE USER belledonne IDENTIFIED BY 'cOmmu2015nicatiOns'"
 sudo mysql -e "CREATE DATABASE flexisip_messages"
 sudo mysql -e "GRANT ALL PRIVILEGES ON flexisip_messages.* to belledonne"
