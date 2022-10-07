4c70b4d32da96a151243f9fedca9b79fb9fa4d64
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2022-10-07 10:03:52 +0200

    Clang format file for feature/choose_which_body_you_want branch

--- include/flexisip/common.hh
+++ include/flexisip/common.hh
@@ -1,32 +1,31 @@
-
 /*
-	Flexisip, a flexible SIP proxy server with media capabilities.
-	Copyright (C) 2010  Belledonne Communications SARL.
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2022 Belledonne Communications SARL, All rights reserved.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
 
 #include <flexisip/logmanager.hh>
 
-#include <cstdlib>
 #include <cstdarg>
+#include <cstdlib>
 #include <map>
 #include <string>
-#include <vector>
 #include <sys/timeb.h>
+#include <vector>
 
 #ifndef MAX
 #define MAX(a, b) (a) > (b) ? (a) : (b)
@@ -38,13 +37,13 @@ time_t getTimeOffset(time_t current_time);
 namespace flexisip {
 
 class Mutex {
-  public:
+public:
 	Mutex(bool reentrant = false);
 	void lock();
 	void unlock();
 	~Mutex();
 
-  private:
+private:
 	const bool mReentrant;
 	pthread_t mThread;
 	int mCount;
@@ -52,15 +51,17 @@ class Mutex {
 	pthread_mutex_t mInternalMutex;
 };
 
-template <typename _type> class delete_functor {
-  public:
-	void operator()(_type *obj) {
+template <typename _type>
+class delete_functor {
+public:
+	void operator()(_type* obj) {
 		delete obj;
 	}
 };
 
-template <typename _first, typename _last> class map_delete_functor {
-  public:
+template <typename _first, typename _last>
+class map_delete_functor {
+public:
 	void operator()(std::pair<_first, _last> obj) {
 		delete obj.second;
 	}
@@ -73,12 +74,13 @@ template <typename _first, typename _last> class map_delete_functor {
 class BinaryIp {
 public:
 	/* Adds a hostname in the form of BinaryIp into a generic STL container.
-	 If numericOnly is set to true, then no DNS lookup will be made. As a result fully qualified domain names will be ignored.*/
+	 If numericOnly is set to true, then no DNS lookup will be made. As a result fully qualified domain names will be
+	 ignored.*/
 	template <typename _containerT>
-	static _containerT & emplace(_containerT & container, const std::string &hostname, bool numericOnly = false){
-		struct addrinfo *ai = resolve(hostname, numericOnly);
-		struct addrinfo *ai_it;
-		for (ai_it = ai ; ai_it != nullptr; ai_it = ai_it->ai_next){
+	static _containerT& emplace(_containerT& container, const std::string& hostname, bool numericOnly = false) {
+		struct addrinfo* ai = resolve(hostname, numericOnly);
+		struct addrinfo* ai_it;
+		for (ai_it = ai; ai_it != nullptr; ai_it = ai_it->ai_next) {
 			container.emplace(ai_it);
 		}
 		freeaddrinfo(ai);
@@ -86,28 +88,29 @@ public:
 	}
 	/* Builds a BinaryIp from a struct addrinfo containing an AF_INET6 address only !.
 	 * Do not use directly, use static emplace() method to build BinarIps.*/
-	BinaryIp(const struct addrinfo *ai);
-	BinaryIp(const char *ip);
+	BinaryIp(const struct addrinfo* ai);
+	BinaryIp(const char* ip);
 
-	bool operator==(const BinaryIp &ip2) const {
+	bool operator==(const BinaryIp& ip2) const {
 		return memcmp(&mAddr, &ip2.mAddr, sizeof mAddr) == 0;
 	}
-	bool operator<(const BinaryIp &ip2) const{
+	bool operator<(const BinaryIp& ip2) const {
 		return memcmp(&mAddr, &ip2.mAddr, sizeof mAddr) < 0;
 	}
-	bool operator<=(const BinaryIp &ip2) const{
+	bool operator<=(const BinaryIp& ip2) const {
 		return memcmp(&mAddr, &ip2.mAddr, sizeof mAddr) <= 0;
 	}
-	bool operator>(const BinaryIp &ip2) const{
+	bool operator>(const BinaryIp& ip2) const {
 		return memcmp(&mAddr, &ip2.mAddr, sizeof mAddr) > 0;
 	}
-	bool operator>=(const BinaryIp &ip2) const{
+	bool operator>=(const BinaryIp& ip2) const {
 		return memcmp(&mAddr, &ip2.mAddr, sizeof mAddr) >= 0;
 	}
-	//turn hummanely readable IP. This function is not optimized for speed.
+	// turn hummanely readable IP. This function is not optimized for speed.
 	std::string asString() const;
+
 private:
-	static struct addrinfo *resolve(const std::string &hostname, bool numericOnly);
+	static struct addrinfo* resolve(const std::string& hostname, bool numericOnly);
 	struct in6_addr mAddr;
 };
 
--- src/cli.hh
+++ src/cli.hh
@@ -1,27 +1,27 @@
 /*
-	Flexisip, a flexible SIP proxy server with media capabilities.
-	Copyright (C) 2010-2016  Belledonne Communications SARL, All rights reserved.
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2022 Belledonne Communications SARL, All rights reserved.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
 
-#include <sys/un.h>
+#include <flexisip/configmanager.hh>
 #include <pthread.h>
 #include <string>
-#include <flexisip/configmanager.hh>
+#include <sys/un.h>
 
 namespace flexisip {
 class CliHandler {
@@ -52,7 +52,7 @@ class Agent;
 
 class CommandLineInterface {
 public:
-	CommandLineInterface(const std::string &name);
+	CommandLineInterface(const std::string& name);
 	virtual ~CommandLineInterface();
 
 	void start();
@@ -61,41 +61,41 @@ public:
 	void registerHandler(CliHandler& handler);
 
 protected:
-	void answer(unsigned int socket, const std::string &message);
-	virtual void parseAndAnswer(unsigned int socket, const std::string &command, const std::vector<std::string> &args);
+	void answer(unsigned int socket, const std::string& message);
+	virtual void parseAndAnswer(unsigned int socket, const std::string& command, const std::vector<std::string>& args);
 
 private:
-	GenericEntry *getGenericEntry(const std::string &arg) const;
-	void handleConfigGet(unsigned int socket, const std::vector<std::string> &args);
-	void handleConfigList(unsigned int socket, const std::vector<std::string> &args);
-	void handleConfigSet(unsigned int socket, const std::vector<std::string> &args);
+	GenericEntry* getGenericEntry(const std::string& arg) const;
+	void handleConfigGet(unsigned int socket, const std::vector<std::string>& args);
+	void handleConfigList(unsigned int socket, const std::vector<std::string>& args);
+	void handleConfigSet(unsigned int socket, const std::vector<std::string>& args);
 	void dispatch(unsigned int socket, const std::string& command, const std::vector<std::string>& args);
 	void run();
 
-	static GenericEntry* find(GenericStruct *root, std::vector<std::string> &path);
-	static std::string printEntry(GenericEntry *entry, bool printHelpInsteadOfValue);
-	static std::string printSection(GenericStruct *gstruct, bool printHelpInsteadOfValue);
-	std::string agregate(const std::vector<std::string> &args, size_t from_pos);
+	static GenericEntry* find(GenericStruct* root, std::vector<std::string>& path);
+	static std::string printEntry(GenericEntry* entry, bool printHelpInsteadOfValue);
+	static std::string printSection(GenericStruct* gstruct, bool printHelpInsteadOfValue);
+	std::string agregate(const std::vector<std::string>& args, size_t from_pos);
 
-	static void *threadfunc(void *arg);
+	static void* threadfunc(void* arg);
 
 	std::string mName;
 	pthread_t mThread = 0;
-	int mControlFds[2] = { 0, 0 };
+	int mControlFds[2] = {0, 0};
 	bool mRunning = false;
 	std::shared_ptr<CliHandler::HandlerTable> handlers;
 };
 
 class ProxyCommandLineInterface : public CommandLineInterface {
 public:
-	ProxyCommandLineInterface(const std::shared_ptr<Agent> &agent);
+	ProxyCommandLineInterface(const std::shared_ptr<Agent>& agent);
 
 private:
-	void handleRegistrarClear(unsigned int socket, const std::vector<std::string> &args);
-	void handleRegistrarDelete(unsigned int socket, const std::vector<std::string> &args);
-	void handleRegistrarGet(unsigned int socket, const std::vector<std::string> &args);
-	void handleRegistrarDump(unsigned int socket, const std::vector<std::string> &args);
-	void parseAndAnswer(unsigned int socket, const std::string &command, const std::vector<std::string> &args) override;
+	void handleRegistrarClear(unsigned int socket, const std::vector<std::string>& args);
+	void handleRegistrarDelete(unsigned int socket, const std::vector<std::string>& args);
+	void handleRegistrarGet(unsigned int socket, const std::vector<std::string>& args);
+	void handleRegistrarDump(unsigned int socket, const std::vector<std::string>& args);
+	void parseAndAnswer(unsigned int socket, const std::string& command, const std::vector<std::string>& args) override;
 
 	std::shared_ptr<Agent> mAgent;
 };
