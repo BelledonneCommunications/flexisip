871169281ebf4e072ca71f637759d8ccb0a98b18
Thibault Lemaire <thibault.lemaire@belledonne-communications.com>
2022-11-21 11:38:24 +0100

    Fix: mantis-10965, mantis-11013
    Segfault when disconnecting from Redis Registrar

---
+++ tester/registrardb-redis-tester.cc
@@ -0,0 +1,74 @@
+/** Copyright (C) 2010-2022 Belledonne Communications SARL
+ *  SPDX-License-Identifier: AGPL-3.0-or-later
+ */
+
+#include <chrono>
+
+#include "tester.hh"
+#include "utils/asserts.hh"
+#include "utils/proxy-server.hh"
+#include "utils/redis-server.hh"
+
+#include "registrardb-redis.hh"
+
+using namespace std::chrono_literals;
+
+namespace flexisip {
+namespace tester {
+namespace registrardb_redis {
+
+class OperationFailedListener : public ContactUpdateListener {
+public:
+	bool finished = false;
+
+	OperationFailedListener() {
+	}
+
+	void onRecordFound(const std::shared_ptr<Record>& r) override {
+		BC_FAIL(unexpected call to onRecordFound);
+	}
+	void onError() override {
+		finished = true;
+	}
+	void onInvalid() override {
+		BC_FAIL(unexpected call to onInvalid);
+	}
+	void onContactUpdated(const std::shared_ptr<ExtendedContact>& ec) override {
+		BC_FAIL(unexpected call to onContactUpdated);
+	}
+};
+
+void mContext_should_be_checked_on_serializeAndSendToRedis() {
+	RedisServer redis;
+	Server proxyServer({{"module::Registrar/db-implementation", "redis"},
+	                    {"module::Registrar/redis-server-domain", "localhost"},
+	                    {"module::Registrar/redis-server-port", std::to_string(redis.start())}});
+	BcAssert asserter{};
+	asserter.addCustomIterate([&root = *proxyServer.getRoot()] { root.step(1ms); });
+	auto& registrar = *dynamic_cast<RegistrarDbRedisAsync*>(RegistrarDb::get());
+	const auto placeholder = "sip:placeholder@example.org";
+	BindingParameters bindParams;
+	bindParams.globalExpire = 3001;
+	bindParams.callId = __FUNCTION__;
+	sofiasip::Home home{};
+	auto listener = std::make_shared<OperationFailedListener>();
+
+	registrar.bind(SipUri(placeholder), sip_contact_make(home.home(), placeholder), bindParams, listener);
+	registrar.disconnect(); // disconnecting before the previous bind operation finishes
+
+	// The bind() ends in error, but there should be no segfault
+	BC_ASSERT_TRUE(asserter.iterateUpTo(30, [&finished = listener->finished] { return finished; }));
+}
+
+auto _ = [] {
+	static test_t tests[] = {
+	    TEST_NO_TAG_AUTO_NAMED(mContext_should_be_checked_on_serializeAndSendToRedis),
+	};
+	static test_suite_t suite{"RegistrarDbRedis", NULL, NULL, NULL, NULL, sizeof(tests) / sizeof(tests[0]), tests};
+	bc_tester_add_suite(&suite);
+	return nullptr;
+}();
+
+} // namespace registrardb_redis
+} // namespace tester
+} // namespace flexisip
