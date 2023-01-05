730e14a083dff4ed46af34b715bb7d706f58a440
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2023-01-05 18:06:17 +0100

    Fix scenario where push was not sent
    If you had two devices with same push token for a different push
    provider one of them used to not receive push.

--- src/module-pushnotification.cc
+++ src/module-pushnotification.cc
@@ -1,6 +1,6 @@
 /*
     Flexisip, a flexible SIP proxy server with media capabilities.
-    Copyright (C) 2010-2022 Belledonne Communications SARL, All rights reserved.
+    Copyright (C) 2010-2023 Belledonne Communications SARL, All rights reserved.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -9,11 +9,11 @@
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
-    along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #include "flexisip/module-pushnotification.hh"
@@ -354,11 +354,12 @@ void PushNotification::makePushNotification(const shared_ptr<MsgSip>& ms,
 	// check if another push notification for this device wouldn't be pending
 	shared_ptr<PushNotificationContext> context{};
 	const auto& dest = pinfo->mDestinations.begin()->second;
-	auto pnKey = pinfo->mCallId + ":" + dest->getParam() + ":" + dest->getPrid();
+	auto pnKey = pinfo->mCallId + ":" + dest->getProvider() + ":" + dest->getParam() + ":" + dest->getPrid();
 	auto it = mPendingNotifications.find(pnKey);
 	if (it != mPendingNotifications.end()) {
-		LOGD("Another push notification is pending for this call %s and this device token %s, not creating a new one",
-		     pinfo->mCallId.c_str(), dest->getPrid().c_str());
+		LOGD("Another push notification is pending for this call %s and this device provider %s and token %s, not "
+		     "creating a new one",
+		     pinfo->mCallId.c_str(), dest->getProvider().c_str(), dest->getPrid().c_str());
 		context = it->second;
 	}
 
