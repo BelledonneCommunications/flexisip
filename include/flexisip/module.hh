2b97041781dbf3e833d5ad3d09160d51536f6d48
François Grisez <francois.grisez@belledonne-communications.com>
2023-04-07 15:48:01 +0200

    Clang-format

--- include/flexisip/module.hh
+++ include/flexisip/module.hh
@@ -36,18 +36,15 @@ namespace flexisip {
 
 class ModuleInfoBase;
 
-template<typename T>
+template <typename T>
 class ModuleInfo;
 
 class SharedLibrary;
 class EntryFilter;
 
-enum class ModuleClass {
-	Experimental,
-	Production
-};
+enum class ModuleClass { Experimental, Production };
 
-extern "C" Module *__flexisipCreatePlugin(Agent *agent, SharedLibrary *sharedLibrary);
+extern "C" Module* __flexisipCreatePlugin(Agent* agent, SharedLibrary* sharedLibrary);
 
 /**
  * Abstract base class for all Flexisip module.
@@ -55,7 +52,7 @@ extern "C" Module *__flexisipCreatePlugin(Agent *agent, SharedLibrary *sharedLib
  * It must implements at least:
  * virtual void onRequest(SipEvent *ev)=0;
  * virtual void onResponse(SipEvent *ev)=0;
-**/
+ **/
 class Module : protected ConfigValueListener {
 	template <typename T>
 	friend class ModuleInfo;
@@ -142,24 +139,24 @@ class ModuleInfoManager {
 	friend class ModuleInfoBase;
 
 public:
-	const std::list<ModuleInfoBase *> &getRegisteredModuleInfo() const {
+	const std::list<ModuleInfoBase*>& getRegisteredModuleInfo() const {
 		return mRegisteredModuleInfo;
 	}
 	std::list<ModuleInfoBase*> buildModuleChain() const;
 
-	static ModuleInfoManager *get();
+	static ModuleInfoManager* get();
 
 private:
-	void registerModuleInfo(ModuleInfoBase *moduleInfo);
-	void unregisterModuleInfo(ModuleInfoBase *moduleInfo);
-	void dumpModuleDependencies(const std::list<ModuleInfoBase *> &l) const;
-	bool moduleDependenciesPresent(const std::list<ModuleInfoBase *> &sortedList, ModuleInfoBase *module) const;
+	void registerModuleInfo(ModuleInfoBase* moduleInfo);
+	void unregisterModuleInfo(ModuleInfoBase* moduleInfo);
+	void dumpModuleDependencies(const std::list<ModuleInfoBase*>& l) const;
+	bool moduleDependenciesPresent(const std::list<ModuleInfoBase*>& sortedList, ModuleInfoBase* module) const;
 	void replaceModules(std::list<ModuleInfoBase*>& sortedList,
-	                              const std::list<ModuleInfoBase*>& replacingModules) const;
-	
-	std::list<ModuleInfoBase *> mRegisteredModuleInfo;
+	                    const std::list<ModuleInfoBase*>& replacingModules) const;
 
-	static ModuleInfoManager *sInstance;
+	std::list<ModuleInfoBase*> mRegisteredModuleInfo;
+
+	static ModuleInfoManager* sInstance;
 };
 
 class ModuleInfoBase {
@@ -190,22 +187,41 @@ public:
 		Plugin = 320
 	};
 
-	ModuleInfoBase(
-		const std::string &moduleName, const std::string &help, const std::vector<std::string> &after,
-		ModuleOid oid, ModuleClass moduleClass, const std::string &replace
-	) : mName(moduleName), mHelp(help), mAfter(after), mOidIndex(oid), mClass(moduleClass), mReplace(replace) {
+	ModuleInfoBase(const std::string& moduleName,
+	               const std::string& help,
+	               const std::vector<std::string>& after,
+	               ModuleOid oid,
+	               ModuleClass moduleClass,
+	               const std::string& replace)
+	    : mName(moduleName), mHelp(help), mAfter(after), mOidIndex(oid), mClass(moduleClass), mReplace(replace) {
 		ModuleInfoManager::get()->registerModuleInfo(this);
 	}
-	virtual ~ModuleInfoBase() {ModuleInfoManager::get()->unregisterModuleInfo(this);}
+	virtual ~ModuleInfoBase() {
+		ModuleInfoManager::get()->unregisterModuleInfo(this);
+	}
 
-	const std::string &getModuleName() const {return mName;}
-	const std::string &getModuleHelp() const {return mHelp;}
-	const std::vector<std::string> &getAfter() const {return mAfter;}
-	unsigned int getOidIndex() const {return mOidIndex;}
-	ModuleClass getClass() const {return mClass;}
-	const std::string &getReplace() const {return mReplace;}
+	const std::string& getModuleName() const {
+		return mName;
+	}
+	const std::string& getModuleHelp() const {
+		return mHelp;
+	}
+	const std::vector<std::string>& getAfter() const {
+		return mAfter;
+	}
+	unsigned int getOidIndex() const {
+		return mOidIndex;
+	}
+	ModuleClass getClass() const {
+		return mClass;
+	}
+	const std::string& getReplace() const {
+		return mReplace;
+	}
 
-	const std::string &getFunction() const {return mReplace.empty() ? mName : mReplace;}
+	const std::string& getFunction() const {
+		return mReplace.empty() ? mName : mReplace;
+	}
 
 	virtual std::shared_ptr<Module> create(Agent* agent) = 0;
 
@@ -218,15 +234,19 @@ private:
 	std::string mReplace;
 };
 
-template<typename T>
+template <typename T>
 class ModuleInfo : public ModuleInfoBase {
 public:
 	using ModuleType = T;
 
-	ModuleInfo(
-		const std::string &moduleName, const std::string &help, const std::vector<std::string> &after,
-		ModuleOid oid, ModuleClass moduleClass = ModuleClass::Production, const std::string &replace = ""
-	) : ModuleInfoBase(moduleName, help, after, oid, moduleClass, replace) {}
+	ModuleInfo(const std::string& moduleName,
+	           const std::string& help,
+	           const std::vector<std::string>& after,
+	           ModuleOid oid,
+	           ModuleClass moduleClass = ModuleClass::Production,
+	           const std::string& replace = "")
+	    : ModuleInfoBase(moduleName, help, after, oid, moduleClass, replace) {
+	}
 
 	std::shared_ptr<Module> create(Agent* agent) override {
 		std::shared_ptr<Module> module = std::make_shared<T>(agent);
@@ -241,69 +261,67 @@ public:
 
 /**
  * Some useful routines any module can use by derivating from this class.
-**/
+ **/
 class ModuleToolbox {
 public:
-	static msg_auth_t *findAuthorizationForRealm(su_home_t *home, msg_auth_t *au, const char *realm);
-	static const tport_t *getIncomingTport(const std::shared_ptr<RequestSipEvent> &ev, Agent *agent);
+	static msg_auth_t* findAuthorizationForRealm(su_home_t* home, msg_auth_t* au, const char* realm);
+	static const tport_t* getIncomingTport(const std::shared_ptr<RequestSipEvent>& ev, Agent* agent);
 
-	static void addRecordRouteIncoming(Agent *agent, const std::shared_ptr<RequestSipEvent> &ev);
-	static void addRecordRoute(Agent *agent, const std::shared_ptr<RequestSipEvent> &ev, const tport_t *tport);
+	static void addRecordRouteIncoming(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev);
+	static void addRecordRoute(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev, const tport_t* tport);
 
-	static void cleanAndPrependRoute(Agent *agent, msg_t *msg, sip_t *sip, sip_route_t *route);
+	static void cleanAndPrependRoute(Agent* agent, msg_t* msg, sip_t* sip, sip_route_t* route);
 
-	static bool sipPortEquals(const char *p1, const char *p2, const char *transport = nullptr);
-	static int sipPortToInt(const char *port);
+	static bool sipPortEquals(const char* p1, const char* p2, const char* transport = nullptr);
+	static int sipPortToInt(const char* port);
 
-	static bool fromMatch(const sip_from_t *from1, const sip_from_t *from2);
-	static bool matchesOneOf(const std::string &item, const std::list<std::string> &set);
+	static bool fromMatch(const sip_from_t* from1, const sip_from_t* from2);
+	static bool matchesOneOf(const std::string& item, const std::list<std::string>& set);
 
-	static bool fixAuthChallengeForSDP(su_home_t *home, msg_t *msg, sip_t *sip);
-	static bool transportEquals(const char *tr1, const char *tr2);
-	static bool isNumeric(const char *host);
-	static bool isManagedDomain(const Agent *agent, const std::list<std::string> &domains, const url_t *url);
-	static void addRoutingParam(
-		su_home_t *home, sip_contact_t *contacts, const std::string &routingParam, const char *domain
-	);
-	static struct sip_route_s *prependNewRoutable(msg_t *msg, sip_t *sip, sip_route_t *&sipr, sip_route_t *value);
-	static void addPathHeader(
-		Agent *agent, const std::shared_ptr<RequestSipEvent> &ev, tport_t *tport, const char *uniq = nullptr
-	);
+	static bool fixAuthChallengeForSDP(su_home_t* home, msg_t* msg, sip_t* sip);
+	static bool transportEquals(const char* tr1, const char* tr2);
+	static bool isNumeric(const char* host);
+	static bool isManagedDomain(const Agent* agent, const std::list<std::string>& domains, const url_t* url);
+	static void
+	addRoutingParam(su_home_t* home, sip_contact_t* contacts, const std::string& routingParam, const char* domain);
+	static struct sip_route_s* prependNewRoutable(msg_t* msg, sip_t* sip, sip_route_t*& sipr, sip_route_t* value);
+	static void
+	addPathHeader(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev, tport_t* tport, const char* uniq = nullptr);
 
 	// These methods do host comparison taking into account that each one of argument can be an ipv6 address enclosed in
 	// brakets.
-	static bool urlHostMatch(const char *host1, const char *host2);
-	static bool urlHostMatch(const url_t *url, const char *host);
+	static bool urlHostMatch(const char* host1, const char* host2);
+	static bool urlHostMatch(const url_t* url, const char* host);
 	static bool urlHostMatch(const std::string& host1, const std::string& host2);
 
 	// Returns the host taking into account that if it is an ipv6 address, then brakets are removed.
-	static std::string getHost(const char *host);
+	static std::string getHost(const char* host);
 
-	static std::string urlGetHost(url_t *url);
-	static void urlSetHost(su_home_t *home, url_t *url, const char *host);
-	static bool urlIsResolved(url_t *uri);
+	static std::string urlGetHost(url_t* url);
+	static void urlSetHost(su_home_t* home, url_t* url, const char* host);
+	static bool urlIsResolved(url_t* uri);
 
-	//Returns true if via and url represent the same network address.
-	static bool urlViaMatch(const url_t *url, const sip_via_t *via, bool use_received_rport);
+	// Returns true if via and url represent the same network address.
+	static bool urlViaMatch(const url_t* url, const sip_via_t* via, bool use_received_rport);
 
 	// Returns true if the destination represented by url is present in the via chain.
-	static bool viaContainsUrl(const sip_via_t *vias, const url_t *url);
+	static bool viaContainsUrl(const sip_via_t* vias, const url_t* url);
 	// Returns true if the destination host contained in 'url' is present in via headers. This helps loop detection.
-	static bool viaContainsUrlHost(const sip_via_t *vias, const url_t *url);
-	
+	static bool viaContainsUrlHost(const sip_via_t* vias, const url_t* url);
+
 	/* Return the next hop by skipping possible Route headers pointing to this proxy.*/
-	static const url_t *getNextHop(Agent *ag, const sip_t *sip, bool *isRoute);
+	static const url_t* getNextHop(Agent* ag, const sip_t* sip, bool* isRoute);
 
 	// Returns true if the two url represent the same transport channel (IP, port and protocol).
-	static bool urlTransportMatch(const url_t *url1, const url_t *url2);
-	static std::string urlGetTransport(const url_t *url);
-	static void removeParamsFromContacts(su_home_t *home, sip_contact_t *c, std::list<std::string> &params);
-	static void removeParamsFromUrl(su_home_t *home, url_t *u, std::list<std::string> &params);
-	static sip_unknown_t *getCustomHeaderByName(const sip_t *sip, const char *name);
+	static bool urlTransportMatch(const url_t* url1, const url_t* url2);
+	static std::string urlGetTransport(const url_t* url);
+	static void removeParamsFromContacts(su_home_t* home, sip_contact_t* c, std::list<std::string>& params);
+	static void removeParamsFromUrl(su_home_t* home, url_t* u, std::list<std::string>& params);
+	static sip_unknown_t* getCustomHeaderByName(const sip_t* sip, const char* name);
 	static int getCpuCount();
-	static sip_via_t *getLastVia(sip_t *sip);
+	static sip_via_t* getLastVia(sip_t* sip);
 	/* same as url_make() from sofia, but unsure that the url is sip or sips; otherwise return NULL*/
-	static url_t *sipUrlMake(su_home_t *home, const char *value);
+	static url_t* sipUrlMake(su_home_t* home, const char* value);
 };
 
-}
+} // namespace flexisip
--- src/module-nat-helper.cc
+++ src/module-nat-helper.cc
@@ -27,16 +27,16 @@ using namespace flexisip;
 
 class NatHelper : public Module, protected ModuleToolbox {
 public:
-	NatHelper(Agent *ag) : Module(ag) {
+	NatHelper(Agent* ag) : Module(ag) {
 	}
 
 	~NatHelper() {
 	}
 
-	virtual void onRequest(shared_ptr<RequestSipEvent> &ev) {
+	virtual void onRequest(shared_ptr<RequestSipEvent>& ev) {
 		shared_ptr<MsgSip> ms = ev->getMsgSip();
-		sip_t *sip = ms->getSip();
-		sip_request_t *rq = sip->sip_request;
+		sip_t* sip = ms->getSip();
+		sip_request_t* rq = sip->sip_request;
 		/* if we receive a request whose first via is wrong (received or rport parameters are present),
 		fix any possible Contact headers with the same wrong ip address and ports */
 		fixContactFromVia(ms->getHome(), sip, sip->sip_via);
@@ -45,14 +45,13 @@ public:
 		if (rq->rq_method == sip_method_invite || rq->rq_method == sip_method_subscribe) {
 			if (sip->sip_to->a_tag == NULL) {
 				// fix potential record-route from a natted proxy added before us
-				if (mFixRecordRoutes)
-					fixRecordRouteInRequest(ms);
+				if (mFixRecordRoutes) fixRecordRouteInRequest(ms);
 				addRecordRouteIncoming(getAgent(), ev);
 			}
 		}
 		// fix potential Path header inserted before us by a flexisip natted proxy
 		if (rq->rq_method == sip_method_register && sip->sip_path &&
-			url_has_param(sip->sip_path->r_url, "fs-proxy-id")) {
+		    url_has_param(sip->sip_path->r_url, "fs-proxy-id")) {
 			// note: why limiting this to flexisip ? it could fix any path header, even without fs-proxy-id param.
 			fixPath(ms);
 		}
@@ -60,24 +59,26 @@ public:
 		// a Path header corresponding to this proxy
 	}
 
-	virtual void onResponse(shared_ptr<ResponseSipEvent> &ev) {
-		const shared_ptr<MsgSip> &ms = ev->getMsgSip();
-		sip_status_t *st = ms->getSip()->sip_status;
-		sip_cseq_t *cseq = ms->getSip()->sip_cseq;
+	virtual void onResponse(shared_ptr<ResponseSipEvent>& ev) {
+		const shared_ptr<MsgSip>& ms = ev->getMsgSip();
+		sip_status_t* st = ms->getSip()->sip_status;
+		sip_cseq_t* cseq = ms->getSip()->sip_cseq;
 		/*in responses that establish a dialog, masquerade Contact so that further requests (including the ACK) are
 		 * routed in the same way*/
 		if (cseq && (cseq->cs_method == sip_method_invite || cseq->cs_method == sip_method_subscribe)) {
 			if (st->st_status >= 200 && st->st_status <= 299) {
-				sip_contact_t *ct = ms->getSip()->sip_contact;
+				sip_contact_t* ct = ms->getSip()->sip_contact;
 				if (ct) {
-					if (!url_has_param(ct->m_url, mContactVerifiedParam.c_str()) && !url_has_param(ct->m_url,"gr") && !msg_params_find(ct->m_params, "isfocus")) {
+					if (!url_has_param(ct->m_url, mContactVerifiedParam.c_str()) && !url_has_param(ct->m_url, "gr") &&
+					    !msg_params_find(ct->m_params, "isfocus")) {
 						fixContactInResponse(ms->getHome(), ms->getMsg(), ms->getSip());
 						url_param_add(ms->getHome(), ct->m_url, mContactVerifiedParam.c_str());
-					} else if (ms->getSip()->sip_via && ms->getSip()->sip_via->v_next && !ms->getSip()->sip_via->v_next->v_next) {
+					} else if (ms->getSip()->sip_via && ms->getSip()->sip_via->v_next &&
+					           !ms->getSip()->sip_via->v_next->v_next) {
 						// Via contains client and first proxy
 						LOGD("Removing verified param from response contact");
 						ct->m_url->url_params = url_strip_param_string(su_strdup(ms->getHome(), ct->m_url->url_params),
-																		 mContactVerifiedParam.c_str());
+						                                               mContactVerifiedParam.c_str());
 					}
 				}
 			}
@@ -86,18 +87,18 @@ public:
 
 protected:
 	enum RecordRouteFixingPolicy { Safe, Always };
-	virtual void onDeclare(GenericStruct *module_config) {
+	virtual void onDeclare(GenericStruct* module_config) {
 		ConfigItemDescriptor items[] = {
-			{String, "contact-verified-param",
-			 "Internal URI parameter added to response contact by first proxy and cleaned by last one.", "verified"},
-			{Boolean, "fix-record-routes",
-			 "Fix record-routes, to workaround proxies behind firewalls but not aware of it.", "false"},
-			{String, "fix-record-routes-policy",
-			 "Policy to recognize nat'd record-route and fix them. There are two modes: 'safe' and 'always'", "safe"},
-			config_item_end};
+		    {String, "contact-verified-param",
+		     "Internal URI parameter added to response contact by first proxy and cleaned by last one.", "verified"},
+		    {Boolean, "fix-record-routes",
+		     "Fix record-routes, to workaround proxies behind firewalls but not aware of it.", "false"},
+		    {String, "fix-record-routes-policy",
+		     "Policy to recognize nat'd record-route and fix them. There are two modes: 'safe' and 'always'", "safe"},
+		    config_item_end};
 		module_config->addChildrenValues(items);
 	}
-	virtual void onLoad(const GenericStruct *sec) {
+	virtual void onLoad(const GenericStruct* sec) {
 		mContactVerifiedParam = sec->get<ConfigString>("contact-verified-param")->read();
 		mFixRecordRoutes = sec->get<ConfigBoolean>("fix-record-routes")->read();
 		string rr_policy = sec->get<ConfigString>("fix-record-routes-policy")->read();
@@ -112,72 +113,71 @@ protected:
 
 private:
 	string mContactVerifiedParam;
-	bool empty(const char *value) {
+	bool empty(const char* value) {
 		return value == NULL || value[0] == '\0';
 	}
-	void fixContactInResponse(su_home_t *home, msg_t *msg, sip_t *sip) {
-		const su_addrinfo_t *ai = msg_addrinfo(msg);
-		const sip_via_t *via = sip->sip_via;
-		const char *via_transport = sip_via_transport(via);
+	void fixContactInResponse(su_home_t* home, msg_t* msg, sip_t* sip) {
+		const su_addrinfo_t* ai = msg_addrinfo(msg);
+		const sip_via_t* via = sip->sip_via;
+		const char* via_transport = sip_via_transport(via);
 		char ct_transport[20] = {0};
 		if (ai != NULL) {
 			char ip[NI_MAXHOST];
 			char port[NI_MAXSERV];
-			int err = getnameinfo(ai->ai_addr, ai->ai_addrlen, ip, sizeof(ip), port, sizeof(port), NI_NUMERICHOST | NI_NUMERICSERV);
+			int err = getnameinfo(ai->ai_addr, ai->ai_addrlen, ip, sizeof(ip), port, sizeof(port),
+			                      NI_NUMERICHOST | NI_NUMERICSERV);
 			if (err != 0) {
 				LOGE("getnameinfo() error: %s", gai_strerror(err));
 			} else {
-				sip_contact_t *ctt = sip->sip_contact;
+				sip_contact_t* ctt = sip->sip_contact;
 				if (ctt && ctt->m_url->url_host) {
 					if (!ModuleToolbox::urlHostMatch(ctt->m_url, ip) || !sipPortEquals(ctt->m_url->url_port, port)) {
 						LOGD("Response is coming from %s:%s, fixing contact", ip, port);
 						ModuleToolbox::urlSetHost(home, ctt->m_url, ip);
 						ctt->m_url->url_port = su_strdup(home, port);
-					} else
-						LOGD("Contact in response is correct.");
+					} else LOGD("Contact in response is correct.");
 					url_param(ctt->m_url->url_params, "transport", ct_transport, sizeof(ct_transport) - 1);
 					fixTransport(home, ctt->m_url, via_transport);
 				}
 			}
 		}
 	}
-	void fixContactFromVia(su_home_t *home, sip_t *msg, const sip_via_t *via) {
-		sip_contact_t *ctt = msg->sip_contact;
-		const char *received = via->v_received;
-		const char *rport = via->v_rport;
-		const char *via_transport = sip_via_transport(via);
+	void fixContactFromVia(su_home_t* home, sip_t* msg, const sip_via_t* via) {
+		sip_contact_t* ctt = msg->sip_contact;
+		const char* received = via->v_received;
+		const char* rport = via->v_rport;
+		const char* via_transport = sip_via_transport(via);
 		bool is_frontend = (via->v_next == NULL); /*true if we are the first proxy the request is walking through*/
 		bool single_contact = (ctt != NULL && ctt->m_next == NULL);
 
-		if (empty(received) && empty(rport))
-			return; /*nothing to do*/
+		if (empty(received) && empty(rport)) return; /*nothing to do*/
 
 		if (empty(received)) {
 			/*case where the rport is not empty  but received is empty (because the host was correct)*/
 			received = via->v_host;
 		}
 
-		if (rport == NULL)
-			rport = via->v_port; // if no rport is given, then trust the via port.
+		if (rport == NULL) rport = via->v_port; // if no rport is given, then trust the via port.
 
 		for (; ctt != NULL; ctt = ctt->m_next) {
 			if (ctt->m_url->url_host) {
-				const char *host = ctt->m_url->url_host;
+				const char* host = ctt->m_url->url_host;
 				char ct_transport[20] = {0};
-				if (url_has_param(ctt->m_url,"gr")) {
-					SLOGD << "Gruu found in contact header ["<<ctt<<"] for message ["<< msg << "] skipping nat fixing process for contact";
+				if (url_has_param(ctt->m_url, "gr")) {
+					SLOGD << "Gruu found in contact header [" << ctt << "] for message [" << msg
+					      << "] skipping nat fixing process for contact";
 					continue;
 				}
 				url_param(ctt->m_url->url_params, "transport", ct_transport, sizeof(ct_transport) - 1);
-				// If we have a single contact and we are the front-end proxy, or if we found a ip:port in a contact that
-				// seems incorrect because the same appeared fixed in the via, then fix it.
+				// If we have a single contact and we are the front-end proxy, or if we found a ip:port in a contact
+				// that seems incorrect because the same appeared fixed in the via, then fix it.
 				if ((is_frontend && single_contact) || (ModuleToolbox::urlHostMatch(host, via->v_host) &&
-														sipPortEquals(ctt->m_url->url_port, via->v_port) &&
-														transportEquals(via_transport, ct_transport))) {
+				                                        sipPortEquals(ctt->m_url->url_port, via->v_port) &&
+				                                        transportEquals(via_transport, ct_transport))) {
 
 					if (!ModuleToolbox::urlHostMatch(host, received) || !sipPortEquals(ctt->m_url->url_port, rport)) {
 						LOGD("Fixing contact header with %s:%s to %s:%s", ctt->m_url->url_host,
-							 ctt->m_url->url_port ? ctt->m_url->url_port : "", received, rport ? rport : "");
+						     ctt->m_url->url_port ? ctt->m_url->url_port : "", received, rport ? rport : "");
 						ModuleToolbox::urlSetHost(home, ctt->m_url, received);
 						ctt->m_url->url_port = rport;
 					}
@@ -186,85 +186,69 @@ private:
 			}
 		}
 	}
-	void fixTransport(su_home_t *home, url_t *url, const char *transport) {
+	void fixTransport(su_home_t* home, url_t* url, const char* transport) {
 		if (url_has_param(url, "transport")) {
 			url->url_params = url_strip_param_string(su_strdup(home, url->url_params), "transport");
 		}
 		if (url->url_type != url_sips) {
-			const char *url_transport = NULL;
-			if (strcasecmp(transport, "TCP") == 0)
-				url_transport = "tcp";
-			else if (strcasecmp(transport, "TLS") == 0)
-				url_transport = "tls";
-			if (url_transport)
-				url_param_add(home, url, su_sprintf(home, "transport=%s", url_transport));
+			const char* url_transport = NULL;
+			if (strcasecmp(transport, "TCP") == 0) url_transport = "tcp";
+			else if (strcasecmp(transport, "TLS") == 0) url_transport = "tls";
+			if (url_transport) url_param_add(home, url, su_sprintf(home, "transport=%s", url_transport));
 		}
 	}
-	void fixPath(shared_ptr<MsgSip> &ms) {
-		sip_t *sip = ms->getSip();
-		const sip_via_t *via = sip->sip_via;
-		const char *received = via->v_received;
-		const char *rport = via->v_rport;
-		const char *transport = sip_via_transport(via);
+	void fixPath(shared_ptr<MsgSip>& ms) {
+		sip_t* sip = ms->getSip();
+		const sip_via_t* via = sip->sip_via;
+		const char* received = via->v_received;
+		const char* rport = via->v_rport;
+		const char* transport = sip_via_transport(via);
 
-		url_t *path = sip->sip_path->r_url;
-		if (empty(received))
-			received = via->v_host;
-		if (!rport)
-			rport = via->v_port;
-		if (!transport)
-			transport = "udp";
+		url_t* path = sip->sip_path->r_url;
+		if (empty(received)) received = via->v_host;
+		if (!rport) rport = via->v_port;
+		if (!transport) transport = "udp";
 		ModuleToolbox::urlSetHost(ms->getHome(), path, received);
 		path->url_port = rport;
 		fixTransport(ms->getHome(), path, transport);
 	}
-	bool isPrivateAddress(const char *host) {
+	bool isPrivateAddress(const char* host) {
 		return strstr(host, "10.") == host || strstr(host, "192.168.") == host || strstr(host, "176.12.") == host;
 	}
-	void fixRecordRouteInRequest(shared_ptr<MsgSip> &ms) {
-		sip_t *sip = ms->getSip();
+	void fixRecordRouteInRequest(shared_ptr<MsgSip>& ms) {
+		sip_t* sip = ms->getSip();
 		if (sip->sip_record_route) {
 			if (mRRPolicy == Safe) {
 				if (urlViaMatch(sip->sip_record_route->r_url, sip->sip_via, false)) {
-					const char *transport = sip_via_transport(sip->sip_via);
+					const char* transport = sip_via_transport(sip->sip_via);
 					LOGD("Record-route and via are matching.");
 					if (sip->sip_via->v_received) {
 						LOGD("This record-route needs to be fixed for host");
-						url_param_add(
-							ms->getHome(),
-							sip->sip_record_route->r_url,
-							su_sprintf(ms->getHome(), "fs-received=%s", sip->sip_via->v_received)
-						);
+						url_param_add(ms->getHome(), sip->sip_record_route->r_url,
+						              su_sprintf(ms->getHome(), "fs-received=%s", sip->sip_via->v_received));
 					}
 					if (sip->sip_via->v_rport) {
 						LOGD("This record-route needs to be fixed for port");
-						url_param_add(
-							ms->getHome(),
-							sip->sip_record_route->r_url,
-							su_sprintf(ms->getHome(), "fs-rport=%s", sip->sip_via->v_rport)
-						);
+						url_param_add(ms->getHome(), sip->sip_record_route->r_url,
+						              su_sprintf(ms->getHome(), "fs-rport=%s", sip->sip_via->v_rport));
 					}
 					fixTransport(ms->getHome(), sip->sip_record_route->r_url, transport);
 				}
 			} else {
-				const char *host = sip->sip_record_route->r_url->url_host;
+				const char* host = sip->sip_record_route->r_url->url_host;
 				if (host && isPrivateAddress(host)) {
-					const char *transport = sip_via_transport(sip->sip_via);
-					const char *received = sip->sip_via->v_received ? sip->sip_via->v_received : sip->sip_via->v_host;
-					const char *rport = sip->sip_via->v_rport ? sip->sip_via->v_rport : sip->sip_via->v_port;
+					const char* transport = sip_via_transport(sip->sip_via);
+					const char* received = sip->sip_via->v_received ? sip->sip_via->v_received : sip->sip_via->v_host;
+					const char* rport = sip->sip_via->v_rport ? sip->sip_via->v_rport : sip->sip_via->v_port;
 					if (!ModuleToolbox::urlHostMatch(received, host)) {
 						LOGD("This record-route needs to be fixed for host");
-						url_param_add(
-							ms->getHome(), sip->sip_record_route->r_url,
-							su_sprintf(ms->getHome(), "fs-received=%s", received)
-						);
+						url_param_add(ms->getHome(), sip->sip_record_route->r_url,
+						              su_sprintf(ms->getHome(), "fs-received=%s", received));
 					}
 					if (!sipPortEquals(rport, sip->sip_record_route->r_url->url_port, transport)) {
 						LOGD("This record-route needs to be fixed for port");
-						url_param_add(
-							ms->getHome(), sip->sip_record_route->r_url,
-							su_sprintf(ms->getHome(), "fs-rport=%s", rport)
-						);
+						url_param_add(ms->getHome(), sip->sip_record_route->r_url,
+						              su_sprintf(ms->getHome(), "fs-rport=%s", rport));
 					}
 					fixTransport(ms->getHome(), sip->sip_record_route->r_url, transport);
 				}
@@ -277,10 +261,9 @@ private:
 };
 
 ModuleInfo<NatHelper> NatHelper::sInfo(
-	"NatHelper",
-	"The NatHelper module executes small tasks to make SIP work smoothly despite firewalls. It corrects the Contact "
-	"headers that contain obviously inconsistent addresses, and adds a Record-Route to ensure subsequent requests are "
-	"routed also by the proxy, through the same UDP or TCP channel used for the initial request.",
-	{ "GarbageIn" },
-	ModuleInfoBase::ModuleOid::NatHelper
-);
+    "NatHelper",
+    "The NatHelper module executes small tasks to make SIP work smoothly despite firewalls. It corrects the Contact "
+    "headers that contain obviously inconsistent addresses, and adds a Record-Route to ensure subsequent requests are "
+    "routed also by the proxy, through the same UDP or TCP channel used for the initial request.",
+    {"GarbageIn"},
+    ModuleInfoBase::ModuleOid::NatHelper);
