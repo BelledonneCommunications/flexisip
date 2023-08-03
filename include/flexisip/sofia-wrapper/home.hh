d8b46061394acad380e836ff3f5aaaf58bc10585
Thibault Lemaire <thibault.lemaire@belledonne-communications.com>
2023-08-03 10:27:20 +0200

    clang-format
    (cherry picked from commit 50e200672985215c201cbc2727fcfba3be851a67)

--- include/flexisip/sofia-wrapper/home.hh
+++ include/flexisip/sofia-wrapper/home.hh
@@ -1,19 +1,19 @@
 /*
-	Flexisip, a flexible SIP proxy server with media capabilities.
-	Copyright (C) 2010-2020  Belledonne Communications SARL, All rights reserved.
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2023 Belledonne Communications SARL, All rights reserved.
 
-	This program is free software: you can redistribute it and/or modify
-	it under the terms of the GNU Affero General Public License as
-	published by the Free Software Foundation, either version 3 of the
-	License, or (at your option) any later version.
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
 
-	This program is distributed in the hope that it will be useful,
-	but WITHOUT ANY WARRANTY; without even the implied warranty of
-	MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-	GNU Affero General Public License for more details.
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    GNU Affero General Public License for more details.
 
-	You should have received a copy of the GNU Affero General Public License
-	along with this program.  If not, see <http://www.gnu.org/licenses/>.
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
 #pragma once
@@ -29,20 +29,30 @@ namespace sofiasip {
  */
 class Home {
 public:
-	Home() noexcept {su_home_init(&mHome);}
-	Home(const Home &src) = delete;
-	Home(Home &&src) noexcept : Home() {su_home_move(&mHome, &src.mHome);}
-	~Home() noexcept {su_home_deinit(&mHome);}
+	Home() noexcept {
+		su_home_init(&mHome);
+	}
+	Home(const Home& src) = delete;
+	Home(Home&& src) noexcept : Home() {
+		su_home_move(&mHome, &src.mHome);
+	}
+	~Home() noexcept {
+		su_home_deinit(&mHome);
+	}
 
-	Home &operator=(const Home &src) = delete;
-	Home &operator=(Home &&src) noexcept {
+	Home& operator=(const Home& src) = delete;
+	Home& operator=(Home&& src) noexcept {
 		reset();
 		su_home_move(&mHome, &src.mHome);
 		return *this;
 	}
 
-	su_home_t* home() noexcept {return &mHome;}
-	const su_home_t* home() const noexcept {return &mHome;}
+	su_home_t* home() noexcept {
+		return &mHome;
+	}
+	const su_home_t* home() const noexcept {
+		return &mHome;
+	}
 
 	// Free all the buffers which are referenced by this Home.
 	void reset() noexcept {
@@ -50,13 +60,18 @@ public:
 		su_home_init(&mHome);
 	}
 
-	void* alloc(std::size_t size) noexcept {return su_alloc(&mHome, size);}
-	void free(void *data) noexcept {return su_free(&mHome, data);}
+	void* alloc(std::size_t size) noexcept {
+		return su_alloc(&mHome, size);
+	}
+	void free(void* data) noexcept {
+		return su_free(&mHome, data);
+	}
 
 	char* vsprintf(char const* fmt, va_list ap) noexcept {
 		return su_vsprintf(&mHome, fmt, ap);
 	}
-	template <typename... Args> char* sprintf(const char* fmt, Args&&... args) noexcept {
+	template <typename... Args>
+	char* sprintf(const char* fmt, Args&&... args) noexcept {
 		return su_sprintf(&mHome, fmt, args...);
 	}
 
@@ -64,4 +79,4 @@ private:
 	su_home_t mHome{};
 };
 
-} // end of sofiasip namespace
+} // namespace sofiasip
