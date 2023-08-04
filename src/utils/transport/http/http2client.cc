6b5b1b4b96669302d8f6e9ea25b6af692c2090bd
Anthony Gauchy <anthony.gauchy@belledonne-communications.com>
2023-08-04 09:49:14 +0200

    HTTP2 client - stop poll in on disconnect

--- src/utils/transport/http/http2client.cc
+++ src/utils/transport/http/http2client.cc
@@ -338,10 +338,12 @@ int Http2Client::onPollInCb(su_root_magic_t*, su_wait_t* w, su_wakeup_arg_t* arg
 	if (w->revents & SU_WAIT_HUP) {
 		SLOGD << thiz->mLogPrefix << ": peer has hung up";
 		thiz->disconnect();
+		return 0;
 	}
 	if (w->revents & SU_WAIT_ERR) {
 		SLOGE << thiz->mLogPrefix << ": socket error";
 		thiz->disconnect();
+		return 0;
 	}
 
 	auto status = nghttp2_session_recv(thiz->mHttpSession.get());
