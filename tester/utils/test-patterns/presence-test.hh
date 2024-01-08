a261f6275a6390637feec8dc5c8eefda19d4d96f
Émilie Gerbaud <emilie.gerbaud@belledonne-communications.com>
2024-01-08 15:22:08 +0100

    tests: remove shadowing from base class members

--- tester/utils/test-patterns/presence-test.hh
+++ tester/utils/test-patterns/presence-test.hh
@@ -1,6 +1,6 @@
 /*
     Flexisip, a flexible SIP proxy server with media capabilities.
-    Copyright (C) 2010-2023 Belledonne Communications SARL, All rights reserved.
+    Copyright (C) 2010-2024 Belledonne Communications SARL, All rights reserved.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -77,8 +77,6 @@ protected:
 	}
 
 	// Protected attributes
-	std::shared_ptr<sofiasip::SuRoot> mRoot{std::make_shared<sofiasip::SuRoot>()};
-	std::shared_ptr<Agent> mAgent{std::make_shared<Agent>(mRoot)};
 	std::shared_ptr<PresenceServer> mPresence{nullptr};
 };
 
