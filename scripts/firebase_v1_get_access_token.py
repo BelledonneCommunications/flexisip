ccf3b2138a1aea1964a5331eed6f86b1a1d58b6b
Félix Olart <felix.olart@belledonne-communications.com>
2024-11-06 17:17:20 +0100

    fix: warnings print in output (modules import) breaks parsing (FirebaseV1 get access token)

--- scripts/firebase_v1_get_access_token.py
+++ scripts/firebase_v1_get_access_token.py
@@ -10,11 +10,11 @@
 #
 #  This program is distributed in the hope that it will be useful,
 #  but WITHOUT ANY WARRANTY; without even the implied warranty of
-#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+#  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
 #  GNU Affero General Public License for more details.
-
-# You should have received a copy of the GNU Affero General Public License
-# along with this program.  If not, see <http://www.gnu.org/licenses/>.
+#
+#  You should have received a copy of the GNU Affero General Public License
+#  along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 """
 Retrieve a valid access token that can be used to authorize requests.
@@ -22,92 +22,104 @@ Lifetime is expressed in seconds.
 
 All outputs of this script are printed in the standard output and formatted in JSON.
 The structure is as follows: {"state": str ['ERROR', 'SUCCESS'], "data": obj, "warnings": list[str]}
-    On success: data = {"token": str, "lifetime": int}
-    On error:   data = {"message": str}
+	On success: data = {"token": str, "lifetime": int}
+	On error:   data = {"message": str}
 """
 
-try:
-    import sys
-    import json
-    import datetime
-    import argparse
-    from pathlib import Path
-    import warnings as warnlib
+import warnings as warnlib
 
-    # https://github.com/googleapis/google-auth-library-python
-    import google.auth.transport.requests
-    from google.oauth2 import service_account
+warnlib.simplefilter("ignore")
+
+try:
+	import sys
+	import json
+	import datetime
+	import argparse
+	from pathlib import Path
+
+	# https://github.com/googleapis/google-auth-library-python
+	import google.auth.transport.requests
+	from google.oauth2 import service_account
 
 except BaseException as exception:
 
-    print(f'{{"state": "ERROR", "data": {{"message": "{exception}"}}, "warnings": {[]}}}')
-    sys.exit(0)
+	print(f'{{"state": "ERROR", "data": {{"message": "{exception}"}}, "warnings": {[]}}}')
+	sys.exit(0)
 
 
 class ScriptState:
-    ERROR = "ERROR"
-    SUCCESS = "SUCCESS"
+	ERROR = "ERROR"
+	SUCCESS = "SUCCESS"
 
 
-def error(message: str) -> None:
-    data = {
-        "state": ScriptState.ERROR,
-        "data": {
-            "message": message,
-        },
-        "warnings": [],
-    }
-    print(json.dumps(data))
+def error(message: str, warnings: list[str]) -> None:
+	payload = {
+		"state": ScriptState.ERROR,
+		"data": {
+			"message": message,
+		},
+		"warnings": warnings,
+	}
+	print(json.dumps(payload))
+
+
+def extract_warnings(warnings: list[warnlib.WarningMessage]) -> list[str]:
+	return [f"{warning.message}" for warning in warnings]
 
 
 SCOPES = ['https://www.googleapis.com/auth/firebase.messaging']
 
 if __name__ == "__main__":
 
-    with warnlib.catch_warnings(record=True) as warnings:
-        warnlib.simplefilter("always")
+	with warnlib.catch_warnings(record=True) as warnings:
+		warnlib.simplefilter("always")
 
-        parser = argparse.ArgumentParser(
-            prog="FirebaseV1 access token provider",
-            description="Try to get Firebase OAuth2 access token for the given service account",
-            epilog=""
-        )
+		parser = argparse.ArgumentParser(
+			prog="FirebaseV1 access token provider",
+			description="Try to get Firebase OAuth2 access token for the given service account",
+			epilog=""
+		)
+		parser.add_argument(
+			"-f",
+			"--filename",
+			type=Path,
+			dest="filename",
+			help="path to the service account json file",
+			required=True
+		)
+		arguments = parser.parse_args()
 
-        parser.add_argument("-f", "--filename", type=Path, dest="filename",
-                            help="path to the service account json file", required=True)
-        arguments = parser.parse_args()
+		if not arguments.filename.exists() or not arguments.filename.is_file():
+			error(f"path to service account json file is not valid ({arguments.filename})", extract_warnings(warnings))
+			sys.exit(0)
 
-        if not arguments.filename.exists() or not arguments.filename.is_file():
-            error(f"path to service account json file is not valid ({arguments.filename})")
-            sys.exit(0)
+		try:
+			credentials = service_account.Credentials.from_service_account_file(arguments.filename, scopes=SCOPES)
+			request = google.auth.transport.requests.Request()
+			credentials.refresh(request)
 
-        try:
+			# Make expiry an offset-aware datetime.
+			expiry = credentials.expiry.replace(tzinfo=datetime.timezone.utc)
+			now = datetime.datetime.now(datetime.timezone.utc)
+			lifetime = int((expiry - now).total_seconds())
 
-            credentials = service_account.Credentials.from_service_account_file(arguments.filename, scopes=SCOPES)
-            request = google.auth.transport.requests.Request()
-            credentials.refresh(request)
+			if lifetime <= 0:
+				error(f"computed token lifetime is negative or null ({lifetime}s) [now = {now}, expiry = {expiry}]",
+					  extract_warnings(warnings))
+				sys.exit(0)
 
-            # Make expiry an offset-aware datetime.
-            expiry = credentials.expiry.replace(tzinfo=datetime.timezone.utc)
-            now = datetime.datetime.now(datetime.timezone.utc)
-            lifetime = int((expiry - now).total_seconds())
+			data = {
+				"state": ScriptState.SUCCESS,
+				"data": {
+					"token": credentials.token,
+					"lifetime": lifetime,
+				},
+				"warnings": extract_warnings(warnings),
+			}
+			print(json.dumps(data))
 
-            if lifetime <= 0:
-                error(f"computed token lifetime is negative or null ({lifetime}s) [now = {now}, expiry = {expiry}]")
-                sys.exit(0)
+		except SystemExit:
+			pass
 
-            data = {
-                "state": ScriptState.SUCCESS,
-                "data": {
-                    "token": credentials.token,
-                    "lifetime": lifetime,
-                },
-                "warnings": [f"{warning.message}" for warning in warnings],
-            }
-            print(json.dumps(data))
-
-        except SystemExit:
-            pass
-
-        except BaseException as exception:
-            error(f"{exception}")
+		except BaseException as exception:
+			error(f"{exception}", extract_warnings(warnings))
