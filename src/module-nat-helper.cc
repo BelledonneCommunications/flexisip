BelledonneCommunications/flexisip

commit b051e9a6af05f499873e1f65effa32ff12e9c350

Add new strategy for NatHelper to route requests through NATs: flow-token

src/module-nat-helper.cc
@@ -18,9 +18,9 @@
 
 #include "module-nat-helper.hh"
 
-#include <sofia-sip/msg_addr.h>
-
 #include "agent.hh"
+#include "eventlogs/writers/event-log-writer.hh"
+#include "nat/nat-traversal-strategy.hh"
 
 using namespace std;
 using namespace flexisip;
@@ -29,198 +29,55 @@ NatHelper::NatHelper(Agent* ag, const ModuleInfoBase* moduleInfo) : Module(ag, m
 }
 
 void NatHelper::onRequest(shared_ptr<RequestSipEvent>& ev) {
-	shared_ptr<MsgSip> ms = ev->getMsgSip();
-	sip_t* sip = ms->getSip();
-	sip_request_t* rq = sip->sip_request;
-	/* if we receive a request whose first via is wrong (received or rport parameters are present),
-	fix any possible Contact headers with the same wrong ip address and ports */
-	if (needToBeFixed(ev)) fixContactFromVia(ms->getHome(), sip, sip->sip_via);
-
-	// processing of requests that may establish a dialog.
-	if (rq->rq_method == sip_method_invite || rq->rq_method == sip_method_subscribe) {
-		if (sip->sip_to->a_tag == NULL) {
-			// fix potential record-route from a natted proxy added before us
-			if (mFixRecordRoutes) fixRecordRouteInRequest(ms);
-			addRecordRouteIncoming(getAgent(), ev);
-		}
-	}
-	// fix potential Path header inserted before us by a flexisip natted proxy
-	if (rq->rq_method == sip_method_register && sip->sip_path && url_has_param(sip->sip_path->r_url, "fs-proxy-id")) {
-		// note: why limiting this to flexisip ? it could fix any path header, even without fs-proxy-id param.
-		fixPath(ms);
-	}
-	// Idea for future: for the case where a natted proxy forwards a REGISTER (which can be detected , we could add
-	// a Path header corresponding to this proxy
-}
-
-/* TODO: Fixing contacts in responses is unreliable: we can't know if we are the first hop of the response.
- * This feature should be removed from Flexisip.
- */
-void NatHelper::onResponse(shared_ptr<ResponseSipEvent>& ev) {
 	const auto& ms = ev->getMsgSip();
-	const auto sip = ms->getSip();
-	const auto home = ev->getHome();
-	const auto* st = ms->getSip()->sip_status;
-	const auto* cseq = ms->getSip()->sip_cseq;
+	const auto* sip = ms->getSip();
+	const auto* path = sip->sip_path;
+	const auto rqMethod = ms->getSipMethod();
+	const auto& strategy = mAgent->getNatTraversalStrategy();
 
-	/* In responses that establish a dialog, masquerade Contact so that further requests (including the ACK) are
-	 * routed in the same way */
-	if (cseq && (cseq->cs_method == sip_method_invite || cseq->cs_method == sip_method_subscribe)) {
-		if (st->st_status >= 200 && st->st_status <= 299) {
-			const auto ct = sip->sip_contact;
-			if (ct) {
-				if (needToBeFixed(ev)) {
-					fixContactInResponse(home, ms->getMsg(), sip);
-				}
+	strategy->preProcessOnRequestNatHelper(ev);
 
-				if (sip->sip_via && sip->sip_via->v_next && !sip->sip_via->v_next->v_next /* is last hop */) {
-					if (url_has_param(ct->m_url, mContactVerifiedParam.c_str()) /* is verified */) {
-						// Via contains client and first proxy
-						LOGD("Removing \"verified\" parameter from response contact");
-						ct->m_url->url_params = url_strip_param_string(su_strdup(home, ct->m_url->url_params),
-						                                               mContactVerifiedParam.c_str());
-					}
-					return;
-				}
-
-				/* The "verified" parameter must be added whenever we fix or not the Contact, in order to signal
-				 * other nodes processing this response that the contact has been processed already. */
-				url_param_add(home, ct->m_url, mContactVerifiedParam.c_str());
-			}
+	// Processing of requests that may establish a dialog.
+	if ((rqMethod == sip_method_invite or rqMethod == sip_method_subscribe) and sip->sip_to->a_tag == nullptr) {
+		// Fix potential record-route from a "NATed" proxy added before us.
+		if (mFixRecordRoutes) {
+			fixRecordRouteInRequest(ms);
 		}
+
+		strategy->addRecordRouteNatHelper(ev);
 	}
+
+	// Fix potential "Path" header inserted before us by a flexisip "NATed" proxy.
+	if (rqMethod == sip_method_register and path != nullptr and url_has_param(path->r_url, "fs-proxy-id")) {
+		// Note: why limiting this to Flexisip? It could fix any path header, even without fs-proxy-id param.
+		NatTraversalStrategy::Helper::fixPath(ms);
+	}
+
+	// Idea for future: for the case where a "NATed" proxy forwards a REGISTER (which can be detected), we could
+	// add a "Path" header corresponding to this proxy.
 }
 
-bool NatHelper::needToBeFixed(const shared_ptr<SipEvent>& ev) {
-	const shared_ptr<MsgSip>& ms = ev->getMsgSip();
-	sip_contact_t* ct = ms->getSip()->sip_contact;
-	tport_t* primary = tport_parent(ev->getIncomingTport().get());
-	return ct && !url_has_param(ct->m_url, mContactVerifiedParam.c_str()) && !url_has_param(ct->m_url, "gr") &&
-	       !msg_params_find(ct->m_params, "isfocus") && getAgent()->getInternalTport() != primary;
+void NatHelper::onResponse(shared_ptr<ResponseSipEvent>& ev) {
+	mAgent->getNatTraversalStrategy()->onResponseNatHelper(ev);
 }
 
 void NatHelper::onLoad(const GenericStruct* sec) {
-	mContactVerifiedParam = sec->get<ConfigString>("contact-verified-param")->read();
 	mFixRecordRoutes = sec->get<ConfigBoolean>("fix-record-routes")->read();
-	string rr_policy = sec->get<ConfigString>("fix-record-routes-policy")->read();
+	const auto& rr_policy = sec->get<ConfigString>("fix-record-routes-policy")->read();
 	if (rr_policy == "safe") {
 		mRRPolicy = Safe;
 	} else if (rr_policy == "always") {
 		mRRPolicy = Always;
 	} else {
-		LOGF("NatHelper: Unsupported value '%s' for fix-record-routes-policy parameter", rr_policy.c_str());
+		LOGF("NatHelper: unsupported value '%s' for fix-record-routes-policy parameter", rr_policy.c_str());
 	}
-}
-
-bool NatHelper::empty(const char* value) {
-	return value == NULL || value[0] == '\0';
 }
 
 bool NatHelper::isPrivateAddress(const char* host) {
 	return strstr(host, "10.") == host || strstr(host, "192.168.") == host || strstr(host, "176.12.") == host;
 }
 
-void NatHelper::fixPath(shared_ptr<MsgSip>& ms) {
-	sip_t* sip = ms->getSip();
-	const sip_via_t* via = sip->sip_via;
-	const char* received = via->v_received;
-	const char* rport = via->v_rport;
-	const char* transport = sip_via_transport(via);
-
-	url_t* path = sip->sip_path->r_url;
-	if (empty(received)) received = via->v_host;
-	if (!rport) rport = via->v_port;
-	if (!transport) transport = "udp";
-	ModuleToolbox::urlSetHost(ms->getHome(), path, received);
-	path->url_port = rport;
-	fixTransport(ms->getHome(), path, transport);
-}
-
-void NatHelper::fixContactInResponse(su_home_t* home, msg_t* msg, sip_t* sip) {
-	const su_addrinfo_t* ai = msg_addrinfo(msg);
-	const sip_via_t* via = sip->sip_via;
-	const char* via_transport = sip_via_transport(via);
-	char ct_transport[20] = {0};
-	if (ai != NULL) {
-		char ip[NI_MAXHOST];
-		char port[NI_MAXSERV];
-		int err = getnameinfo(ai->ai_addr, ai->ai_addrlen, ip, sizeof(ip), port, sizeof(port),
-		                      NI_NUMERICHOST | NI_NUMERICSERV);
-		if (err != 0) {
-			LOGE("getnameinfo() error: %s", gai_strerror(err));
-		} else {
-			sip_contact_t* ctt = sip->sip_contact;
-			if (ctt && ctt->m_url->url_host) {
-				if (!ModuleToolbox::urlHostMatch(ctt->m_url, ip) || !sipPortEquals(ctt->m_url->url_port, port)) {
-					LOGD("Response is coming from %s:%s, fixing contact", ip, port);
-					ModuleToolbox::urlSetHost(home, ctt->m_url, ip);
-					ctt->m_url->url_port = su_strdup(home, port);
-				} else LOGD("Contact in response is correct.");
-				url_param(ctt->m_url->url_params, "transport", ct_transport, sizeof(ct_transport) - 1);
-				fixTransport(home, ctt->m_url, via_transport);
-			}
-		}
-	}
-}
-
-void NatHelper::fixTransport(su_home_t* home, url_t* url, const char* transport) {
-	if (url_has_param(url, "transport")) {
-		url->url_params = url_strip_param_string(su_strdup(home, url->url_params), "transport");
-	}
-	if (url->url_type != url_sips) {
-		const char* url_transport = NULL;
-		if (strcasecmp(transport, "TCP") == 0) url_transport = "tcp";
-		else if (strcasecmp(transport, "TLS") == 0) url_transport = "tls";
-		if (url_transport) url_param_add(home, url, su_sprintf(home, "transport=%s", url_transport));
-	}
-}
-
-void NatHelper::fixContactFromVia(su_home_t* home, sip_t* msg, const sip_via_t* via) {
-	sip_contact_t* ctt = msg->sip_contact;
-	const char* received = via->v_received;
-	const char* rport = via->v_rport;
-	const char* via_transport = sip_via_transport(via);
-	bool is_frontend = (via->v_next == NULL); /*true if we are the first proxy the request is walking through*/
-	bool single_contact = (ctt != NULL && ctt->m_next == NULL);
-
-	if (empty(received) && empty(rport)) return; /*nothing to do*/
-
-	if (empty(received)) {
-		/*case where the rport is not empty  but received is empty (because the host was correct)*/
-		received = via->v_host;
-	}
-
-	if (rport == NULL) rport = via->v_port; // if no rport is given, then trust the via port.
-
-	for (; ctt != NULL; ctt = ctt->m_next) {
-		if (ctt->m_url->url_host) {
-			const char* host = ctt->m_url->url_host;
-			char ct_transport[20] = {0};
-			if (url_has_param(ctt->m_url, "gr")) {
-				SLOGD << "Gruu found in contact header [" << ctt << "] for message [" << msg
-				      << "] skipping nat fixing process for contact";
-				continue;
-			}
-			url_param(ctt->m_url->url_params, "transport", ct_transport, sizeof(ct_transport) - 1);
-			// If we have a single contact and we are the front-end proxy, or if we found a ip:port in a contact
-			// that seems incorrect because the same appeared fixed in the via, then fix it.
-			if ((is_frontend && single_contact) ||
-			    (ModuleToolbox::urlHostMatch(host, via->v_host) && sipPortEquals(ctt->m_url->url_port, via->v_port) &&
-			     transportEquals(via_transport, ct_transport))) {
-
-				if (!ModuleToolbox::urlHostMatch(host, received) || !sipPortEquals(ctt->m_url->url_port, rport)) {
-					LOGD("Fixing contact header with %s:%s to %s:%s", ctt->m_url->url_host,
-					     ctt->m_url->url_port ? ctt->m_url->url_port : "", received, rport ? rport : "");
-					ModuleToolbox::urlSetHost(home, ctt->m_url, received);
-					ctt->m_url->url_port = rport;
-				}
-				fixTransport(home, ctt->m_url, via_transport);
-			}
-		}
-	}
-}
-
-void NatHelper::fixRecordRouteInRequest(shared_ptr<MsgSip>& ms) {
+void NatHelper::fixRecordRouteInRequest(const shared_ptr<MsgSip>& ms) {
 	sip_t* sip = ms->getSip();
 	if (sip->sip_record_route) {
 		if (mRRPolicy == Safe) {
@@ -237,7 +94,7 @@ void NatHelper::fixRecordRouteInRequest(shared_ptr<MsgSip>& ms) {
 					url_param_add(ms->getHome(), sip->sip_record_route->r_url,
 					              su_sprintf(ms->getHome(), "fs-rport=%s", sip->sip_via->v_rport));
 				}
-				fixTransport(ms->getHome(), sip->sip_record_route->r_url, transport);
+				NatTraversalStrategy::Helper::fixTransport(ms->getHome(), sip->sip_record_route->r_url, transport);
 			}
 		} else {
 			const char* host = sip->sip_record_route->r_url->url_host;
@@ -255,7 +112,7 @@ void NatHelper::fixRecordRouteInRequest(shared_ptr<MsgSip>& ms) {
 					url_param_add(ms->getHome(), sip->sip_record_route->r_url,
 					              su_sprintf(ms->getHome(), "fs-rport=%s", rport));
 				}
-				fixTransport(ms->getHome(), sip->sip_record_route->r_url, transport);
+				NatTraversalStrategy::Helper::fixTransport(ms->getHome(), sip->sip_record_route->r_url, transport);
 			}
 		}
 	}
@@ -263,20 +120,33 @@ void NatHelper::fixRecordRouteInRequest(shared_ptr<MsgSip>& ms) {
 
 ModuleInfo<NatHelper> NatHelper::sInfo(
     "NatHelper",
-    "The NatHelper module executes small tasks to make SIP work smoothly despite firewalls. It corrects the Contact "
-    "headers that contain obviously inconsistent addresses, and adds a Record-Route to ensure subsequent requests are "
-    "routed also by the proxy, through the same UDP or TCP channel used for the initial request.",
+    "The NatHelper module executes small tasks to make SIP work smoothly despite firewalls and NATs. There are two "
+    "strategies available: \"contact-correction\" and \"flow-token\".\n"
+    "Contact-Correction: corrects Contact headers that contain obviously inconsistent addresses.\n"
+    "Flow-Token: add routing information in the Record-Routes as defined in RFC 5626.\n"
+    "Both methods ensure that subsequent requests are correctly routed by the proxy through the same UDP or TCP "
+    "channel used for the initial request.",
     {"Capabilities"},
     ModuleInfoBase::ModuleOid::NatHelper,
 
     [](GenericStruct& moduleConfig) {
 	    ConfigItemDescriptor items[] = {
-	        {String, "contact-verified-param",
-	         "Internal URI parameter added to response contact by first proxy and cleaned by last one.", "verified"},
+	        {String, "nat-traversal-strategy",
+	         "Strategy to manage client-initiated connections when SIP messages are routed through NATs. You can "
+	         "choose between \"contact-correction\" and \"flow-token\".",
+	         "contact-correction"},
+	        {BooleanExpr, "force-flow-token",
+	         "Boolean expression in order to force the use of flow-token under specific conditions. This expression is "
+	         "only evaluated if the \"flow-token\" strategy is used.\n",
+	         "user-agent contains 'Linphone'"},
+	        {String, "contact-correction-param",
+	         "Internal URI parameter added to response contact by first proxy and cleaned by last one. It indicates if "
+	         "the contact was already verified and corrected.",
+	         "verified"},
 	        {Boolean, "fix-record-routes",
 	         "Fix record-routes, to workaround proxies behind firewalls but not aware of it.", "false"},
 	        {String, "fix-record-routes-policy",
-	         "Policy to recognize nat'd record-route and fix them. There are two modes: 'safe' and 'always'", "safe"},
+	         "Policy to recognize NATed record-route and fix them. There are two modes: 'safe' and 'always'", "safe"},
 	        config_item_end};
 	    moduleConfig.addChildrenValues(items);
     });

cmake/flexisip-config.h.in
@@ -73,3 +73,5 @@
 #define BELR_GRAMMARS_DIR "${BELR_GRAMMARS_DIR}"
 
 #define FIREBASE_GET_ACCESS_TOKEN_SCRIPT_PATH "${CMAKE_INSTALL_FULL_DATADIR}/flexisip/firebase_v1_get_access_token.py"
+
+#define FLOW_TOKEN_HASH_KEY_FILE_PATH DEFAULT_LIB_DIR "/flow-token-hash-key"

include/flexisip/event.hh
@@ -32,6 +32,7 @@
 #include <sofia-sip/tport.h>
 
 #include "flexisip/sofia-wrapper/msg-sip.hh"
+#include "utils/socket-address.hh"
 
 namespace flexisip {
 
@@ -124,6 +125,7 @@ public:
 	std::shared_ptr<OutgoingTransaction> getOutgoingTransaction();
 
 	const std::shared_ptr<tport_t>& getIncomingTport() const;
+	std::shared_ptr<SocketAddress> getMsgAddress() const;
 
 protected:
 	enum class State {

include/flexisip/module.hh
@@ -26,6 +26,8 @@
 #include "flexisip/event.hh"
 #include "flexisip/sofia-wrapper/home.hh"
 
+#include "utils/flow.hh"
+
 namespace flexisip {
 
 // =============================================================================
@@ -264,8 +266,12 @@ public:
 	static msg_auth_t* findAuthorizationForRealm(su_home_t* home, msg_auth_t* au, const char* realm);
 	static const tport_t* getIncomingTport(const std::shared_ptr<RequestSipEvent>& ev, Agent* agent);
 
-	static void addRecordRouteIncoming(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev);
-	static void addRecordRoute(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev, const tport_t* tport);
+	static void
+	addRecordRouteIncoming(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev, const Flow::Token& token = "");
+	static void addRecordRoute(Agent* agent,
+	                           const std::shared_ptr<RequestSipEvent>& ev,
+	                           const tport_t* tport,
+	                           const Flow::Token& token = "");
 
 	static void cleanAndPrependRoute(Agent* agent, msg_t* msg, sip_t* sip, sip_route_t* route);
 
@@ -282,8 +288,11 @@ public:
 	static void
 	addRoutingParam(su_home_t* home, sip_contact_t* contacts, const std::string& routingParam, const char* domain);
 	static struct sip_route_s* prependNewRoutable(msg_t* msg, sip_t* sip, sip_route_t*& sipr, sip_route_t* value);
-	static void
-	addPathHeader(Agent* agent, const std::shared_ptr<RequestSipEvent>& ev, tport_t* tport, const char* uniq = nullptr);
+	static void addPathHeader(Agent* agent,
+	                          const std::shared_ptr<RequestSipEvent>& ev,
+	                          tport_t* tport,
+	                          const char* uniq = nullptr,
+	                          const Flow::Token& token = "");
 
 	// These methods do host comparison taking into account that each one of argument can be an ipv6 address enclosed in
 	// brakets.

src/CMakeLists.txt
@@ -99,6 +99,9 @@ add_library(flexisip SHARED
 	module-b2bua.cc
 	module.cc
 	monitor.cc monitor.hh
+	nat/contact-correction-strategy.cc nat/contact-correction-strategy.hh
+	nat/flow-token-strategy.cc nat/flow-token-strategy.hh
+	nat/nat-traversal-strategy.hh nat/nat-traversal-strategy.cc
 	plugin/plugin-loader.cc plugin/plugin-loader.hh
 	recordserializer-c.cc
 	recordserializer-json.cc

src/agent.cc
@@ -39,6 +39,8 @@
 #include "auth/db/authdb.hh"
 #include "domain-registrations.hh"
 #include "etchosts.hh"
+#include "nat/contact-correction-strategy.hh"
+#include "nat/flow-token-strategy.hh"
 #include "plugin/plugin-loader.hh"
 #include "utils/uri-utils.hh"
 
@@ -322,6 +324,18 @@ void Agent::start(const string& transport_override, const string& passphrase) {
 
 	mProxyToProxyKeepAliveInterval =
 	    global->get<ConfigDuration<chrono::seconds>>("proxy-to-proxy-keepalive-interval")->read().count();
+
+	const auto* natHelperConfig = mConfigManager->getRoot()->get<GenericStruct>("module::NatHelper");
+	const auto& strategy = natHelperConfig->get<ConfigString>("nat-traversal-strategy")->read();
+	if (strategy == "contact-correction") {
+		const auto& contactCorrectionParameter = natHelperConfig->get<ConfigString>("contact-correction-param")->read();
+		mNatTraversalStrategy = make_shared<ContactCorrectionStrategy>(this, contactCorrectionParameter);
+	} else if (strategy == "flow-token") {
+		const auto forceFlowTokenExpr = natHelperConfig->get<ConfigBooleanExpression>("force-flow-token")->read();
+		mNatTraversalStrategy = make_shared<FlowTokenStrategy>(this, forceFlowTokenExpr, FLOW_TOKEN_HASH_KEY_FILE_PATH);
+	} else {
+		throw runtime_error("unknown value for \"nat-traversal-strategy\" (" + strategy + ")");
+	}
 
 	mTimer = su_timer_create(mRoot->getTask(), 5000);
 	su_timer_set_for_ever(mTimer, reinterpret_cast<su_timer_f>(timerfunc), this);

src/agent.hh
@@ -57,6 +57,7 @@
 namespace flexisip {
 
 class Module;
+class NatTraversalStrategy;
 class DomainRegistrationManager;
 
 /**
@@ -195,6 +196,10 @@ public:
 	 */
 	const std::string& getUniqueId() const;
 
+	const std::shared_ptr<NatTraversalStrategy>& getNatTraversalStrategy() const {
+		return mNatTraversalStrategy;
+	}
+
 	EventLogWriter* getEventLogWriter() const {
 		return mLogWriter.get();
 	}
@@ -292,6 +297,7 @@ private:
 	const std::shared_ptr<AuthDbBackendOwner> mAuthDbOwner;
 	const std::shared_ptr<RegistrarDb> mRegistrarDb;
 	std::list<std::shared_ptr<Module>> mModules;
+	std::shared_ptr<NatTraversalStrategy> mNatTraversalStrategy;
 	std::list<std::string> mAliases;
 	url_t* mPreferredRouteV4 = nullptr;
 	url_t* mPreferredRouteV6 = nullptr;

src/event.cc
@@ -18,10 +18,10 @@
 
 #include "flexisip/event.hh"
 
+#include <sofia-sip/msg_addr.h>
 #include <sofia-sip/sip_protos.h>
 #include <sofia-sip/su_tagarg.h>
 
-#include "flexisip/common.hh"
 #include "flexisip/module.hh"
 
 #include "agent.hh"
@@ -29,6 +29,7 @@
 #include "eventlogs/writers/event-log-writer.hh"
 #include "transaction/incoming-transaction.hh"
 #include "transaction/outgoing-transaction.hh"
+#include "utils/socket-address.hh"
 
 using namespace std;
 
@@ -141,6 +142,7 @@ std::shared_ptr<IncomingTransaction> SipEvent::getIncomingTransaction() {
 std::shared_ptr<OutgoingTransaction> SipEvent::getOutgoingTransaction() {
 	return dynamic_pointer_cast<OutgoingTransaction>(getOutgoingAgent());
 }
+
 const std::shared_ptr<tport_t>& SipEvent::getIncomingTport() const {
 	return mIncomingTport;
 }
@@ -163,6 +165,17 @@ std::shared_ptr<IncomingAgent> SipEvent::getIncomingAgent() const {
 		return sharedAgent;
 	}
 	return nullptr;
+}
+
+/*
+ * Get a copy of the socket address associated with the message.
+ * Return nullptr if it failed to make the SocketAddress.
+ */
+std::shared_ptr<SocketAddress> SipEvent::getMsgAddress() const {
+	su_sockaddr_t suSocketAddress;
+	socklen_t socklen = sizeof(su_sockaddr_t);
+	msg_get_address(mMsgSip->getMsg(), &suSocketAddress, &socklen);
+	return SocketAddress::make(&suSocketAddress);
 }
 
 void RequestSipEvent::checkContentLength(const url_t* url) {
@@ -188,6 +201,7 @@ std::shared_ptr<RequestSipEvent> RequestSipEvent::makeRestored(std::shared_ptr<I
 
 	return shared;
 }
+
 RequestSipEvent::RequestSipEvent(shared_ptr<IncomingAgent> incomingAgent,
                                  const shared_ptr<MsgSip>& msgSip,
                                  tport_t* tport)

commit be83fe4b694738a7a583bc5f4f16bd9d5787bfb2

fix(nat-helper): "contact-verified-param" was renamed but not deprecated
Bug introduced in b051e9a6af05f499873e1f65effa32ff12e9c350

src/module-nat-helper.cc
@@ -148,24 +148,60 @@ ModuleInfo<NatHelper> NatHelper::sInfo(
 
     [](GenericStruct& moduleConfig) {
 	    ConfigItemDescriptor items[] = {
-	        {String, "nat-traversal-strategy",
-	         "Strategy to manage client-initiated connections when SIP messages are routed through NATs. You can "
-	         "choose between \"contact-correction\" and \"flow-token\".",
-	         "contact-correction"},
-	        {BooleanExpr, "force-flow-token",
-	         "Boolean expression in order to force the use of flow-token under specific conditions. This expression is "
-	         "only evaluated if the \"flow-token\" strategy is used.\n",
-	         "user-agent contains 'Linphone'"},
-	        {String, "flow-token-path", "Path to the file containing the hash key used to hash flow tokens.",
-	         kFlowTokenHashKeyFilePath},
-	        {String, "contact-correction-param",
-	         "Internal URI parameter added to response contact by first proxy and cleaned by last one. It indicates if "
-	         "the contact was already verified and corrected.",
-	         "verified"},
-	        {Boolean, "fix-record-routes",
-	         "Fix record-routes, to workaround proxies behind firewalls but not aware of it.", "false"},
-	        {String, "fix-record-routes-policy",
-	         "Policy to recognize NATed record-route and fix them. There are two modes: 'safe' and 'always'", "safe"},
-	        config_item_end};
+	        {
+	            String,
+	            "nat-traversal-strategy",
+	            "Strategy to manage client-initiated connections when SIP messages are routed through NATs. You can "
+	            "choose between \"contact-correction\" and \"flow-token\".",
+	            "contact-correction",
+	        },
+	        {
+	            BooleanExpr,
+	            "force-flow-token",
+	            "Boolean expression in order to force the use of flow-token under specific conditions. This expression "
+	            "is only evaluated if the \"flow-token\" strategy is used.\n",
+	            "user-agent contains 'Linphone'",
+	        },
+	        {
+	            String,
+	            "flow-token-path",
+	            "Path to the file containing the hash key used to hash flow tokens.",
+	            kFlowTokenHashKeyFilePath,
+	        },
+	        {
+	            String,
+	            "contact-correction-param",
+	            "Internal URI parameter added to response contact by first proxy and cleaned by last one. It indicates "
+	            "if the contact was already verified and corrected.",
+	            "verified",
+	        },
+	        {
+	            Boolean,
+	            "fix-record-routes",
+	            "Fix record-routes, to workaround proxies behind firewalls but not aware of it.",
+	            "false",
+	        },
+	        {
+	            String,
+	            "fix-record-routes-policy",
+	            "Policy to recognize NATed record-route and fix them. There are two modes: 'safe' and 'always'",
+	            "safe",
+	        },
+
+	        // Deprecated parameter.
+	        {
+	            String,
+	            "contact-verified-param",
+	            "Internal URI parameter added to response contact by first proxy and cleaned by last one. It indicates "
+	            "if the contact was already verified and corrected.",
+	            "verified",
+	        },
+	        config_item_end,
+	    };
 	    moduleConfig.addChildrenValues(items);
+
+	    auto* contactVerifiedParam = moduleConfig.get<ConfigString>("contact-verified-param");
+	    contactVerifiedParam->setDeprecated("2025-01-30", "2.4.0",
+	                                        "This parameter is renamed to \"contact-correction-param\".");
+	    moduleConfig.get<ConfigString>("contact-correction-param")->setFallback(*contactVerifiedParam);
     });