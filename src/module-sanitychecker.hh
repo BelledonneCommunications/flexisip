bfc1d47c665440295404a8795fcfb0c4349f6e1a
flore.harle <flore.harle@belledonne-communications.com>
2025-12-17 16:20:49 +0100

    fix: sanity checker rejects request with forbidden characters in uri
    The uri are checked by the SipUri::hasParsingError() method in order
    to find forbidden characters `@` in hots part or `\` in user and host
    part. If such character is found the check fails and a message is
    returned.
    
    The sanity checker module runs that test on From and To fields of the
    header in onRequest() method. The checkHeaders() method has been
    rewritten in order to throw InvalidRequestError exception instead of
    using an event whether a check on SIP headers fails.
    
    Add a header file to SanityChecker module.
    
    Add a test suite for SanityChecker module.

---
+++ src/module-sanitychecker.hh
@@ -0,0 +1,44 @@
+/*
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2026 Belledonne Communications SARL, All rights reserved.
+
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include "flexisip/module.hh"
+
+#include "agent.hh"
+
+using namespace flexisip;
+
+class ModuleSanityChecker : public Module {
+	friend std::shared_ptr<Module> ModuleInfo<ModuleSanityChecker>::create(Agent*);
+
+public:
+	~ModuleSanityChecker() override = default;
+
+	void onRequest(std::shared_ptr<RequestSipEvent>& ev) override;
+
+	void onResponse([[maybe_unused]] std::shared_ptr<ResponseSipEvent>& ev) override {
+		// don't check our responses ;)
+	}
+
+private:
+	ModuleSanityChecker(Agent* ag, const ModuleInfoBase* moduleInfo) : Module(ag, moduleInfo) {
+	}
+
+	void checkHeaders(sip_t* sip);
+
+	static ModuleInfo<ModuleSanityChecker> sInfo;
+};
