BelledonneCommunications/flexisip

commit 524dd5a5dfa0052043f948706e19118d98d0edec

fix(mediarelay): IncomingTransaction leak on INVITE/CANCEL
(cherry picked from commit 1aacf09d68a1e0c1efa0f1ba3143d3353d6ccdde)

src/mediarelay.cc
@@ -16,7 +16,6 @@
     along with this program. If not, see <http://www.gnu.org/licenses/>.
 */
 
-#include <algorithm>
 #include <list>
 #include <poll.h>
 #include <sys/resource.h>
@@ -313,7 +312,7 @@ void RelaySession::removeBranch(const std::string& trId) {
 	}
 }
 
-int RelaySession::getActiveBranchesCount() {
+int RelaySession::getActiveBranchesCount() const {
 	int count = 0;
 	mMutex.lock();
 	for (auto it = mBacks.begin(); it != mBacks.end(); ++it) {

src/mediarelay.hh
@@ -47,7 +47,6 @@ public:
 private:
 	MediaRelay(Agent* ag, const ModuleInfoBase* moduleInfo);
 
-	bool isInviteOrUpdate(sip_method_t method) const;
 	void createServers();
 	bool processNewInvite(const std::shared_ptr<RelayedCall>& c,
 	                      const std::shared_ptr<OutgoingTransaction>& transaction,
@@ -154,7 +153,7 @@ public:
 	void fillPollFd(PollFd* pfd);
 	void checkPollFd(const PollFd* pfd, time_t curtime);
 	void unuse();
-	int getActiveBranchesCount();
+	int getActiveBranchesCount() const;
 
 	bool isUsed() const {
 		return mUsed;

commit 89bffe66e491bd78a75b7de47e353b956229a638

fix: Early call deletion in MediaRelay
Commit 524dd5a5 introduced a regression where RelayedCall was ended early on 300 and higher responses.

src/callcontext-mediarelay.cc
@@ -221,7 +221,7 @@ void RelayedCall::setChannelDestinations(const shared_ptr<SdpModifier>& sdpModif
 	if (s == nullptr) return;
 
 	// Default direction is SendRecv.
-	auto dir = RelayChannel::SendRecv;
+	auto dir = RelayChannel::Dir::SendRecv;
 	const bool isEarlyMediaState = isEarlyMedia && !mIsEstablished && mServer->mModule->mEarlyMediaRelaySingle;
 
 	// Make sure that only one device can send media to the caller (caller POV: receive media from only one callee),
@@ -235,7 +235,7 @@ void RelayedCall::setChannelDestinations(const shared_ptr<SdpModifier>& sdpModif
 			if (session == nullptr) break; // sentinel reached
 
 			if (const auto& channel = session->getChannel("", mSendRecvTrId))
-				channel->setDirection(RelayChannel::SendOnly);
+				channel->setDirection(RelayChannel::Dir::SendOnly);
 		}
 
 		mSendRecvTrId = trId;
@@ -260,7 +260,7 @@ void RelayedCall::setChannelDestinations(const shared_ptr<SdpModifier>& sdpModif
 					 */
 				} else if (s->getActiveBranchesCount() >= maxEarlyRelays) {
 					LOGW << "Maximum number of relayed early media streams reached";
-					dir = RelayChannel::Inactive;
+					dir = RelayChannel::Dir::Inactive;
 				}
 			}
 		}

src/mediarelay.cc
@@ -20,8 +20,6 @@
 #include <poll.h>
 #include <sys/resource.h>
 
-#include "flexisip-config.h"
-
 #include "agent.hh"
 #include "mediarelay.hh"
 
@@ -59,8 +57,9 @@ unsigned int PollFd::getREvents(int index) const {
 	return mPfd[index].revents;
 }
 
-RelayChannel::RelayChannel(RelaySession* relaySession, const RelayTransport& rt, bool preventLoops)
-    : mRelayTransport(rt), mRemoteIp(std::string("undefined")), mDir(SendRecv), mPacketsReceived{}, mPacketsSent{} {
+RelayChannel::RelayChannel(RelaySession* relaySession, const RelayTransport& rt, bool preventLoops, Type type)
+    : mRelayTransport(rt), mRemoteIp(std::string("undefined")), mDir(Dir::SendRecv), mType(type), mPacketsReceived{},
+      mPacketsSent{} {
 	mPfdIndex = -1;
 	initializeRtpSession(relaySession);
 	mSockAddrSize[0] = mSockAddrSize[1] = 0;
@@ -70,7 +69,7 @@ RelayChannel::RelayChannel(RelaySession* relaySession, const RelayTransport& rt,
 	mRecvErrorCount[0] = mRecvErrorCount[1] = 0;
 	mRemotePort[0] = mRemotePort[1] = -1;
 	mIsOpen = false;
-	mLogPrefix = LogManager::makeLogPrefixForInstance(this, "RelayChannel");
+	mLogPrefix = LogManager::makeLogPrefixForInstance(this, typeToString(mType) + ".RelayChannel");
 }
 
 void RelayChannel::initializeRtpSession(RelaySession* relaySession) {
@@ -98,12 +97,22 @@ RelayChannel::~RelayChannel() {
 
 const char* RelayChannel::dirToString(Dir dir) {
 	switch (dir) {
-		case SendOnly:
+		case Dir::SendOnly:
 			return "SendOnly";
-		case SendRecv:
+		case Dir::SendRecv:
 			return "SendRecv";
-		case Inactive:
+		case Dir::Inactive:
 			return "Inactive";
+	}
+	return "invalid";
+}
+
+string RelayChannel::typeToString(Type type) {
+	switch (type) {
+		case Type::Back:
+			return "Back";
+		case Type::Front:
+			return "Front";
 	}
 	return "invalid";
 }
@@ -221,7 +230,7 @@ int RelayChannel::recv(int i, uint8_t* buf, size_t buflen, time_t curTime) {
 			mSockAddrLastUseTime[i] = curTime;
 		}
 
-		if (!mIsOpen || mDir == SendOnly || mDir == Inactive) {
+		if (!mIsOpen || mDir == Dir::SendOnly || mDir == Dir::Inactive) {
 			return 0;
 		}
 		if (mFilter &&
@@ -241,7 +250,7 @@ int RelayChannel::recv(int i, uint8_t* buf, size_t buflen, time_t curTime) {
 int RelayChannel::send(int i, uint8_t* buf, size_t buflen) {
 	int err = 0;
 	/*if destination address is working mSockAddrSize>0*/
-	if (mRemotePort[i] > 0 && mSockAddrSize[i] > 0 && mDir != Inactive && mRecvErrorCount[i] < sMaxRecvErrors &&
+	if (mRemotePort[i] > 0 && mSockAddrSize[i] > 0 && mDir != Dir::Inactive && mRecvErrorCount[i] < sMaxRecvErrors &&
 	    mIsOpen) {
 		if (!mFilter || mFilter->onOutgoingTransfer(buf, buflen, (struct sockaddr*)&mSockAddr[i], mSockAddrSize[i])) {
 			int localPort = (i == 0) ? mRelayTransport.mRtpPort : mRelayTransport.mRtcpPort;
@@ -267,7 +276,7 @@ RelaySession::RelaySession(MediaRelayServer* server, const string& frontId, cons
     : mServer(server), mFrontChannelId(frontId) {
 	mLastActivityTime = getCurrentTime();
 	mUsed = true;
-	mFrontChannel = make_shared<RelayChannel>(this, rt, mServer->loopPreventionEnabled());
+	mFrontChannel = make_shared<RelayChannel>(this, rt, mServer->loopPreventionEnabled(), RelayChannel::Type::Front);
 	mLogPrefix = LogManager::makeLogPrefixForInstance(this, "RelaySession");
 }
 
@@ -290,11 +299,11 @@ std::shared_ptr<RelayChannel>
 RelaySession::createBranch(const std::string& trId, const RelayTransport& rt, bool hasMultipleTargets) {
 	shared_ptr<RelayChannel> ret;
 	mMutex.lock();
-	ret = make_shared<RelayChannel>(this, rt, mServer->loopPreventionEnabled());
+	ret = make_shared<RelayChannel>(this, rt, mServer->loopPreventionEnabled(), RelayChannel::Type::Back);
 	ret->setMultipleTargets(hasMultipleTargets);
 	mPotentialBackChannels.insert(make_pair(trId, ret));
 	mMutex.unlock();
-	LOGD << "Branch corresponding to transaction [" << trId << "] added";
+	LOGD << ret->getLogPrefix() << " corresponding to transaction [" << trId << "] added";
 	return ret;
 }
 
@@ -314,13 +323,21 @@ void RelaySession::removeBranch(const std::string& trId) {
 
 int RelaySession::getActiveBranchesCount() const {
 	int count = 0;
-	mMutex.lock();
+	std::scoped_lock lock{mMutex};
 	for (auto it = mPotentialBackChannels.begin(); it != mPotentialBackChannels.end(); ++it) {
 		if ((*it).second->getRemoteRtpPort() > 0) count++;
 	}
 	if (mSelectedBackChannel && mSelectedBackChannel->getRemoteRtpPort() > 0) count++;
-	mMutex.unlock();
 	LOGD << "getActiveBranchesCount(): " << count;
+	return count;
+}
+
+int RelaySession::getBranchesCount() const {
+	int count = 0;
+	std::scoped_lock lock{mMutex};
+	count = mPotentialBackChannels.size();
+	if (mSelectedBackChannel) count++;
+	LOGD << "getBranchesCount(): " << count;
 	return count;
 }
 

src/mediarelay.hh
@@ -154,6 +154,7 @@ public:
 	void checkPollFd(const PollFd* pfd, time_t curtime);
 	void unuse();
 	int getActiveBranchesCount() const;
+	int getBranchesCount() const;
 
 	bool isUsed() const {
 		return mUsed;
@@ -207,9 +208,10 @@ public:
 
 class RelayChannel : public SdpMasqueradeContext {
 public:
-	enum Dir { SendOnly, SendRecv, Inactive };
+	enum class Dir { SendOnly, SendRecv, Inactive };
+	enum class Type { Front, Back };
 
-	RelayChannel(RelaySession* relaySession, const RelayTransport& rt, bool preventLoops);
+	RelayChannel(RelaySession* relaySession, const RelayTransport& rt, bool preventLoops, Type type);
 	~RelayChannel();
 	bool checkSocketsValid();
 	void setRemoteAddr(const std::string& ip, int port, int rtcp_port, Dir dir);
@@ -247,8 +249,12 @@ public:
 	}
 	void setDirection(Dir);
 	Dir getDirection() const;
+	const std::string& getLogPrefix() const {
+		return mLogPrefix;
+	}
 
 	static const char* dirToString(Dir dir);
+	static std::string typeToString(Type type);
 
 private:
 	static const int sMaxRecvErrors = 50;
@@ -266,6 +272,7 @@ private:
 	int mPfdIndex;
 	int mRecvErrorCount[2];
 	Dir mDir;
+	Type mType;
 	uint64_t mPacketsReceived[2];
 	uint64_t mPacketsSent[2];
 	bool mPreventLoop;

src/module-mediarelay.cc
@@ -494,10 +494,10 @@ unique_ptr<ResponseSipEvent> MediaRelay::onResponse(unique_ptr<ResponseSipEvent>
 				processResponseWithSDP(c, ot, ev->getMsgSip());
 			} else if (sip->sip_status->st_status >= 300) {
 				c->removeBranch(ot->getBranchId());
-				const auto hasActiveBranches =
+				const auto hasBranches =
 				    std::any_of(c->getSessions().begin(), c->getSessions().end(),
-				                [](const auto& session) { return session && (0 < session->getActiveBranchesCount()); });
-				if (!hasActiveBranches) {
+				                [](const auto& session) { return session && 0 < session->getBranchesCount(); });
+				if (!hasBranches) {
 					LOGD << "RelayedCall[" << c << "] terminated: Last branch removed.";
 					mCalls->remove(c);
 				}

tester/config/flexisip_fork_call_context.conf
@@ -1,24 +1,0 @@
-[global]
-transports=sip:127.0.0.1:0;transport=tcp
-aliases=localhost sip.test.org
-
-[module::Forward]
-enabled=true
-
-[module::DoSProtection]
-enabled=false
-
-[module::Router]
-fork-late=true
-
-[module::PushNotification]
-enabled=true
-
-# Test using unauthenticated accounts on sip.test.org.
-[module::Registrar]
-enabled=true
-reg-domains=sip.test.org
-
-[module::MediaRelay]
-# Media Relay has problems when everyone is running on localhost.
-enabled=false

tester/tests/callcontext-mediarelay-tester.cc
@@ -94,7 +94,7 @@ static void makeDeviceTakeOverSendRecvCapabilities(const shared_ptr<RelayedCall>
 
 		const auto channel = call->getSessions()[sessionId]->getChannel("stub-party-id", trId);
 		BC_HARD_ASSERT(channel != nullptr);
-		BC_HARD_ASSERT_CPP_EQUAL(channel->getDirection(), RelayChannel::Dir::SendRecv);
+		BC_ASSERT_ENUM_EQUAL(channel->getDirection(), RelayChannel::Dir::SendRecv);
 	}
 }
 }; // namespace helper
@@ -120,7 +120,7 @@ void setChannelDestinationsTakesOverPreviousSendRecvTrId() {
 	// Verify that all media sessions of the first device now only have SendOnly capabilities.
 	for (int session = 0; session < nbMediaSessionsFirstDevice; session++) {
 		const auto& previousChannel = call->getSessions()[session]->getChannel("stub-party-id", fTrId);
-		BC_ASSERT_CPP_EQUAL(previousChannel->getDirection(), RelayChannel::Dir::SendOnly);
+		BC_ASSERT_ENUM_EQUAL(previousChannel->getDirection(), RelayChannel::Dir::SendOnly);
 	}
 
 	// Smoke test: passed if this doesn't crash

tester/tests/integration/call-tester.cc
@@ -39,19 +39,24 @@ using namespace std;
 namespace flexisip::tester {
 namespace {
 
+std::map<std::string, std::string> sServerConfig = {
+    {"global/transports", "sip:127.0.0.1:0 sip:[::1]:0"},
+    {"module::MediaRelay/enabled", "false"},
+    {"module::MediaRelay/prevent-loops", "false"}, // Allow loopback to localnetwork
+    {"module::Registrar/enabled", "true"},
+    {"module::Registrar/reg-domains", "sip.example.org"},
+};
+
 class CallTestContext {
 public:
-	using ConfigMap = std::map<std::string, std::string>;
 	CallTestContext() = default;
-	CallTestContext(ConfigMap&& serverConfig,
-	                const uint numberOfCallerDevices,
+	CallTestContext(const uint numberOfCallerDevices,
 	                const uint numberOfCalleeDevices,
 	                const std::string& callerUri,
 	                const std::string& calleeUri,
 	                const OnOff videoCall)
-	    : serverConfig(std::move(serverConfig)), numberOfCallerDevices(numberOfCallerDevices),
-	      numberOfCalleeDevices(numberOfCalleeDevices), callerUri(callerUri), calleeUri(calleeUri),
-	      videoCall(videoCall) {
+	    : numberOfCallerDevices(numberOfCallerDevices), numberOfCalleeDevices(numberOfCalleeDevices),
+	      callerUri(callerUri), calleeUri(calleeUri), videoCall(videoCall) {
 	}
 
 	CallTestContext enableVideoCall() {
@@ -69,21 +74,19 @@ public:
 		return *this;
 	}
 
+	CallTestContext setNumberOfCalleeIdleDevices(const uint pNumberOfCalleeIdleDevices) {
+		this->numberOfCalleeIdleDevices = pNumberOfCalleeIdleDevices;
+		return *this;
+	}
+
 	CallTestContext setInCallMediaState(const CallAssertionInfo::MediaStateList& pInCallMediaState) {
 		this->inCallMediaState = pInCallMediaState;
 		return *this;
 	}
 
-	ConfigMap serverConfig = {
-	    {"global/transports", "sip:127.0.0.1:0 sip:[::1]:0"},
-	    {"module::MediaRelay/enabled", "true"},
-	    {"module::MediaRelay/prevent-loops", "false"}, // Allow loopback to localnetwork
-	    {"module::Registrar/enabled", "true"},
-	    {"module::Registrar/reg-domains", "sip.example.org"},
-	};
-
 	size_t numberOfCallerDevices = 1;
 	size_t numberOfCalleeDevices = 1;
+	size_t numberOfCalleeIdleDevices = 1;
 	std::string callerUri = "sip:caller@sip.example.org";
 	std::string calleeUri = "sip:callee@sip.example.org";
 	OnOff videoCall = OnOff::Off;
@@ -92,7 +95,7 @@ public:
 
 void callTestTemplate(const CallTestContext& ctx) {
 	// Arrange server
-	Server server(ctx.serverConfig);
+	Server server(sServerConfig);
 	server.start();
 
 	// Arrange clients
@@ -104,14 +107,19 @@ void callTestTemplate(const CallTestContext& ctx) {
 	}
 	const auto callee = "sip:callee@sip.example.org";
 	vector<CoreClient> calleeDevices;
-	for (size_t i = 0; i < ctx.numberOfCallerDevices; i++) {
+	for (size_t i = 0; i < ctx.numberOfCalleeDevices; i++) {
 		calleeDevices.push_back(builder.build(callee + ";device=n"s + to_string(i)));
+	}
+	vector<CoreClient> calleeIdleDevices;
+	for (size_t i = 0; i < ctx.numberOfCalleeIdleDevices; i++) {
+		calleeIdleDevices.push_back(builder.build(callee + ";device=idle-n"s + to_string(i)));
 	}
 
 	// Arrange asserter
 	CoreAssert asserter(server);
 	asserter.registerSteppables(callerDevices);
 	asserter.registerSteppables(calleeDevices);
+	asserter.registerSteppables(calleeIdleDevices);
 
 	// Assert all clients registered without issue
 	for (const auto& calleeDevice : calleeDevices) {
@@ -119,6 +127,10 @@ void callTestTemplate(const CallTestContext& ctx) {
 	}
 	for (const auto& callerDevice : callerDevices) {
 		BC_HARD_ASSERT_TRUE(callerDevice.isRegistered(asserter));
+	}
+	for (const auto& calleeIdleDevice : calleeIdleDevices) {
+		BC_HARD_ASSERT_TRUE(calleeIdleDevice.isRegistered(asserter));
+		calleeIdleDevice.disconnect();
 	}
 
 	// Actually start the call
@@ -168,13 +180,36 @@ void videoCallWithMultipleDevices() {
 	callTestTemplate(CallTestContext{}.enableVideoCall().setNumberOfCallerDevices(10).setNumberOfCalleeDevices(10));
 }
 
+void videoCallWithMultipleDevicesAndIdleDevices() {
+	callTestTemplate(CallTestContext{}
+	                     .enableVideoCall()
+	                     .setNumberOfCallerDevices(10)
+	                     .setNumberOfCalleeDevices(5)
+	                     .setNumberOfCalleeIdleDevices(5));
+}
+
+const std::vector<test_t> sTestList = {
+    CLASSY_TEST(basicCall),
+    CLASSY_TEST(videoCall),
+    CLASSY_TEST(videoCallWithMultipleDevices),
+    CLASSY_TEST(videoCallWithMultipleDevicesAndIdleDevices),
+};
+
 TestSuite _{
     "Call",
-    {
-        CLASSY_TEST(basicCall),
-        CLASSY_TEST(videoCall),
-        CLASSY_TEST(videoCallWithMultipleDevices),
-    },
+    sTestList,
+    Hooks().beforeSuite([] {
+	    sServerConfig.insert_or_assign("module::MediaRelay/enabled", "false");
+	    return 0;
+    }),
+};
+TestSuite __{
+    "CallWithMediaRelay",
+    sTestList,
+    Hooks().beforeSuite([] {
+	    sServerConfig.insert_or_assign("module::MediaRelay/enabled", "true");
+	    return 0;
+    }),
 };
 } // namespace
 } // namespace flexisip::tester

tester/tests/router/fork/fork-call-tester.cc
@@ -36,8 +36,22 @@ using namespace linphone;
 namespace flexisip::tester {
 namespace {
 
+std::map<std::string, std::string> sDefaultConfig = {{
+    {"global/transports", "sip:127.0.0.1:0;transport=tcp"},
+    {"global/aliases", "localhost sip.test.org"},
+    {"module::Forward/enabled", "true"},
+    {"module::DoSProtection/enabled", "false"},
+    {"module::Router/enabled", "true"},
+    {"module::Router/fork-late", "true"},
+    {"module::PushNotification/enabled", "true"},
+    {"module::Registrar/enabled", "true"},
+    {"module::Registrar/reg-domains", "sip.test.org"},
+    {"module::MediaRelay/enabled", "false"},
+    {"module::MediaRelay/prevent-loops", "false"},
+}};
+
 void basicCall() {
-	Server server{"/config/flexisip_fork_call_context.conf"};
+	Server server{sDefaultConfig};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -56,7 +70,7 @@ void basicCall() {
 }
 
 void callWithEarlyCancel() {
-	Server server{"/config/flexisip_fork_call_context.conf"};
+	Server server{sDefaultConfig};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -109,17 +123,7 @@ void callWithEarlyCancelCalleeOffline() {
 		        return std::move(request);
 	        },
 	};
-	Server server{{
-	                  {"global/transports", "sip:127.0.0.1:0;transport=tcp"},
-	                  {"module::Router/fork-late", "true"},
-	                  {"module::PushNotification/enabled", "true"},
-	                  {"module::Registrar/reg-domains", "sip.test.org"},
-
-	                  // Caused a leak on injection of the cancelled INVITE
-	                  // (A new incoming transaction was wrongly created)
-	                  {"module::MediaRelay/enabled", "true"},
-	              },
-	              &hooks};
+	Server server{sDefaultConfig, &hooks};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -213,7 +217,7 @@ void callWithEarlyCancelCalleeOffline() {
  */
 template <bool clientBecomesAvailable>
 void callWithEarlyCancelCalleeOnlyOffline() {
-	Server server{"/config/flexisip_fork_call_context.conf"};
+	Server server{sDefaultConfig};
 	server.setConfigParameter({"module::Router/call-fork-timeout", "1s"});
 	server.start();
 
@@ -343,17 +347,7 @@ void callWithEarlyCancelCalleeOnlyOffline() {
  * Assert that only the device with voip push receive an INVITE+CANCEL on register.
  */
 void callWithEarlyCancelCalleeOfflineNoVOIPPush() {
-	Server server{{
-	    {"global/transports", "sip:127.0.0.1:0;transport=tcp"},
-	    {"global/aliases", "localhost sip.test.org"},
-	    {"module::Forward/enabled", "true"},
-	    {"module::DoSProtection/enabled", "false"},
-	    {"module::Router/fork-late", "true"},
-	    {"module::PushNotification/enabled", "true"},
-	    {"module::Registrar/enabled", "true"},
-	    {"module::Registrar/reg-domains", "sip.test.org"},
-	    {"module::MediaRelay/enabled", "true"},
-	}};
+	Server server{sDefaultConfig};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -415,7 +409,7 @@ void callWithEarlyCancelCalleeOfflineNoVOIPPush() {
 }
 
 void calleeOfflineWithOneDevice() {
-	Server server{"/config/flexisip_fork_call_context.conf"};
+	Server server{sDefaultConfig};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -468,7 +462,7 @@ void calleeOfflineWithOneDevice() {
 }
 
 void calleeOfflineWithOneDeviceEarlyDecline() {
-	Server server{"/config/flexisip_fork_call_context.conf"};
+	Server server{sDefaultConfig};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -521,7 +515,7 @@ void calleeOfflineWithOneDeviceEarlyDecline() {
 }
 
 void calleeMultipleOnlineDevices() {
-	Server server{"/config/flexisip_fork_call_context.conf"};
+	Server server{sDefaultConfig};
 	server.start();
 
 	ClientBuilder builder{*server.getAgent()};
@@ -553,12 +547,7 @@ struct BrCancelListener : public BranchInfoListener {
  * Verify that the cancellation status is linked to the cancellation reason.
  */
 void cancelStatusOnCancel() {
-	Server proxy{{
-	    {"global/transports", "sip:127.0.0.1:0"},
-	    {"module::Registrar/enabled", "true"},
-	    {"module::Registrar/reg-domains", "localhost"},
-	    {"module::Router/enabled", "true"},
-	}};
+	Server proxy{sDefaultConfig};
 	proxy.start();
 	const auto moduleRouter = dynamic_pointer_cast<ModuleRouter>(proxy.getAgent()->findModuleByRole("Router"));
 	const auto forkFactory = moduleRouter->getForkManager()->getFactory();
@@ -610,12 +599,7 @@ void cancelStatusOnCancel() {
  * Check that an accepted call on a branch leads to a cancel with AcceptedElseWhere status on another branch.
  */
 void cancelStatusOnResponse() {
-	Server proxy{{
-	    {"global/transports", "sip:127.0.0.1:0"},
-	    {"module::Registrar/enabled", "true"},
-	    {"module::Registrar/reg-domains", "localhost"},
-	    {"module::Router/enabled", "true"},
-	}};
+	Server proxy{sDefaultConfig};
 	proxy.start();
 	const auto moduleRouter = dynamic_pointer_cast<ModuleRouter>(proxy.getAgent()->findModuleByRole("Router"));
 	const auto forkFactory = moduleRouter->getForkManager()->getFactory();
@@ -662,22 +646,37 @@ void cancelStatusOnResponse() {
 	BC_ASSERT(branchListener->mCancelStatus.value() == ForkStatus::AcceptedElsewhere);
 }
 
-TestSuite _{
-    "ForkCallContext",
-    {
-        CLASSY_TEST(basicCall),
-        CLASSY_TEST(callWithEarlyCancel),
-        CLASSY_TEST(calleeOfflineWithOneDeviceEarlyDecline),
-        CLASSY_TEST(callWithEarlyCancelCalleeOffline),
-        CLASSY_TEST(callWithEarlyCancelCalleeOnlyOffline<true>),
-        CLASSY_TEST(callWithEarlyCancelCalleeOnlyOffline<false>),
-        CLASSY_TEST(calleeOfflineWithOneDevice),
-        CLASSY_TEST(calleeMultipleOnlineDevices),
-        CLASSY_TEST(callWithEarlyCancelCalleeOfflineNoVOIPPush),
-        CLASSY_TEST(cancelStatusOnCancel),
-        CLASSY_TEST(cancelStatusOnResponse),
-    },
+const std::vector<test_t> sTestList = {
+    CLASSY_TEST(basicCall),
+    CLASSY_TEST(callWithEarlyCancel),
+    CLASSY_TEST(calleeOfflineWithOneDeviceEarlyDecline),
+    CLASSY_TEST(callWithEarlyCancelCalleeOffline),
+    CLASSY_TEST(callWithEarlyCancelCalleeOnlyOffline<true>),
+    CLASSY_TEST(callWithEarlyCancelCalleeOnlyOffline<false>),
+    CLASSY_TEST(calleeOfflineWithOneDevice),
+    CLASSY_TEST(calleeMultipleOnlineDevices),
+    CLASSY_TEST(callWithEarlyCancelCalleeOfflineNoVOIPPush),
+    CLASSY_TEST(cancelStatusOnCancel),
+    CLASSY_TEST(cancelStatusOnResponse),
+
 };
 
+TestSuite _{
+    "ForkCallContext",
+    sTestList,
+    Hooks().beforeSuite([] {
+	    sDefaultConfig.insert_or_assign("module::MediaRelay/enabled", "false");
+	    return 0;
+    }),
+};
+
+TestSuite __{
+    "ForkCallContextWithMediaRelay",
+    sTestList,
+    Hooks().beforeSuite([] {
+	    sDefaultConfig.insert_or_assign("module::MediaRelay/enabled", "true");
+	    return 0;
+    }),
+};
 } // namespace
 } // namespace flexisip::tester

tester/utils/test-suite.hh
@@ -81,6 +81,10 @@ public:
 		bc_tester_add_suite(&mSuite);
 	}
 
+	TestSuite(const char* name, const std::vector<test_t>& tests, const Hooks& hooks = {})
+	    : TestSuite(name, std::vector(tests), hooks) {
+	}
+
 	const char* getName() const {
 		return mSuite.name;
 	}