f5ed1af8b90f970690ddd6a9a1ba3424117b4e41
Félix Olart <felix.olart@belledonne-communications.com>
2026-01-28 17:21:35 +0100

    fix(sofia-sip): infinite loop on timer C expiration
    See the commit in sofia-sip for more information.

--- tester/tests/sofia-api/nta-reply.cc
+++ tester/tests/sofia-api/nta-reply.cc
@@ -236,11 +236,71 @@ void incomingReply() {
 	nta_incoming_treply(irq, SIP_200_OK, TAG_END());
 }
 
+/**
+ * Test that on timer C expiration (RFC3261 16.6 11), a CANCEL request is sent (RFC3261 16.8) and the outgoing
+ * transaction is properly transitioned to the "terminated" state.
+ */
+void cancelOnTimerCWithProvisionalResponse() {
+	const auto suRoot = make_shared<SuRoot>();
+	static bool receivedCancel{false};
+
+	NtaAgent caller{suRoot, "sip:caller@127.0.0.2:0;transport=tcp"};
+	const string callerUri{"sip:caller@sip.example.org"};
+
+	const auto calleeCb = [](nta_agent_magic_t*, nta_agent_t* agent, msg_t* msg, sip_t* sip) {
+		if (sip->sip_request->rq_method == sip_method_cancel) {
+			receivedCancel = true;
+			nta_msg_treply(agent, msg, SIP_200_OK, TAG_END());
+			return 0;
+		}
+		// It is important the callee provides a provisional response (so the client transaction on the proxy goes in
+		// the "proceeding" state).
+		if (sip->sip_request->rq_method == sip_method_invite) nta_msg_treply(agent, msg, SIP_100_TRYING, TAG_END());
+		return 0;
+	};
+	NtaAgent callee{suRoot, "sip:callee@127.0.0.3:0;transport=tcp", calleeCb};
+	static const auto calleeUri = getNtaAgentFirstTransport(callee);
+
+	const auto serverCb = [](nta_agent_magic_t*, nta_agent_t* agent, msg_t* msg, sip_t*) -> int {
+		auto* incoming = nta_incoming_create(agent, nullptr, msg_ref(msg), sip_object(msg), TAG_END());
+		nta_incoming_treply(incoming, SIP_100_TRYING, TAG_END());
+		nta_outgoing_mcreate(agent, {}, toSofiaSipUrlUnion(calleeUri), msg, TAG_END());
+		return 0;
+	};
+	NtaAgent server{suRoot, "sip:127.0.0.1:0;transport=tcp", serverCb, nullptr, NTATAG_TIMER_C(500)};
+
+	stringstream rawRequest{};
+	rawRequest << "INVITE " << callerUri << " SIP/2.0\r\n"
+	           << "Via: SIP/2.0/TCP 127.0.0.1:53314;branch=z9hG4bK.B7fbFxUnN;rport=53314;received=127.0.0.1\r\n"
+	           << "From: <" << callerUri << ">;tag=465687829\r\n"
+	           << "To: <" << callerUri << ">\r\n"
+	           << "Call-ID: stub-call-id\r\n"
+	           << "CSeq: 20 INVITE\r\n"
+	           << "Contact: <" << callerUri << ">\r\n"
+	           << "Expires: 600\r\n"
+	           << "Content-Length: 0\r\n\r\n";
+
+	MsgSip message{0, rawRequest.str()};
+	const auto transaction = caller.createOutgoingTransaction(message.msgAsString(), getNtaAgentFirstTransport(server));
+
+	usize_t terminatedQueueLength{};
+	CoreAssert{suRoot}
+	    .wait([&] {
+		    FAIL_IF(receivedCancel == false);
+		    nta_agent_get_stats(server.getAgent(), NTATAG_S_ORQ_Q_TERM_LEN_REF(terminatedQueueLength), TAG_END());
+		    FAIL_IF(terminatedQueueLength != 1);
+		    FAIL_IF(transaction->isCompleted() == true);
+		    return ASSERTION_PASSED();
+	    })
+	    .assert_passed();
+}
+
 TestSuite _{
     "sofia::nta::reply",
     {
         CLASSY_TEST(outgoingTransportSelection),
         CLASSY_TEST(incomingReply),
+        CLASSY_TEST(cancelOnTimerCWithProvisionalResponse),
     },
 };
 
