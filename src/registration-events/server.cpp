BelledonneCommunications/flexisip

commit 57c7e76a9080b894f1f15f6a93a9d0bd42d5fedc

fix(conference): update linphone-SDK
This is to prevent terminating a resource list subscription if a device leaves a chatroom.

src/registration-events/server.cpp
@@ -175,9 +175,13 @@ void Server::Application::onSubscribeReceived(const shared_ptr<Core>&,
 
 	// If subscriber already exists, replace the old subscription with the new one.
 	if (subscriptionIt != subscriptions.end()) {
-		LOGD << "Replacing Subscription[event=" << (*subscriptionIt)->getEvent() << "] from '" << fromUri
-		     << "' to record key '" << recordKey.asString() << "'";
-		subscriptions.erase(subscriptionIt);
+		if (auto event = (*subscriptionIt)->getEvent()) {
+			LOGD << "Replacing Subscription[event=" << event << "] from '" << fromUri
+			     << "' to record key '" << recordKey.asString() << "'";
+			// Event termination triggers erase from the mSubscriptions map
+			event->terminate();
+		}
+
 	}
 	subscriptions.emplace_back(make_shared<Subscription>(event));
 	LOGD << "Added Subscription[event=" << event << "] from '" << fromUri << "' to record key '" << recordKey.asString()
@@ -220,7 +224,7 @@ void Server::Application::onSubscriptionStateChanged(const shared_ptr<linphone::
 			const auto fromUri = event->getFromAddress()->asStringUriOnly();
 			const auto subscriptionIt =
 			    find_if(subscriptions.begin(), subscriptions.end(), [&fromUri](const auto& subscription) {
-				    return subscription->getEvent()->getFromAddress()->asStringUriOnly() == fromUri;
+				    return subscription && (subscription->getEvent()->getFromAddress()->asStringUriOnly() == fromUri);
 			    });
 
 			if (subscriptionIt != subscriptions.end()) {

tester/registration-event-tester.cc
@@ -179,9 +179,8 @@ void basicSubscription() {
 	// Remove a device.
 	inserter.setExpire(0s).insert({.uniqueId = "device-d"});
 	registrarDb->publish(otherParticipantTopic, "");
-	asserter
-	    .iterateUpTo(
-	        10, [&totalDevicesCount] { return LOOP_ASSERTION(totalDevicesCount() == 3); }, 1s)
+	asserter.iterateUpTo(
+	            50, [&totalDevicesCount] { return LOOP_ASSERTION(totalDevicesCount() == 3); }, 1s)
 	    .hard_assert_passed();
 
 	{
@@ -194,7 +193,7 @@ void basicSubscription() {
 	// Remove the last device of a participant.
 	registrarDb->clear(SipUri(participantFrom), "stub-callid", make_shared<StubListener>());
 	registrarDb->publish(participantTopic, "");
-	asserter.iterateUpTo(3, [&totalDevicesCount] { return LOOP_ASSERTION(totalDevicesCount() == 2); })
+	asserter.iterateUpTo(50, [&totalDevicesCount] { return LOOP_ASSERTION(totalDevicesCount() == 2); })
 	    .hard_assert_passed();
 
 	{
@@ -226,7 +225,7 @@ void basicSubscription() {
 	const string participantRebindFrom{"sip:participant_re_bind@localhost"};
 	inserter.setExpire(10s).setAor(participantRebindFrom).insert({.uniqueId = "re-device-a"});
 	chatRoom->addParticipant(linphone::Factory::get()->createAddress(participantRebindFrom));
-	asserter.iterateUpTo(8, [&totalDevicesCount] { return LOOP_ASSERTION(totalDevicesCount() == 3); })
+	asserter.iterateUpTo(50, [&totalDevicesCount] { return LOOP_ASSERTION(totalDevicesCount() == 3); })
 	    .hard_assert_passed();
 
 	// Check if the participant was still added (locally).
@@ -430,7 +429,9 @@ void multipleSubscribersToOneRecordKey() {
 	    .hard_assert_passed();
 
 	// Replace subscription from 'subscriber' to topic.
-	const auto newSubscriptionFromSubscriber = subscriber.subscribe(aorOfInterest, regEventUri);
+	Subscriber subscriberBis{SipUri{"sip:subscriber@sip.example.org"}, onSubscriberResponse, rsg};
+	asserter.registerSteppable(subscriberBis.mSuRoot);
+	const auto newSubscriptionFromSubscriber = subscriberBis.subscribe(aorOfInterest, regEventUri);
 
 	asserter
 	    .iterateUpTo(
@@ -439,6 +440,7 @@ void multipleSubscribersToOneRecordKey() {
 		        FAIL_IF(!newSubscriptionFromSubscriber->isCompleted());
 		        FAIL_IF(newSubscriptionFromSubscriber->getStatus() != 200);
 		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 1);
 		        FAIL_IF(otherSubscriber.mTotalNotifyReceived != 2);
 		        return ASSERTION_PASSED();
 	        },
@@ -446,7 +448,7 @@ void multipleSubscribersToOneRecordKey() {
 	    .hard_assert_passed();
 
 	// Unsubscribe 'subscriber' from topic.
-	const auto unsubscriptionFromSubscriber = subscriber.unsubscribe(aorOfInterest, regEventUri);
+	const auto unsubscriptionFromSubscriber = subscriberBis.unsubscribe(aorOfInterest, regEventUri);
 
 	asserter
 	    .iterateUpTo(
@@ -455,6 +457,7 @@ void multipleSubscribersToOneRecordKey() {
 		        FAIL_IF(!unsubscriptionFromSubscriber->isCompleted());
 		        FAIL_IF(unsubscriptionFromSubscriber->getStatus() != 200);
 		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 1);
 		        FAIL_IF(otherSubscriber.mTotalNotifyReceived != 2);
 		        return ASSERTION_PASSED();
 	        },
@@ -470,6 +473,7 @@ void multipleSubscribersToOneRecordKey() {
 	        32,
 	        [&]() {
 		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 1);
 		        FAIL_IF(otherSubscriber.mTotalNotifyReceived != 3);
 		        return ASSERTION_PASSED();
 	        },
@@ -484,6 +488,7 @@ void multipleSubscribersToOneRecordKey() {
 	        32,
 	        [&]() {
 		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 1);
 		        FAIL_IF(!unsubscriptionFromOtherSubscriber->isCompleted());
 		        FAIL_IF(unsubscriptionFromOtherSubscriber->getStatus() != 200);
 		        FAIL_IF(otherSubscriber.mTotalNotifyReceived != 3);
@@ -501,6 +506,7 @@ void multipleSubscribersToOneRecordKey() {
 	        32,
 	        [&]() {
 		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 1);
 		        FAIL_IF(otherSubscriber.mTotalNotifyReceived != 3);
 		        return ASSERTION_PASSED();
 	        },

tester/tests/conference/conference-server-tester.cc
@@ -283,7 +283,7 @@ void inviteResentOnReconnect() {
 	const auto devices = julienParticipant->getDevices();
 	BC_HARD_ASSERT_CPP_EQUAL(devices.size(), 1);
 	const auto& offlineDevice = devices.front();
-	BC_ASSERT_ENUM_EQUAL(offlineDevice->getState(), linphone::ParticipantDevice::State::Joining);
+	BC_ASSERT_ENUM_EQUAL(offlineDevice->getState(), linphone::ParticipantDevice::State::ScheduledForJoining);
 
 	SLOGD << "TEST " << __FUNCTION__ << " Client reconnects";
 	julien.reconnect();

commit 6256215de7728bba311a08d66921bc7fd6659b6d

fix(regevent): crash on subscription replacement
A heap-use-after-free error occurred when replacing a subscription. This bug
was introduced in 57c7e76a9080b894f1f15f6a93a9d0bd42d5fedc. As event
termination is now executed during the subscription replacement, the whole
entry in the map for the associated record key can be removed at the same
time. Therefore we must ensure it exists before using it.

src/registration-events/server.cpp
@@ -20,10 +20,10 @@
 
 #include <memory>
 
-#include "exceptions/bad-configuration.hh"
+#include "linphone++/enums.hh"
+
 #include "flexisip/registrar/registar-listeners.hh"
 #include "flexisip/utils/sip-uri.hh"
-#include "linphone++/enums.hh"
 #include "registrar/record.hh"
 #include "utils/configuration/transport.hh"
 #include "xml/reginfo.hh"
@@ -35,21 +35,17 @@ using namespace flexisip::Xsd::XmlSchema;
 
 namespace flexisip::RegistrationEvent {
 
-Server::Subscription::Subscription(const std::shared_ptr<linphone::Event>& event) : mEvent(event) {
-}
+Server::Subscription::Subscription(const std::shared_ptr<linphone::Event>& event) : mEvent(event) {}
 
 void Server::Subscription::onRecordFound(const shared_ptr<Record>& record) {
 	processRecord(record, "");
 }
 
-void Server::Subscription::onError(const flexisip::SipStatus&) {
-}
+void Server::Subscription::onError(const flexisip::SipStatus&) {}
 
-void Server::Subscription::onInvalid(const flexisip::SipStatus&) {
-}
+void Server::Subscription::onInvalid(const flexisip::SipStatus&) {}
 
-void Server::Subscription::onContactUpdated(const std::shared_ptr<ExtendedContact>&) {
-}
+void Server::Subscription::onContactUpdated(const std::shared_ptr<ExtendedContact>&) {}
 
 void Server::Subscription::onContactRegistered(const shared_ptr<Record>& record, const string& uidOfFreshlyRegistered) {
 	processRecord(record, uidOfFreshlyRegistered);
@@ -129,8 +125,7 @@ std::shared_ptr<linphone::Event> Server::Subscription::getEvent() const {
 }
 
 Server::Application::Application(const std::shared_ptr<RegistrarDb>& registrarDb)
-    : mRegistrarDb(registrarDb), mSubscriptions() {
-}
+    : mRegistrarDb(registrarDb), mSubscriptions() {}
 
 void Server::Application::onSubscribeReceived(const shared_ptr<Core>&,
                                               const shared_ptr<linphone::Event>& event,
@@ -164,25 +159,28 @@ void Server::Application::onSubscribeReceived(const shared_ptr<Core>&,
 	const auto recordKey = Record::Key(toUri, mRegistrarDb->useGlobalDomain());
 	const auto fromUri = event->getFromAddress()->asStringUriOnly();
 
-	// Iterator to the record key in the subscriptions map.
-	auto recordKeyIt = mSubscriptions.insert({recordKey.asString(), {}}).first;
+	// Manage subscription replacement if subscriber already exists for the same record key.
+	{
+		auto& subscriptions = mSubscriptions[recordKey.asString()];
+		const auto subscriptionIt =
+		    find_if(subscriptions.begin(), subscriptions.end(), [&fromUri](const auto& subscription) {
+			    return subscription->getEvent()->getFromAddress()->asStringUriOnly() == fromUri;
+		    });
 
-	auto& subscriptions = recordKeyIt->second;
-	const auto subscriptionIt =
-	    find_if(subscriptions.begin(), subscriptions.end(), [&fromUri](const auto& subscription) {
-		    return subscription->getEvent()->getFromAddress()->asStringUriOnly() == fromUri;
-	    });
-
-	// If subscriber already exists, replace the old subscription with the new one.
-	if (subscriptionIt != subscriptions.end()) {
-		if (auto event = (*subscriptionIt)->getEvent()) {
-			LOGD << "Replacing Subscription[event=" << event << "] from '" << fromUri
-			     << "' to record key '" << recordKey.asString() << "'";
-			// Event termination triggers erase from the mSubscriptions map
-			event->terminate();
+		// If subscriber already exists, replace the old subscription with the new one.
+		if (subscriptionIt != subscriptions.end()) {
+			if (auto oldEvent = (*subscriptionIt)->getEvent()) {
+				LOGD << "Replacing Subscription[event=" << oldEvent << "] from '" << fromUri << "' to record key '"
+				     << recordKey.asString() << "'";
+				// Event termination triggers erase from the mSubscriptions map.
+				oldEvent->terminate();
+			}
 		}
-
 	}
+
+	// Warning: the entry in the map is not guaranteed to be present (it may have been erased because of subscription
+	// replacement). The [] operator ensures that the entry is created if it does not exist.
+	auto& subscriptions = mSubscriptions[recordKey.asString()];
 	subscriptions.emplace_back(make_shared<Subscription>(event));
 	LOGD << "Added Subscription[event=" << event << "] from '" << fromUri << "' to record key '" << recordKey.asString()
 	     << "'";

tester/registration-event-tester.cc
@@ -19,12 +19,12 @@
 #include <memory>
 #include <string>
 
-#include "bctoolbox/logging.h"
 #include "bctoolbox/tester.h"
+#include "linphone++/linphone.hh"
+
 #include "flexisip/configmanager.hh"
 #include "flexisip/registrar/registar-listeners.hh"
 #include "flexisip/utils/sip-uri.hh"
-#include "linphone++/linphone.hh"
 #include "registrar/record.hh"
 #include "sofia-wrapper/nta-agent.hh"
 #include "sofia-wrapper/nta-outgoing-transaction.hh"
@@ -49,14 +49,10 @@ namespace flexisip::tester {
 
 class StubListener : public ContactUpdateListener {
 public:
-	void onRecordFound(const shared_ptr<Record>&) override {
-	}
-	void onError(const SipStatus&) override {
-	}
-	void onInvalid(const SipStatus&) override {
-	}
-	void onContactUpdated(const shared_ptr<ExtendedContact>&) override {
-	}
+	void onRecordFound(const shared_ptr<Record>&) override {}
+	void onError(const SipStatus&) override {}
+	void onInvalid(const SipStatus&) override {}
+	void onContactUpdated(const shared_ptr<ExtendedContact>&) override {}
 };
 
 // Check that a SUBSCRIBE without an Event header leads to Bad request
@@ -162,11 +158,11 @@ void basicSubscription() {
 	}
 
 	// Let's add a new device.
+	SLOGD << "Inserting a new device for participant 2";
 	inserter.insert({.uniqueId = "device-d"});
 	registrarDb->publish(otherParticipantTopic, "");
-	asserter
-	    .iterateUpTo(
-	        7, [&totalDevicesCount] { return LOOP_ASSERTION(4 <= totalDevicesCount()); }, 1s)
+	asserter.iterateUpTo(
+	            7, [&totalDevicesCount] { return LOOP_ASSERTION(4 <= totalDevicesCount()); }, 1s)
 	    .hard_assert_passed();
 
 	{
@@ -247,8 +243,7 @@ struct Subscriber {
 	                                                        reinterpret_cast<nta_agent_magic_t*>(this)),
 	      mAor(aor.str()), mUri("sip:" + aor.getUser() + "@127.0.0.1:" + mClient.getFirstPort() + ";transport=tcp"),
 	      mCallId(rsg.generate(10)), mFromTag(rsg.generate(10)), mTotalNotifyReceived(0), mToHeader(), mEvent("reg"),
-	      mAccept("application/reginfo+xml") {
-	}
+	      mAccept("application/reginfo+xml") {}
 
 	shared_ptr<sofiasip::NtaOutgoingTransaction> subscribe(string_view to, string_view destUri) {
 		auto request = make_unique<MsgSip>();
@@ -514,16 +509,140 @@ void multipleSubscribersToOneRecordKey() {
 	    .assert_passed();
 }
 
+void replaceSubscriptionWithOnlyOneSubscriber() {
+	auto random = tester::random::random();
+	auto rsg = random.string();
+
+	const auto suRoot = make_shared<sofiasip::SuRoot>();
+	const auto configuration = make_shared<ConfigManager>();
+	const auto registrarDb = make_shared<RegistrarDb>(suRoot, configuration);
+
+	const string aorOfInterest{"sip:aor-of-interest@sip.example.org"};
+	const auto topic = Record::Key{SipUri{aorOfInterest}, registrarDb->useGlobalDomain()};
+
+	// Fill the Registrar DB with a topic ('AOR of interest').
+	ContactInserter inserter{*registrarDb, make_shared<AcceptUpdatesListener>()};
+	const auto deviceId = rsg.generate(25);
+	inserter.withGruu(true).setExpire(10s).setAor(aorOfInterest).insert({.uniqueId = deviceId});
+
+	RegEventServer regEvent{registrarDb};
+	const auto regEventUri = regEvent.getTransport().str();
+
+	const auto onSubscriberResponse = [](nta_agent_magic_t* magic, nta_agent_t* agent, msg_t* msg, sip_t* sip) {
+		auto* subscriber = reinterpret_cast<Subscriber*>(magic);
+
+		if (sip->sip_request and sip->sip_request->rq_method == sip_method_notify) subscriber->mTotalNotifyReceived++;
+
+		if (subscriber->mToHeader.empty()) {
+			sofiasip::Home home{};
+			subscriber->mToHeader =
+			    "<"s + url_as_string(home.home(), sip->sip_from->a_url) + ">;tag=" + sip->sip_from->a_tag;
+		}
+
+		nta_msg_treply(agent, msg, 200, "Notification received", TAG_END());
+		return 0;
+	};
+
+	Subscriber subscriber{SipUri{"sip:subscriber@sip.example.org"}, onSubscriberResponse, rsg};
+	const auto subscriptionFromSubscriber = subscriber.subscribe(aorOfInterest, regEventUri);
+
+	CoreAssert asserter{regEvent.getCore(), suRoot, subscriber.mSuRoot};
+	asserter
+	    .iterateUpTo(
+	        32,
+	        [&]() {
+		        FAIL_IF(!subscriptionFromSubscriber->isCompleted());
+		        FAIL_IF(subscriptionFromSubscriber->getStatus() != 200);
+		        FAIL_IF(subscriber.mTotalNotifyReceived != 1);
+		        return ASSERTION_PASSED();
+	        },
+	        2s)
+	    .hard_assert_passed();
+
+	// Add a new device to 'AOR of interest'.
+	{
+		const auto newDeviceId = rsg.generate(25);
+		inserter.insert({.uniqueId = newDeviceId});
+		registrarDb->publish(topic, "");
+	}
+
+	asserter
+	    .iterateUpTo(
+	        32,
+	        [&]() {
+		        FAIL_IF(subscriber.mTotalNotifyReceived != 2);
+		        return ASSERTION_PASSED();
+	        },
+	        2s)
+	    .hard_assert_passed();
+
+	// Replace subscription from 'subscriber' to topic.
+	Subscriber subscriberBis{SipUri{"sip:subscriber@sip.example.org"}, onSubscriberResponse, rsg};
+	asserter.registerSteppable(subscriberBis.mSuRoot);
+	const auto newSubscriptionFromSubscriber = subscriberBis.subscribe(aorOfInterest, regEventUri);
+
+	asserter
+	    .iterateUpTo(
+	        32,
+	        [&]() {
+		        FAIL_IF(!newSubscriptionFromSubscriber->isCompleted());
+		        FAIL_IF(newSubscriptionFromSubscriber->getStatus() != 200);
+		        // A notification is sent on subscription replacement to terminate the old one.
+		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 1);
+		        return ASSERTION_PASSED();
+	        },
+	        2s)
+	    .hard_assert_passed();
+
+	// Add a new device to 'AOR of interest'.
+	{
+		const auto newDeviceId = rsg.generate(25);
+		inserter.insert({.uniqueId = newDeviceId});
+		registrarDb->publish(topic, "");
+	}
+
+	asserter
+	    .iterateUpTo(
+	        32,
+	        [&]() {
+		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 2);
+		        return ASSERTION_PASSED();
+	        },
+	        2s)
+	    .hard_assert_passed();
+
+	// Unsubscribe 'subscriber' from topic.
+	const auto unsubscriptionFromSubscriber = subscriberBis.unsubscribe(aorOfInterest, regEventUri);
+
+	asserter
+	    .iterateUpTo(
+	        32,
+	        [&]() {
+		        FAIL_IF(!unsubscriptionFromSubscriber->isCompleted());
+		        FAIL_IF(unsubscriptionFromSubscriber->getStatus() != 200);
+		        FAIL_IF(subscriber.mTotalNotifyReceived != 3);
+		        FAIL_IF(subscriberBis.mTotalNotifyReceived != 2);
+		        return ASSERTION_PASSED();
+	        },
+	        2s)
+	    .hard_assert_passed();
+}
+
 namespace {
 
-TestSuite _("regevent",
-            {
-                CLASSY_TEST(badSubscriptionRequest),
-                CLASSY_TEST(basicSubscription),
-                CLASSY_TEST(wrongEventHeaderInSubscribeRequest),
-                CLASSY_TEST(wrongAcceptHeaderInSubscribeRequest),
-                CLASSY_TEST(multipleSubscribersToOneRecordKey),
-            });
+TestSuite _{
+    "regevent",
+    {
+        CLASSY_TEST(badSubscriptionRequest),
+        CLASSY_TEST(basicSubscription),
+        CLASSY_TEST(wrongEventHeaderInSubscribeRequest),
+        CLASSY_TEST(wrongAcceptHeaderInSubscribeRequest),
+        CLASSY_TEST(multipleSubscribersToOneRecordKey),
+        CLASSY_TEST(replaceSubscriptionWithOnlyOneSubscriber),
+    },
+};
 
 }
 } // namespace flexisip::tester