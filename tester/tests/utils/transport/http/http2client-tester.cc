BelledonneCommunications/flexisip

commit 75e780d3b527071771d277c79bcd80cddddd159b

fix(Http2Client): try disconnect/reconnect in case of a connection error
An error can occur while trying to send HTTP/2 requests through a tcp connection (example: "connection reset by peer"). However, the Http2Client is not taking into account such errors and is still trying to send requests through the same - broken - connection.
It is now trying to establish the connection again before sending pending requests.

src/utils/transport/http/http2client.cc
@@ -144,6 +144,7 @@ void Http2Client::send(const shared_ptr<HttpRequest>& request,
 	if (status < 0) {
 		SLOGE << logPrefix << ": sending failed (" << nghttp2_strerror(status) << ")";
 		mActiveHttpContexts.erase(streamId);
+		if (!mConn->isConnected()) disconnect();
 		onErrorCb(request);
 		return;
 	}
@@ -235,7 +236,7 @@ void Http2Client::http2Setup() {
 	int status;
 	if ((status = mSessionSettings.submitTo(session)) != 0) {
 		SLOGE << mLogPrefix << ": sending settings failed (status = " << to_string(status) << ")";
-		disconnect();
+		abort();
 		return;
 	}
 
@@ -353,12 +354,12 @@ int Http2Client::onPollInCb(su_root_magic_t*, su_wait_t* w, su_wakeup_arg_t* arg
 
 	if (w->revents & SU_WAIT_HUP) {
 		SLOGD << thiz->mLogPrefix << ": peer has hung up";
-		thiz->disconnect();
+		thiz->abort();
 		return 0;
 	}
 	if (w->revents & SU_WAIT_ERR) {
 		SLOGE << thiz->mLogPrefix << ": socket error";
-		thiz->disconnect();
+		thiz->abort();
 		return 0;
 	}
 
@@ -366,13 +367,13 @@ int Http2Client::onPollInCb(su_root_magic_t*, su_wait_t* w, su_wakeup_arg_t* arg
 	if (status < 0) {
 		SLOGE << thiz->mLogPrefix << ": error while receiving HTTP2 data (" << nghttp2_strerror(status)
 		      << "), disconnecting";
-		thiz->disconnect();
+		thiz->abort();
 		return 0;
 	}
 	if (thiz->mLastSID >= 0) {
 		SLOGD << thiz->mLogPrefix << ": closing connection after receiving 'GOAWAY' frame, last processed stream is ["
 		      << thiz->mLastSID << "]";
-		thiz->disconnect();
+		thiz->abort();
 	}
 	return 0;
 }
@@ -434,11 +435,8 @@ void Http2Client::resumeSending(const std::string& logPrefix) {
 
 void Http2Client::disconnect() {
 	SLOGD << mLogPrefix << ": disconnecting";
-	if (mState == State::Disconnected) {
-		return;
-	}
-	discardAllPendingRequests();
-	discardAllActiveRequests();
+	if (mState == State::Disconnected) return;
+
 	su_root_unregister(mRoot.getCPtr(), &mPollInWait, onPollInCb, this);
 	mHttpSession.reset();
 	mConn->disconnect();
@@ -447,9 +445,18 @@ void Http2Client::disconnect() {
 	setState(State::Disconnected);
 }
 
+void Http2Client::abort() {
+	SLOGD << mLogPrefix << ": aborting";
+	if (mState == State::Disconnected) return;
+
+	discardAllPendingRequests();
+	discardAllActiveRequests();
+	disconnect();
+}
+
 void Http2Client::onConnectionIdle() noexcept {
 	SLOGD << mLogPrefix << ": connection is idle";
-	disconnect();
+	abort();
 }
 
 void Http2Client::setState(State state) noexcept {

tester/tests/utils/transport/http/http2client-tester.cc
@@ -1,30 +1,42 @@
-/** Copyright (C) 2010-2023 Belledonne Communications SARL
- *  SPDX-License-Identifier: AGPL-3.0-or-later
- */
+/*
+    Flexisip, a flexible SIP proxy server with media capabilities.
+    Copyright (C) 2010-2025 Belledonne Communications SARL, All rights reserved.
 
-#include "bctoolbox/tester.h"
-#include "utils/http-mock/http-mock.hh"
+    This program is free software: you can redistribute it and/or modify
+    it under the terms of the GNU Affero General Public License as
+    published by the Free Software Foundation, either version 3 of the
+    License, or (at your option) any later version.
+
+    This program is distributed in the hope that it will be useful,
+    but WITHOUT ANY WARRANTY; without even the implied warranty of
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+    GNU Affero General Public License for more details.
+
+    You should have received a copy of the GNU Affero General Public License
+    along with this program. If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#include <sys/types.h>
 
 #include <atomic>
 #include <chrono>
-#include <limits>
 #include <memory>
 #include <mutex>
 #include <optional>
 #include <sstream>
 #include <string>
 
-#include <sys/types.h>
-
-#include <nghttp2/nghttp2.h>
-
+#include "bctoolbox/tester.h"
 #include "flexisip/sofia-wrapper/su-root.hh"
-
+#include "utils/core-assert.hh"
+#include "utils/http-mock/http-mock.hh"
+#include "utils/server/tls-server.hh"
 #include "utils/test-patterns/test.hh"
 #include "utils/test-suite.hh"
 #include "utils/transport/http/http-headers.hh"
 #include "utils/transport/http/http2client.hh"
 
+using namespace std;
 using namespace std::string_literals;
 using namespace std::chrono_literals;
 
@@ -126,11 +138,68 @@ void partiallySentRequestResumedAtWindowUpdate() {
 	setup.root.run();
 }
 
+/**
+ * Test that the Http2Client switches to the "disconnected" state if the "connection reset by peer" error occurs.
+ * Then, it should try to reconnect before sending pending requests.
+ */
+void reconnectAfterConnectionResetByPeer() {
+	TlsServer server{};
+	const auto serverPort = to_string(server.getPort());
+
+	sofiasip::SuRoot root{};
+	CoreAssert asserter{root};
+
+	const auto onError = [&root](const auto&) { root.quit(); };
+	const auto onResponse = [&root](const auto&, const auto&) { root.quit(); };
+	const auto client = Http2Client::make(root, "127.0.0.1", serverPort);
+	client->setRequestTimeout(5s);
+	client->enableInsecureTestMode();
+
+	const auto& conn = client->getConnection();
+	const auto func = [&server] {
+		return server.runServerForTest("PRI * HTTP/2.0\r\n\r\nSM\r\n\r\n", "stub-response", 0ms);
+	};
+
+	// The first request works as expected.
+	{
+		auto result = async(launch::async, func);
+		client->send(make_shared<Http2Client::HttpRequest>(HttpHeaders{}, "stub-request"), onResponse, onError);
+		asserter.iterateUpTo(0x20, [&conn] { return LOOP_ASSERTION(conn->isConnected()); }, 2s).assert_passed();
+		BC_HARD_ASSERT(result.wait_for(2s) == std::future_status::ready);
+		BC_ASSERT(result.get());
+	}
+
+	// Simulate the "connection reset by peer" error.
+	server.resetSocket();
+
+	// The second request fails, and the connection is then closed by the client.
+	// Test that the third request works as expected.
+	{
+		auto result = async(launch::async, func);
+		client->send(make_shared<Http2Client::HttpRequest>(HttpHeaders{}, "stub-request"), onResponse, onError);
+
+		asserter.iterateUpTo(0x20, [&conn] { return LOOP_ASSERTION(!conn->isConnected()); }, 2s).assert_passed();
+
+		BC_HARD_ASSERT(result.wait_for(500ms) == std::future_status::timeout);
+
+		client->send(make_shared<Http2Client::HttpRequest>(HttpHeaders{}, "stub-request"), onResponse, onError);
+		asserter.iterateUpTo(0x20, [&conn] { return LOOP_ASSERTION(conn->isConnected()); }, 2s).assert_passed();
+
+		BC_HARD_ASSERT(result.wait_for(2s) == std::future_status::ready);
+		BC_ASSERT(result.get());
+	}
+}
+
 namespace {
-TestSuite _("Http2Client",
-            {
-                CLASSY_TEST(partiallySentRequestCanceledByTimeout),
-                CLASSY_TEST(partiallySentRequestResumedAtWindowUpdate),
-            });
+
+TestSuite _{
+    "Http2Client",
+    {
+        CLASSY_TEST(partiallySentRequestCanceledByTimeout),
+        CLASSY_TEST(partiallySentRequestResumedAtWindowUpdate),
+        CLASSY_TEST(reconnectAfterConnectionResetByPeer),
+    },
+};
+
 }
 } // namespace flexisip::tester

src/utils/transport/http/http2client.hh
@@ -199,7 +199,14 @@ private:
 
 	void tlsConnect();
 	void http2Setup();
+	/**
+	 * Disconnect the client, clear timers and switch to the 'Disconnected' state.
+	 */
 	void disconnect();
+	/**
+	 * Disconnect the client (see @disconnect) and clear the list of active and pending requests.
+	 */
+	void abort();
 
 	int sendAll() {
 		return nghttp2_session_send(mHttpSession.get());

src/utils/transport/tls-connection.cc
@@ -287,6 +287,8 @@ int TlsConnection::write(const void* data, int dlen) noexcept {
 			return 0;
 		}
 		handleBioError(mLogPrefix + "Error while writing data", nbBytes);
+		// If an error occurs and that we know we cannot simply retry, prefer disconnecting right now.
+		disconnect();
 	}
 
 	return nbBytes;

tester/utils/server/tls-server.cc
@@ -1,6 +1,6 @@
 /*
     Flexisip, a flexible SIP proxy server with media capabilities.
-    Copyright (C) 2010-2022  Belledonne Communications SARL, All rights reserved.
+    Copyright (C) 2010-2025 Belledonne Communications SARL, All rights reserved.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -9,7 +9,7 @@
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
@@ -21,8 +21,6 @@
 #include "bctoolbox/tester.h"
 
 #include "flexisip/logmanager.hh"
-
-#include "flexisip-tester-config.hh"
 #include "tester.hh"
 #include "tls-server.hh"
 
@@ -43,7 +41,7 @@ TlsServer::TlsServer(int port)
 	mContext.set_options(ssl::context::default_workarounds | ssl::context::verify_none | ssl::context::no_sslv2 |
 	                     ssl::context::no_sslv3);
 	mContext.use_certificate_chain_file(bcTesterRes("cert/self.signed.cert.test.pem"));
-	mContext.use_private_key_file(bcTesterRes("cert/self.signed.key.test.pem"), boost::asio::ssl::context::pem);
+	mContext.use_private_key_file(bcTesterRes("cert/self.signed.key.test.pem"), context::pem);
 	mSocket = make_unique<ssl::stream<ip::tcp::socket>>(mIoService, mContext);
 }
 
@@ -51,11 +49,11 @@ void TlsServer::accept() {
 	LOGD("TlsServer[%p] entering accept", this);
 	mAcceptor.accept(mSocket->lowest_layer());
 	LOGD("TlsServer[%p] new connection accepted, starting handshake", this);
-	mSocket->handshake(boost::asio::ssl::stream_base::server);
+	mSocket->handshake(ssl::stream_base::server);
 	LOGD("TlsServer[%p] handshake ok", this);
 }
 
-void TlsServer::accept(std::string sniValueExpected) {
+void TlsServer::accept(const std::string& sniValueExpected) {
 	accept();
 
 	const auto SSL = mSocket->native_handle();
@@ -64,10 +62,12 @@ void TlsServer::accept(std::string sniValueExpected) {
 	if (sniType == -1 && !sniValueExpected.empty()) {
 		BC_FAIL("No SNI found after SSL hanshake.");
 		return;
-	} else if (sniType != -1 && sniValueExpected.empty()) {
+	}
+	if (sniType != -1 && sniValueExpected.empty()) {
 		BC_FAIL("SNI found after SSL hanshake.");
 		return;
-	} else if (sniType == -1 && sniValueExpected.empty()) {
+	}
+	if (sniType == -1 && sniValueExpected.empty()) {
 		return;
 	}
 
@@ -82,7 +82,7 @@ std::string TlsServer::read() {
 	std::istream is(&b);
 	ostringstream line;
 	line << is.rdbuf();
-	LOGD("TlsServer[%p] read : %s", this, line.str().c_str());
+	LOGD("TlsServer[%p] read: %s", this, line.str().c_str());
 	return line.str();
 }
 
@@ -90,7 +90,11 @@ void TlsServer::send(const std::string& message) {
 	LOGD("TlsServer[%p] entering send", this);
 	const string msg = message + "\n";
 	boost::asio::write(*mSocket, buffer(message));
-	LOGD("TlsServer[%p] send : %s", this, message.c_str());
+	LOGD("TlsServer[%p] send: %s", this, message.c_str());
+}
+
+int TlsServer::getPort() const {
+	return mAcceptor.local_endpoint().port();
 }
 
 bool TlsServer::runServerForTest(const std::string& expectedRequest,
@@ -101,4 +105,9 @@ bool TlsServer::runServerForTest(const std::string& expectedRequest,
 	this_thread::sleep_for(responseDelay);
 	send(response);
 	return request == expectedRequest;
+}
+
+void TlsServer::resetSocket() {
+	LOGD("TlsServer[%p] entering %s", this, __func__);
+	mSocket = make_unique<ssl::stream<ip::tcp::socket>>(mIoService, mContext);
 }

tester/utils/server/tls-server.hh
@@ -1,6 +1,6 @@
 /*
     Flexisip, a flexible SIP proxy server with media capabilities.
-    Copyright (C) 2010-2022  Belledonne Communications SARL, All rights reserved.
+    Copyright (C) 2010-2025 Belledonne Communications SARL, All rights reserved.
 
     This program is free software: you can redistribute it and/or modify
     it under the terms of the GNU Affero General Public License as
@@ -9,7 +9,7 @@
 
     This program is distributed in the hope that it will be useful,
     but WITHOUT ANY WARRANTY; without even the implied warranty of
-    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
     GNU Affero General Public License for more details.
 
     You should have received a copy of the GNU Affero General Public License
@@ -17,8 +17,6 @@
  */
 
 #pragma once
-
-#include <iostream>
 
 #include <boost/asio.hpp>
 #include <boost/asio/ssl.hpp>
@@ -35,13 +33,13 @@ public:
 	                      const std::chrono::milliseconds responseDelay = std::chrono::milliseconds{0});
 
 	void accept();
-	void accept(std::string sniValueExpected);
+	void accept(const std::string& sniValueExpected);
 	std::string read();
 	void send(const std::string& message);
 
-	int getPort() {
-		return mAcceptor.local_endpoint().port();
-	}
+	int getPort() const;
+
+	void resetSocket();
 
 private:
 	boost::asio::io_service mIoService;

commit 6fb71910cd4c61fd42416b4067d8d30c1df4d75b

fix(Http2Client): prevent infinite loop on "connection reset by peer"
Resolves an edge case where the client enters an infinite loop when:
- It is in the CONNECTING state with pending requests.
- After transitioning to CONNECTED, it attempts to send all pending requests.
- A "connection reset by peer" error occurs during this process.

The client was continuously retrying to send the same pending requests due to
the broken connection, causing the loop.

Bug introduced in 75e780d3b527071771d277c79bcd80cddddd159b

src/utils/transport/http/http2client.cc
@@ -19,6 +19,7 @@
 #include "http2client.hh"
 
 #include <array>
+#include <iterator>
 #include <sstream>
 
 #include <nghttp2/nghttp2.h>
@@ -79,8 +80,17 @@ Http2Client::Http2Client(sofiasip::SuRoot& root,
 }
 
 void Http2Client::sendAllPendingRequests() {
-	for (auto it = mPendingHttpContexts.begin(); it != mPendingHttpContexts.end();
-	     it = mPendingHttpContexts.erase(it)) {
+	HttpContextList pendingHttpContexts{};
+	pendingHttpContexts.swap(mPendingHttpContexts);
+
+	for (auto it = pendingHttpContexts.begin(); it != pendingHttpContexts.end(); ++it) {
+		if (mState != State::Connected) {
+			// Keep requests that were already pending ahead of requests queued while handling the connection error.
+			mPendingHttpContexts.insert(mPendingHttpContexts.begin(), std::move_iterator(it),
+			                            std::move_iterator(pendingHttpContexts.end()));
+			return;
+		}
+
 		send(it->get()->getRequest(), it->get()->getOnResponseCb(), it->get()->getOnErrorCb());
 	}
 }

tester/tester.cc
@@ -17,7 +17,8 @@
 */
 
 #include "tester.hh"
-#include "utils/rand.hh"
+
+#include <signal.h>
 
 #include <cstddef>
 #include <cstdlib>
@@ -38,7 +39,8 @@
 #endif
 
 #include "flexisip-tester-config.hh"
-#include <flexisip/logmanager.hh>
+#include "flexisip/logmanager.hh"
+#include "utils/rand.hh"
 
 namespace flexisip::tester {
 
@@ -160,6 +162,10 @@ void flexisip_tester_init() {
 	flexisip_tester_add_grammar_loader_path(kLibLinphoneLocalGrammarLocation);
 
 	flexisip_tester_set_factory_resources_path(FLEXISIP_ROOT_DIR);
+
+	// We must ignore SIGPIPE to avoid the process being killed when a socket is closed by the peer.
+	// Therefore, we are able to test error cases.
+	signal(SIGPIPE, SIG_IGN);
 }
 
 void flexisip_tester_uninit(void) {

tester/tests/utils/transport/http/http2client-tester.cc
@@ -20,11 +20,13 @@
 
 #include <atomic>
 #include <chrono>
+#include <future>
+#include <initializer_list>
 #include <memory>
-#include <mutex>
 #include <optional>
 #include <sstream>
 #include <string>
+#include <vector>
 
 #include "flexisip/sofia-wrapper/su-root.hh"
 #include "utils/core-assert.hh"
@@ -192,6 +194,126 @@ void reconnectAfterConnectionResetByPeer() {
 	}
 }
 
+/**
+ * Requests queued before a connection is established are sent once the connection is ready.
+ */
+void requestsQueuedBeforeConnectionSetupAreSentInOrder() {
+	sofiasip::SuRoot root{};
+
+	atomic_int responsesReceivedCount{0};
+	const auto onResponse = [&responsesReceivedCount](const auto&, const auto&) { ++responsesReceivedCount; };
+	atomic_int errorsCount{0};
+	const auto onError = [&errorsCount](const auto&) { ++errorsCount; };
+
+	HttpMock httpMock{{"/"}};
+	const auto portInt = httpMock.serveAsync();
+	BC_HARD_ASSERT_TRUE(portInt > -1);
+	const auto port = to_string(portInt);
+
+	auto client = Http2Client::make(root, "127.0.0.1", port);
+	const HttpHeaders headers = {
+	    {":method"s, "POST"s},
+	    {":scheme", "https"},
+	    {":authority", "127.0.0.1:" + port},
+	    {":path", "/"},
+	};
+	const vector<string> requestBodies{"request_1", "request_2", "request_3"};
+
+	for (const auto& body : requestBodies) {
+		// Note: the client initiates the connection on the first send, so the first request is queued while the
+		// connection is being established. Following requests are queued while the connection is being established, and
+		// sent in order once the connection is ready.
+		client->send(make_shared<Http2Client::HttpRequest>(headers, body), onResponse, onError);
+	}
+
+	CoreAssert asserter{root};
+	asserter.wait([&responsesReceivedCount] { return LOOP_ASSERTION(responsesReceivedCount == 3); })
+	    .hard_assert_passed();
+
+	for (const auto& expectedBody : requestBodies) {
+		const auto request = httpMock.popRequestReceived();
+		BC_HARD_ASSERT_TRUE(request != nullptr);
+		BC_ASSERT_STRING_EQUAL(request->body.c_str(), expectedBody.c_str());
+	}
+
+	BC_ASSERT(httpMock.popRequestReceived() == nullptr);
+}
+
+/**
+ * A send failure during pending-request draining must preserve the remaining requests and their order.
+ */
+void pendingRequestsRemainOrderedAfterDrainFailure() {
+	sofiasip::SuRoot root{};
+
+	atomic_int responsesReceivedCount{0};
+	const auto onResponse = [&responsesReceivedCount](const auto&, const auto&) { ++responsesReceivedCount; };
+	vector<string> errorBodies{};
+	const auto onError = [&errorBodies](const shared_ptr<Http2Client::HttpRequest>& request) {
+		errorBodies.emplace_back(request->getBody().begin(), request->getBody().end());
+	};
+
+	auto firstServer = make_unique<TlsServer>();
+	const auto serverPort = firstServer->getPort();
+
+	auto client = Http2Client::make(root, "127.0.0.1", to_string(serverPort));
+	client->setRequestTimeout(1s);
+	client->enableInsecureTestMode();
+
+	const HttpHeaders headers = {
+	    {":method"s, "POST"s},
+	    {":scheme", "https"},
+	    {":authority", "127.0.0.1:" + to_string(serverPort)},
+	    {":path", "/"},
+	};
+	vector<string> requestBodies{"request_1", "request_2", "request_3"};
+
+	auto firstConnection = async(launch::async, [&firstServer] {
+		firstServer->accept();
+		// Simulate a connection drop after the first request is received.
+		firstServer.reset();
+	});
+
+	// Try to send requests that will be queued while the connection is being established. The first request will fail,
+	// and the connection will be closed, leaving the other requests pending.
+	for (const auto& body : requestBodies) {
+		client->send(make_shared<Http2Client::HttpRequest>(headers, body), onResponse, onError);
+	}
+
+	CoreAssert asserter{root};
+	// Wait until the first request fails and the connection is closed, leaving the other requests pending.
+	asserter.wait([&] { return LOOP_ASSERTION(errorBodies.size() == 1); }).hard_assert_passed();
+	asserter.wait([&] { return LOOP_ASSERTION(!client->isConnected()); }).hard_assert_passed();
+	BC_HARD_ASSERT(errorBodies.front() == requestBodies.front());
+	requestBodies.erase(requestBodies.begin());
+
+	// Reopen the server with the same port.
+	auto secondServer = make_unique<HttpMock>(initializer_list<string>{"/"});
+	BC_ASSERT(secondServer->serveAsync(to_string(serverPort)) == serverPort);
+
+	// Send a new request to trigger reconnection and draining of the pending requests.
+	requestBodies.emplace_back("request_4");
+	client->send(make_shared<Http2Client::HttpRequest>(headers, requestBodies.back()), onResponse, onError);
+
+	asserter
+	    .wait([&] {
+		    FAIL_IF(!client->isConnected());
+		    FAIL_IF(errorBodies.size() != 1);
+		    FAIL_IF(responsesReceivedCount != 3);
+		    FAIL_IF(!client->isIdle());
+		    return ASSERTION_PASSED();
+	    })
+	    .hard_assert_passed();
+
+	// Assert that all the pending requests were sent in order after the first request failed.
+	for (const auto& expectedBody : requestBodies) {
+		const auto request = secondServer->popRequestReceived();
+		BC_HARD_ASSERT_TRUE(request != nullptr);
+		BC_ASSERT_STRING_EQUAL(request->body.c_str(), expectedBody.c_str());
+	}
+
+	BC_ASSERT(secondServer->popRequestReceived() == nullptr);
+}
+
 namespace {
 
 TestSuite _{
@@ -200,6 +322,8 @@ TestSuite _{
         CLASSY_TEST(partiallySentRequestCanceledByTimeout),
         CLASSY_TEST(partiallySentRequestResumedAtWindowUpdate),
         CLASSY_TEST(reconnectAfterConnectionResetByPeer),
+        CLASSY_TEST(requestsQueuedBeforeConnectionSetupAreSentInOrder),
+        CLASSY_TEST(pendingRequestsRemainOrderedAfterDrainFailure),
     },
 };
 